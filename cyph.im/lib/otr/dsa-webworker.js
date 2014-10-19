fef7386f8345bc6a397273bb6c1fcaea4422bf12
Ryan Lester <hacker@linux.com>
2014-10-18 23:19:52 -0700

    debugging mobile issues

--- cyph.im/lib/otr/dsa-webworker.js
+++ cyph.im/lib/otr/dsa-webworker.js
@@ -1,1 +1,52 @@
-(function(e){"use strict";function n(e,t){postMessage({type:e,val:t})}e.OTR={};e.DSA={};e.crypto={randomBytes:function(){throw new Error("Haven't seeded yet.")}};var t=["vendor/salsa20.js","vendor/bigint.js","vendor/crypto.js","vendor/eventemitter.js","lib/const.js","lib/helpers.js","lib/dsa.js"];onmessage=function(r){var i=r.data;if(i.imports)t=i.imports;importScripts.apply(e,t);var s=new e.Salsa20(i.seed.slice(0,32),i.seed.slice(32));e.crypto.randomBytes=function(e){return s.getBytes(e)};if(i.debug)n("debug","DSA key creation started");var o;try{o=new e.DSA}catch(r){if(i.debug)n("debug",r.toString());return}if(i.debug)n("debug","DSA key creation finished");n("data",o.packPrivate())}})(this)
+;(function (root) {
+  "use strict";
+
+  root.OTR = {}
+  root.DSA = {}
+  root.crypto = {
+    randomBytes: function () {
+      throw new Error("Haven't seeded yet.")
+    }
+  }
+
+  // default imports
+  var imports = [
+      'vendor/salsa20.js'
+    , 'vendor/bigint.js'
+    , 'vendor/crypto.js'
+    , 'vendor/eventemitter.js'
+    , 'lib/const.js'
+    , 'lib/helpers.js'
+    , 'lib/dsa.js'
+  ]
+
+  function sendMsg(type, val) {
+    postMessage({ type: type, val: val })
+  }
+
+  onmessage = function (e) {
+    var data = e.data;
+
+    if (data.imports) imports = data.imports
+    importScripts.apply(root, imports);
+
+    // use salsa20 since there's no prng in webworkers
+    var state = new root.Salsa20(data.seed.slice(0, 32), data.seed.slice(32))
+    root.crypto.randomBytes = function (n) {
+      return state.getBytes(n)
+    }
+
+    if (data.debug) sendMsg('debug', 'DSA key creation started')
+    var dsa
+    try {
+      dsa = new root.DSA()
+    } catch (e) {
+      if (data.debug) sendMsg('debug', e.toString())
+      return
+    }
+    if (data.debug) sendMsg('debug', 'DSA key creation finished')
+
+    sendMsg('data', dsa.packPrivate())
+  }
+
+}(this))
