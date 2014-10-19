15eab84cd770b8dc090ced6304fcbe8dff1f02c7
Ryan Lester <hacker@linux.com>
2014-10-19 14:29:24 -0700

    silly bug

--- deploy.sh
+++ deploy.sh
@@ -25,7 +25,7 @@ cd ../..
 
 find . -name '*.js' | grep -v lib | xargs -I% uglifyjs "%" -o "%"
 find . -name '*.css' | xargs -I% cleancss -o "%" "%"
-find . -name '*.html' | html-minifier --minify-js --minify-css --remove-comments --collapse-whitespace "%" -o "%"
+find . -name '*.html' | xargs -I% html-minifier --minify-js --minify-css --remove-comments --collapse-whitespace "%" -o "%"
 
 goapp deploy default/app.yaml cyph.com/cyph-com.yaml cyph.im/cyph-im.yaml cyph.me/cyph-me.yaml
 # goapp deploy default/app.yaml `ls !(default)/*.yaml | tr '\n' ' '`
