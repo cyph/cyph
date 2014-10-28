c0608f259bf7f82f50b8d31247a2301ae596c711
Ryan Lester <hacker@linux.com>
2014-10-27 22:16:48 -0400

    translations

--- translate.py
+++ translate.py
@@ -25,7 +25,6 @@ languages	= [
 	'cs',
 	'da',
 	'nl',
-	'en',
 	'et',
 	'fi',
 	'fr',
@@ -170,27 +169,11 @@ for language in languages:
 
 
 
-# yaml		= check_output('ls *.yaml | head -n1', shell=True)[0:-1]
-# f			= open(yaml, 'r')
-# yamlText	= f.read()
-# f.close()
-
-# handlers		= yamlText.split('handlers:')[1].split('# TRANSLATIONS')[0]
-# yamlTextSplit	= yamlText.split('# TRANSLATIONS')
-# yamlText		= yamlTextSplit[0]
-# yamlTextEnd		= yamlTextSplit[1]
-
-# languages.append('en')
-
-# for language in languages:
-# 	newHandlers	= handlers.replace('url: ', 'url: /' + language)
-# 	newHandlers	= newHandlers.replace('/' + language + '/\n', '/' + language + '\n')
-# 	newHandlers	= newHandlers.replace('index.html', language + '.html')
-
-# 	yamlText	+= newHandlers
-
-# yamlText	+= yamlTextEnd
-
-# f	= open(yaml, 'w')
-# f.write(yamlText)
-# f.close()
+languages.append('en')
+yaml		= check_output('ls *.yaml | head -n1', shell=True)[0:-1]
+f			= open(yaml, 'r')
+yamlText	= f.read().replace('BALLS', '|'.join(languages))
+f.close()
+f			= open(yaml, 'w')
+f.write(yamlText)
+f.close()
