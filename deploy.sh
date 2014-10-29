c432ef476d27e4bc1cc6f9e0833c9d45853129d2
Ryan Lester <hacker@linux.com>
2014-10-28 20:31:06 -0400

    added staging flag to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,12 @@
 dir="$(pwd)"
 cd $(cd "$(dirname "$0")"; pwd) # $(dirname `readlink -f "${0}" || realpath "${0}"`)
 
+staging=''
+if [ "${1}" == '--staging' ] ; then 
+	staging=true
+	shift
+fi
+
 comment="${*}"
 test "${comment}" == "" && comment=deploy
 ./git.sh "${comment}"
@@ -32,7 +38,11 @@ find . -name '*.css' | xargs -I% cleancss -o "%" "%"
 echo 'HTML Minify'
 find . -name '*.html' | xargs -I% html-minifier --minify-js --minify-css --remove-comments --collapse-whitespace "%" -o "%"
 
-ls */*.yaml | xargs -I% appcfg.py rollback %
+if [ $staging ] ; then
+	ls */*.yaml | xargs -I% sed -i.bak 's/version: prod/version: staging/g' %
+fi
+
+# ls */*.yaml | xargs -I% appcfg.py rollback %
 goapp deploy default/app.yaml cyph.com/cyph-com.yaml cyph.im/cyph-im.yaml cyph.me/cyph-me.yaml
 # goapp deploy default/app.yaml `ls !(default)/*.yaml | tr '\n' ' '`
 
