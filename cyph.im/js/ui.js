cb5df5b58b279c5e5ba27e0635aa64ff3e01e059
Ryan Lester <hacker@linux.com>
2014-10-31 18:39:46 -0400

    visibility detection fix

--- cyph.im/js/ui.js
+++ cyph.im/js/ui.js
@@ -410,7 +410,7 @@ angular.
 		var openNotifications	= [];
 
 		notify	= function (message) {
-			if (!disableNotify && Visibility.hidden()) {
+			if (!disableNotify && Visibility.hidden) {
 				if (window.Notification) {
 					var notification	= new Notification(notifyTitle, {body: message, icon: notifyIcon});
 
@@ -421,7 +421,7 @@ angular.
 							openNotifications.pop().close();
 						}
 
-						if (Visibility.hidden()) {
+						if (Visibility.hidden) {
 							disableNotify	= true;
 						}
 					};
@@ -536,8 +536,8 @@ angular.
 
 
 		/* For notify and mobile fullscreen */
-		Visibility.change(function (e, state) {
-			if (state != 'hidden') {
+		Visibility.change(function (isHidden) {
+			if (!isHidden) {
 				disableNotify	= false;
 				while (openNotifications.length > 0) {
 					openNotifications.pop().close();
@@ -563,7 +563,7 @@ angular.
 				return;
 			}
 
-			var isHidden	= Visibility.hidden();
+			var isHidden	= Visibility.hidden;
 			var currentScrollPosition	=
 				$messageList[0].scrollHeight -
 				($messageList[0].scrollTop + $messageList[0].clientHeight)
@@ -578,7 +578,7 @@ angular.
 				apply(function () { $scope.unreadMessages += 1 });
 
 				var intervalId	= setInterval(function () {
-					if (!Visibility.hidden() && ($elem.is(':appeared') || $elem.nextAll(':not(.unread)').length > 0)) {
+					if (!Visibility.hidden && ($elem.is(':appeared') || $elem.nextAll(':not(.unread)').length > 0)) {
 						clearInterval(intervalId);
 						$elem.removeClass('unread');
 						apply(function () { $scope.unreadMessages -= 1 });
