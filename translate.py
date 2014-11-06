72c624a4ec664ab40f4333393ffbbff237b56c9f
Ryan Lester <hacker@linux.com>
2014-11-06 02:01:15 -0800

    deploy

--- translate.py
+++ translate.py
@@ -157,7 +157,7 @@ for language in languages:
 
 
 	f	= open(cyphtranslationsPath, 'w')
-	f.write(json.dumps(cyphtranslations[language], sort_keys = True, indent = True))
+	f.write(json.dumps(cyphtranslations[language], sort_keys = True, indent = True, separators = (',', ': ')))
 	f.close()
 
 
