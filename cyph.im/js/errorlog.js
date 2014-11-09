f1c98ae28e8acbbac5bb3dbceff2e449e273c3a1
Ryan Lester <hacker@linux.com>
2014-11-09 04:16:14 -0800

    error log URL

--- cyph.im/js/errorlog.js
+++ cyph.im/js/errorlog.js
@@ -1,6 +1,6 @@
 /* Log all JS exceptions */
 window.onerror = function () {
 	$.post('https://api.cyph.com/errors', {
-		error: JSON.stringify(arguments) + '\n\n' + navigator.userAgent + '\n\n' + navigator.language
+		error: JSON.stringify(arguments) + '\n\n' + navigator.userAgent + '\n\n' + navigator.language '\n\n' + document.location.toString()
 	});
 }
