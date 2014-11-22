206c00b678e44bfd4ae831819a1da2b2b3548d03
Ryan Lester <hacker@linux.com>
2014-11-22 17:03:51 -0500

    major video fixes (proper positioning/scaling, and now works on mobile thanks to gifification)

--- cyph.com/js/errorlog.js
+++ cyph.com/js/errorlog.js
@@ -1,10 +1,1 @@
-/* Log all JS exceptions */
-window.onerror = function () {
-	$.post('https://api.cyph.com/errors', {
-		error: JSON.stringify(arguments) +
-			'\n\n' + navigator.userAgent +
-			'\n\n' + navigator.language +
-			'\n\n' + language +
-			'\n\n' + document.location.toString()
-	});
-}
+../../shared/js/errorlog.js
