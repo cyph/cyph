6e5153c2359dbc9b8e5c3a43e515bd207eb74e97
Ryan Lester <hacker@linux.com>
2014-12-22 03:01:18 -0500

    base WebRTC implementation

--- cyph.im/js/cyph.im.js
+++ cyph.im/js/cyph.im.js
@@ -3,13 +3,14 @@ var preConnectMessageReceiveQueue	= [];
 var preConnectMessageSendQueue		= [];
 
 var CHANNEL_DATA_PREFIX	= 'CHANNEL DATA: ';
+var WEBRTC_DATA_PREFIX	= 'webrtc: ';
 
 var channelDataMisc	= {
-	connect: 1,
-	ping: 2,
-	pong: 3,
-	imtypingyo: 4,
-	donetyping: 5
+	connect: '1',
+	ping: '2',
+	pong: '3',
+	imtypingyo: '4',
+	donetyping: '5'
 };
 
 var channel, isConnected, isOtrReady, pongReceived, shouldSendQueryMessage, socket;
@@ -223,19 +224,29 @@ function receiveChannelData (data) {
 		if (o.Misc == channelDataMisc.ping) {
 			sendChannelData({Misc: channelDataMisc.pong});
 		}
-		if (o.Misc == channelDataMisc.connect) {
+		else if (o.Misc == channelDataMisc.connect) {
 			beginChat();
 		}
-		if (o.Misc == channelDataMisc.imtypingyo) {
+		else if (o.Misc == channelDataMisc.imtypingyo) {
 			friendIsTyping(true);
 		}
-		if (o.Misc == channelDataMisc.donetyping) {
+		else if (o.Misc == channelDataMisc.donetyping) {
 			friendIsTyping(false);
 		}
+		else if (o.Misc && o.Misc.indexOf(WEBRTC_DATA_PREFIX) == 0) {
+			var webRTCData	= JSON.parse(o.Misc.split(WEBRTC_DATA_PREFIX)[1]);
+			var key			= Object.keys(webRTCData)[0];
+
+			if (webRTC[key]) {
+				webRTC[key](webRTCData[key]);
+			}
+		}
+
 		if (o.Message) {
 			otr.receiveMsg(o.Message);
 			logCyphertext(o.Message, authors.friend);
 		}
+
 		if (o.Destroy) {
 			socketClose();
 		}
@@ -301,6 +312,100 @@ function setUpChannel (channelData) {
 		onerror: function () {},
 		onclose: closeChat
 	});
+}
+
+
+/* Logic for handling WebRTC connections (used for file transfers and voice/video chat) */
+
+var PeerConnection		= window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+var IceCandidate		= window.mozRTCIceCandidate || window.RTCIceCandidate;
+var SessionDescription	= window.mozRTCSessionDescription || window.RTCSessionDescription;
+navigator.getUserMedia	= navigator.getUserMedia || navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
+
+var webRTC	= {
+	channel: null,
+	peer: null,
+
+	isAvailable: false,
+
+	isSupported: !!PeerConnection,
+
+	addIceCandidate: function (candidate) {
+		webRTC.peer.addIceCandidate(new IceCandidate(JSON.parse(candidate)));
+	},
+
+	bindChannelEvents: function () {
+		webRTC.channel.onopen		= function () {
+			webRTC.isAvailable	= true;
+			console.log('WebRTC Channel Open');
+		};
+
+		webRTC.channel.onmessage	= function (e) {
+			console.log(e.data);
+		};
+	},
+
+	receiveAnswer: function (answer) {
+		webRTC.peer.setRemoteDescription(new SessionDescription(JSON.parse(answer)));
+	},
+
+	receiveOffer: function (offer) {
+		setUpWebRTC(false);
+
+		webRTC.peer.ondatachannel = function (e) {
+			webRTC.channel	= e.channel;
+			webRTC.bindChannelEvents();
+		};
+
+		webRTC.peer.setRemoteDescription(new SessionDescription(JSON.parse(offer)));
+
+		webRTC.peer.createAnswer(function (answer) {
+			webRTC.peer.setLocalDescription(answer);
+			sendWebRTCDataToPeer({receiveAnswer: JSON.stringify(answer)});
+		});
+	}
+};
+
+function sendWebRTCDataToPeer (o) {
+	sendChannelData({Misc: WEBRTC_DATA_PREFIX + JSON.stringify(o)});
+}
+
+function setUpWebRTC (isInitiator) {
+	if (!webRTC.isSupported) {
+		return;
+	}
+
+	/* TODO: Set up Cyph STUN and TURN servers */
+	webRTC.peer	= new PeerConnection({
+		iceServers: [
+			{url: 'stun:23.21.150.121'},
+			{url: 'stun:stun.l.google.com:19302'},
+			{url: 'turn:numb.viagenie.ca', credential: 'webrtcdemo', username: 'louis%40mozilla.com'}
+		]
+	}, {
+		optional: [
+			{DtlsSrtpKeyAgreement: true},
+			{RtpDataChannels: true}
+		]
+	});
+
+	webRTC.peer.onicecandidate	= function (e) {
+		if (e.candidate) {
+			delete webRTC.peer.onicecandidate;
+			sendWebRTCDataToPeer({addIceCandidate: JSON.stringify(e.candidate)});
+		}
+	};
+
+	if (isInitiator !== false) {
+		webRTC.channel	= webRTC.peer.createDataChannel('subspace', {});
+
+		webRTC.bindChannelEvents();
+
+		webRTC.peer.createOffer(function (offer) {
+			webRTC.peer.setLocalDescription(offer);
+			sendWebRTCDataToPeer({receiveOffer: JSON.stringify(offer)});
+		});
+	}
 }
 
 
