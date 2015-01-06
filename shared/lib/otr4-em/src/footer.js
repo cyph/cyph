58c12b72c0697e41b700d0644e123f8bbd1878a8
Ryan Lester <hacker@linux.com>
2015-01-06 10:47:54 -0500

    ported to latest (LGPL!!) otr4-em

--- shared/lib/otr4-em/src/footer.js
+++
@@ -1,9 +0,0 @@
-}).call(moduleScope);
-
-    if (typeof exports !== 'undefined'){
-	    module.exports = moduleScope.Module;
-    }else{
-        root.libotr4Module = moduleScope.Module;
-    }
-
-}).call();
