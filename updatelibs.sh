c408293677c44989c890c918090c25ca46f693e8
Ryan Lester <hacker@linux.com>
2015-01-07 00:51:19 -0500

    ported cyph.im and cyph.com to newest version of Angular Material + lib update script

---
+++ updatelibs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+dir="$(pwd)"
+scriptdir="$(cd "$(dirname "$0")"; pwd)" # $(dirname `readlink -f "${0}" || realpath "${0}"`)
+cd "${scriptdir}"
+
+cd shared/lib
+
+rm -rf .oldbower
+mkdir .oldbower
+mv bower* .oldbower/
+
+bower install --save \
+	angular-material \
+	angular-markdown-directive \
+	angular-timer \
+	animate.css \
+	base64 \
+	jquery \
+	magnific-popup \
+	nanoscroller \
+	showdown \
+	unsemantic \
+	visibilityjs \
+	wow \
+	rubycon/isaac.js \
+	morr/jquery.appear \
+	mnaamani/otr4-em
+
+cd "${dir}"
