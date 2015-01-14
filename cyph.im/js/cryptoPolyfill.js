22ce92ffef224d983b3a428ed40afd6159768cd7
Ryan Lester <hacker@linux.com>
2015-01-13 19:16:10 -0500

    update to use URL fragment for creating and joining cyphs, to prevent cache misses

--- cyph.im/js/cryptoPolyfill.js
+++ cyph.im/js/cryptoPolyfill.js
@@ -1,5 +1,5 @@
 window.crypto	= window.crypto || window.msCrypto;
 
-if (!window.crypto || !window.Worker) {
+if (!window.crypto || !window.Worker || !history || !history.pushState || !history.replaceState) {
 	document.location.pathname	= '/unsupportedbrowser';
 }
