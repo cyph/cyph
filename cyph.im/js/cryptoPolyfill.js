f81e01d3b6ab490c85098fd4a5928b59ef625513
Ryan Lester <hacker@linux.com>
2015-01-15 02:53:28 -0500

    this pretty closely resembles what the final product will be

--- cyph.im/js/cryptoPolyfill.js
+++ cyph.im/js/cryptoPolyfill.js
@@ -1,5 +1,5 @@
 window.crypto	= window.crypto || window.msCrypto;
 
-if (!window.crypto || !window.Worker || !history || !history.pushState || !history.replaceState) {
+if (!window.crypto || !window.Worker || !history || !history.pushState || !history.replaceState || !localStorage) {
 	document.location.pathname	= '/unsupportedbrowser';
 }
