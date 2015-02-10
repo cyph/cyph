54816096969c3ed299c2d48139f097a296f6aa60
Ryan Lester <hacker@linux.com>
2015-02-10 00:17:59 -0500

    minor fix

--- shared/js/unsupportedbrowsers.js
+++ shared/js/unsupportedbrowsers.js
@@ -3,9 +3,9 @@
 
 var userAgent	= navigator.userAgent.toLowerCase();
 
-var isFirefoxOS		= userAgent.contains('firefox') && userAgent.contains('mobile') && !userAgent.contains('android');
+var isFirefoxOS		= userAgent.match('firefox') && userAgent.match('mobile') && !userAgent.match('android');
 var isOldIOS		= /ipad|iphone|ipod/.test(userAgent) && (parseInt((userAgent.match(/os (\d+)_(\d+)_?(\d+)?/) || [])[1], 10) || 0) < 7;
-var isStockAndroid	= userAgent.contains('android') && userAgent.match(/version\/\d\.\d/) != null;
+var isStockAndroid	= userAgent.match('android') && userAgent.match(/version\/\d\.\d/);
 
 if (isFirefoxOS || isOldIOS || isStockAndroid) {
 	document.location.pathname	= '/unsupportedbrowser';
