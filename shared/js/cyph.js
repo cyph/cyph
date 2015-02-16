db46660e9a6043976b7f563df261df700e55a87a
Ryan Lester <hacker@linux.com>
2015-02-15 23:15:23 -0500

    fixes

--- shared/js/cyph.js
+++ shared/js/cyph.js
@@ -186,7 +186,27 @@ function makeWorker (f) {
 	var s	= f.toString();
 	s		= s.slice(s.indexOf('{') + 1, s.lastIndexOf('}'));
 
-	return new Worker(window.URL.createObjectURL(new Blob([s], {type: 'text/javascript'})));
+	var blob, worker;
+
+	try {
+		blob	= new Blob([s], {type: 'application/javascript'});
+	}
+	catch (e) {
+		window.BlobBuilder = window.BlobBuilder || window.WebKitBlobBuilder || window.MozBlobBuilder;
+		blob	= new BlobBuilder();
+		blob.append(s);
+		blob	= blob.getBlob();
+	}
+
+	try {
+		worker	= new Worker(URL.createObjectURL(blob));
+	}
+	catch (e) {
+		worker	= new Worker('js/workerHelper.js');
+		worker.postMessage(s);
+	}
+
+	return worker;
 }
 
 function onTick (f) {
