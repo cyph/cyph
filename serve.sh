3da0e3cda2d846ba21f240079b704764491dcc92
Ryan Lester <hacker@linux.com>
2015-02-16 13:52:26 -0500

    PROPER LOCAL DEV ENVIRONMENT

--- serve.sh
+++ serve.sh
@@ -3,18 +3,11 @@
 dir="$(pwd)"
 cd $(cd "$(dirname "$0")"; pwd) # $(dirname `readlink -f "${0}" || realpath "${0}"`)
 
-ps ux | grep sass | awk '{print $2}' | xargs kill -9
-ps ux | grep dev_appserver | awk '{print $2}' | xargs kill -9
+ps ux | grep sass | grep -v grep | awk '{print $2}' | xargs kill -9
+ps ux | grep dev_appserver | grep -v grep | awk '{print $2}' | xargs kill -9
 
-# PORT=8080
-# IFS=\\ ; for cmd in `cat dispatch.yaml | tr '\n' ' ' | perl -pe 's/.*dispatch:(.*)/\1/' | perl -pe 's/ - url: "(.*?)\/\*".*?module: (.*?) /dev_appserver.py \@\@host \1.local \@\@admin_port ADMIN_PORT \@\@port PORT \2\/*.yaml\\\\/g' | tr '-' '.' | tr '@' '-'` ; do
-# 	ADMIN_PORT=$(($PORT+1))
-# 	PORT=$(($PORT+2))
-# 	bash -c "nohup `echo "$cmd" | sed "s/ADMIN_PORT/$ADMIN_PORT/" | sed "s/PORT/$PORT/"` &"
-# done
-
-sass --watch $1 &
 sass --watch shared &
-dev_appserver.py $1/*.yaml
+dev_appserver.py default/app.yaml cyph.com/cyph-com.yaml cyph.im/cyph-im.yaml cyph.me/cyph-me.yaml &
 
-cd "${dir}"
+cat # infinite sleep
+trap 'kill $(jobs -p)' EXIT
