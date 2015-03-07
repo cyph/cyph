a87af443c4d4f235d132ea6213e1defa16fcd337
Ryan Lester <hacker@linux.com>
2015-03-06 21:31:43 -0500

    cron job to clean up SQS queues

--- deploy.sh
+++ deploy.sh
@@ -143,6 +143,11 @@ if [ "${nobackend}" == '' ] ; then
 	git clone git://github.com/gorilla/mux.git
 	cd ../..
 
+	mkdir -p github.com/goamz
+	cd github.com/goamz
+	git clone git@github.com:goamz/goamz.git
+	cd ../..
+
 	# mkdir -p github.com/oschwald
 	# cd github.com/oschwald
 	# git clone git://github.com/oschwald/maxminddb-golang.git
