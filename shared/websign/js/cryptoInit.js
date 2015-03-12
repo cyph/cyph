6ecf052946a276980e74e7a4bd359d175dd59b22
Ryan Lester <hacker@linux.com>
2015-03-11 20:58:41 -0400

    minify websign dependencies

--- shared/websign/js/cryptoInit.js
+++ shared/websign/js/cryptoInit.js
@@ -1,13 +1,1 @@
-window.crypto	= window.crypto || window.msCrypto;
-
-if (
-	!window.crypto ||
-	!window.crypto.getRandomValues ||
-	!window.Worker ||
-	!window.history ||
-	!window.history.pushState ||
-	!window.history.replaceState ||
-	!window.localStorage
-) {
-	document.location.pathname	= '/unsupportedbrowser';
-}
+window.crypto=window.crypto||window.msCrypto;if(!window.crypto||!window.crypto.getRandomValues||!window.Worker||!window.history||!window.history.pushState||!window.history.replaceState||!window.localStorage){document.location.pathname="/unsupportedbrowser"}
--- shared/websign/js/workerHelper.js
+++ shared/websign/js/workerHelper.js
@@ -1,5 +1,1 @@
-/* http://stackoverflow.com/a/10372280/459881 */
-self.onmessage	= function(e) {
-	self.onmessage	= null;
-	eval(e.data);
-};
+self.onmessage=function(e){self.onmessage=null;eval(e.data)};
