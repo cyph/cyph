f5c9f46d6a2571ee260ea1332600f95a949822b4
Ryan Lester <hacker@linux.com>
2015-03-28 03:31:54 -0400

    reblocking master

--- deploy.sh
+++ deploy.sh
@@ -95,10 +95,10 @@ if [ $test ] ; then
 	# ls */*.yaml | xargs -I% sed -i.bak 's/version: prod/version: staging/g' %
 
 	# Disabling admin restriction on test environments for now
-	# for yaml in `ls */cyph*.yaml` ; do
-	# 	cat $yaml | perl -pe 's/(- url: .*)/\1\n  login: admin/g' > $yaml.new
-	# 	mv $yaml.new $yaml
-	# done
+	for yaml in `ls */cyph*.yaml` ; do
+		cat $yaml | perl -pe 's/(- url: .*)/\1\n  login: admin/g' > $yaml.new
+		mv $yaml.new $yaml
+	done
 else
 	ls */*.yaml | xargs -I% sed -i.bak 's/version: staging/version: prod/g' %
 
