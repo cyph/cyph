e2bb8beb13c6ccf45b889a402a5693e6854e44bb
Ryan Lester <hacker@linux.com>
2015-03-31 18:14:18 -0400

    final changes related to decontrolling compiled sass/typescript

--- git.sh
+++ git.sh
@@ -12,7 +12,7 @@ git push
 
 find . -name '*.go' -print0 | xargs -0 -I% gofmt -w "%"
 chmod -R 777 .
-git commit -a -m "gofmt, sass compile, lib updates: ${*}"
+git commit -a -m "gofmt: ${*}"
 git push
 
 cd "${dir}"
