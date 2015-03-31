bfaada84c9cbd8bbb5ec85d8536d9dc3c89acf68
Ryan Lester <hacker@linux.com>
2015-03-31 18:04:33 -0400

    move to TypeScript

--- serve.sh
+++ serve.sh
@@ -6,7 +6,8 @@ cd $(cd "$(dirname "$0")"; pwd) # $(dirname `readlink -f "${0}" || realpath "${0
 ps ux | grep sass | grep -v grep | awk '{print $2}' | xargs kill -9
 ps ux | grep dev_appserver | grep -v grep | awk '{print $2}' | xargs kill -9
 
-sass --watch shared &
+sass --watch shared/css &
+tsc --watch shared/js &
 fake_sqs &
 dev_appserver.py default/app.yaml cyph.com/cyph-com.yaml cyph.im/cyph-im.yaml cyph.me/cyph-me.yaml &
 
