467eca5759c8e8412f8f56f076889572ef0badf7
Ryan Lester <hacker@linux.com>
2015-03-31 19:49:54 -0400

    typescript headers for libs

---
+++ shared/lib/typings/tsd.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="angular-material/angular-material.d.ts" />
+/// <reference path="angularjs/angular-animate.d.ts" />
+/// <reference path="angularjs/angular.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
