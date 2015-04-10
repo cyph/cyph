b6beb1985f92b4760b74e5380b430edaaf433853
Ryan Lester <hacker@linux.com>
2015-04-10 14:28:10 -0400

    otr

--- shared/js/session/enums.ts
+++ shared/js/session/enums.ts
@@ -12,10 +12,24 @@ module Session {
 		public static beginChat: string			= 'beginChat';
 		public static channelRatchet: string	= 'channelRatchet';
 		public static closeChat: string			= 'closeChat';
+		public static cyphertext: string		= 'cyphertext';
 		public static destroy: string			= 'destroy';
 		public static mutex: string				= 'mutex';
+		public static otr: string				= 'otr';
+		public static p2p: string				= 'p2p';
 		public static text: string				= 'text';
 		public static typing: string			= 'typing';
-		public static p2p: string				= 'p2p';
+	}
+
+	export enum OTREvents {
+		abort,
+		authenticated,
+		begin,
+		receive,
+		send
+	}
+
+	export enum P2PEvents {
+
 	}
 }
