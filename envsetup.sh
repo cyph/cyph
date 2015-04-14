13429a148a4dc8b7144d4423bc43ecf94615e45b
Ryan Lester <hacker@linux.com>
2015-04-14 02:38:49 -0400

    local environment dependencies + no bat + 700

---
+++ envsetup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo gem install sass specific_install
+sudo gem specific_install -l https://github.com/buu700/fake_sqs
+npm -g install html-minifier clean-css uglifyjs typescript tsd bower
+pip install beautifulsoup4 html5lib
