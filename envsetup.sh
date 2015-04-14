13fb0a8d1635d72cc0529af98b7e0dbebf6f8ba9
Ryan Lester <hacker@linux.com>
2015-04-14 16:48:34 -0400

    envsetup

--- envsetup.sh
+++ envsetup.sh
@@ -2,5 +2,5 @@
 
 sudo gem install sass specific_install
 sudo gem specific_install -l https://github.com/buu700/fake_sqs
-npm -g install html-minifier clean-css uglifyjs typescript tsd bower
+npm -g install html-minifier clean-css uglifyjs typescript tsd bower browserstack
 pip install beautifulsoup4 html5lib
