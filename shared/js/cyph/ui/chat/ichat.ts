3b87dcbfd9e8756fdb9c42d027c6e625db451de2
Ryan Lester <hacker@linux.com>
2015-04-18 19:37:26 -0400

    ichat

--- shared/js/cyph/ui/chat/ichat.ts
+++ shared/js/cyph/ui/chat/ichat.ts
@@ -1,10 +1,65 @@
+/// <reference path="cyphertext.ts" />
+/// <reference path="enums.ts" />
+/// <reference path="p2pmanager.ts" />
+/// <reference path="photomanager.ts" />
+/// <reference path="../../icontroller.ts" />
+/// <reference path="../../session/enums.ts" />
+/// <reference path="../../session/isession.ts" />
 /// <reference path="../../../global/base.ts" />
 
 
 module Cyph {
 	export module UI {
 		export module Chat {
-			export interface IChat {}
+			export interface IChat {
+				isConnected: boolean;
+
+				isDisconnected: boolean;
+
+				isFriendTyping: boolean;
+
+				unreadMessages: number;
+
+				currentMessage: string;
+
+				state: States;
+
+				messages: {author: Session.Authors; text: string;}[];
+
+				cyphertext: Cyphertext;
+
+				photoManager: PhotoManager;
+
+				p2pManager: P2PManager;
+
+				session: Session.ISession;
+
+				abortSetup () : void;
+
+				addMessage (
+					text: string,
+					author: Session.Authors,
+					shouldNotify?: boolean
+				) : void;
+
+				changeState (state: States) : void;
+
+				begin (callback?: Function) : void;
+
+				close () : void;
+
+				disconnectButton () : void;
+
+				formattingHelpButton () : void;
+
+				messageChange () : void;
+
+				send (message?: string) : void;
+
+				setConnected () : void;
+
+				setFriendTyping (isFriendTyping: boolean) : void;
+			}
 		}
 	}
 }
