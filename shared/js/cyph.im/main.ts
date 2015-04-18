999ee9c44679ff279a02f743f01372982ceee253
Ryan Lester <hacker@linux.com>
2015-04-18 15:56:04 -0400

    function

--- shared/js/cyph.im/main.ts
+++ shared/js/cyph.im/main.ts
@@ -21,44 +21,42 @@ angular.
 		'$mdToast',
 		'$mdDialog',
 
-		($scope, $mdSidenav, $mdToast, $mdDialog) => {
-			$(() => {
-				Cyph.UI.Elements.load();
+		($scope, $mdSidenav, $mdToast, $mdDialog) => $(() => {
+			Cyph.UI.Elements.load();
 
 
-				let controller: Cyph.IController	= {
-					update: () : void => {
-						let phase: string	= $scope.$root.$$phase;
+			let controller: Cyph.IController	= {
+				update: () : void => {
+					let phase: string	= $scope.$root.$$phase;
 
-						if (phase !== '$apply' && phase !== '$digest') {
-							$scope.$apply();
-						}
+					if (phase !== '$apply' && phase !== '$digest') {
+						$scope.$apply();
 					}
-				};
+				}
+			};
 
-				let mobileMenu: Cyph.UI.ISidebar	= Cyph.Env.isMobile ?
-					$mdSidenav('menu') :
-					{
-						close: () => {},
-						open: () => {}
-					}
-				;
+			let mobileMenu: Cyph.UI.ISidebar	= Cyph.Env.isMobile ?
+				$mdSidenav('menu') :
+				{
+					close: () => {},
+					open: () => {}
+				}
+			;
 
-				let dialogManager: Cyph.UI.IDialogManager	= new Cyph.UI.DialogManager($mdDialog, $mdToast);
-				let notifier: Cyph.UI.INotifier				= new Cyph.UI.Notifier;
-
-
-				$scope.Cyph			= Cyph;
-				$scope.Cyph.UI		= Cyph.UI;
-				$scope.Cyph.im		= Cyph.im;
-				$scope.Cyph.im.UI	= Cyph.im.UI;
-				$scope.ui			= new Cyph.im.UI.UI(controller, dialogManager, mobileMenu, notifier);
+			let dialogManager: Cyph.UI.IDialogManager	= new Cyph.UI.DialogManager($mdDialog, $mdToast);
+			let notifier: Cyph.UI.INotifier				= new Cyph.UI.Notifier;
 
 
-				history.pushState({}, '', location.pathname);
-				self.onhashchange = () => location.reload();
-			});
-		}
+			$scope.Cyph			= Cyph;
+			$scope.Cyph.UI		= Cyph.UI;
+			$scope.Cyph.im		= Cyph.im;
+			$scope.Cyph.im.UI	= Cyph.im.UI;
+			$scope.ui			= new Cyph.im.UI.UI(controller, dialogManager, mobileMenu, notifier);
+
+
+			history.pushState({}, '', location.pathname);
+			self.onhashchange = () => location.reload();
+		})
 	]).
 	config([
 		'$compileProvider',
