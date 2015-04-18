6aad4743fad38951bdb0a5d99c02a2b745c9d58d
Ryan Lester <hacker@linux.com>
2015-04-17 23:06:07 -0400

    photo manager

--- shared/js/cyph/ui/chat/photomanager.ts
+++ shared/js/cyph/ui/chat/photomanager.ts
@@ -93,7 +93,7 @@ module Cyph {
 
 									let finish: Function;
 
-									let intervalId	= setInterval(() => {
+									let intervalId: number	= setInterval(() => {
 										if (Util.getValue(elem, 'files', []).length > 0) {
 											finish();
 										}
