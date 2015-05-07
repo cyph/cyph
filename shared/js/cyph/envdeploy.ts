a1b7ff341fcb71e2d225d5874659d733def66466
Ryan Lester <hacker@linux.com>
2015-05-07 03:17:30 +0000

    quotes

--- shared/js/cyph/envdeploy.ts
+++ shared/js/cyph/envdeploy.ts
@@ -12,25 +12,25 @@ module Cyph {
 			!!location && location.host.split('.').slice(-1)[0] === 'onion'
 		;
 
-		/** URL for backend API (https://api.cyph.com/ or equivalent). */
+		/** URL for backend API ("https://api.cyph.com/" or equivalent). */
 		public static baseUrl: string		= EnvDeploy.isOnion ?
 			'/api/' :
 			`${location.protocol}//${location.hostname}:42000/`
 		;
 
-		/** URL for Cyph website (https://www.cyph.com/ or equivalent). */
+		/** URL for Cyph website ("https://www.cyph.com/" or equivalent). */
 		public static homeUrl: string		= EnvDeploy.isOnion ?
 			'/' :
 			`${location.protocol}//${location.hostname}:42001/`
 		;
 
-		/** URL for starting a new cyph (https://www.cyph.im/ or equivalent). */
+		/** URL for starting a new cyph ("https://www.cyph.im/" or equivalent). */
 		public static newCyphUrl: string	= EnvDeploy.isOnion ?
 			'/im/' :
 			`${location.protocol}//${location.hostname}:42002/`
 		;
 
-		/** URL for Cyph account application (https://www.cyph.me/ or equivalent). */
+		/** URL for Cyph account application ("https://www.cyph.me/" or equivalent). */
 		public static cyphMeUrl: string		= EnvDeploy.isOnion ?
 			'/me/' :
 			`${location.protocol}//${location.hostname}:42003/`
