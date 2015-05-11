3c07263d30fcfe786d4016e1372e04b1a2165dcd
Ryan Lester <hacker@linux.com>
2015-05-11 18:44:59 +0000

    more onion-related fixes

--- shared/js/cyph/envdeploy.ts
+++ shared/js/cyph/envdeploy.ts
@@ -5,39 +5,42 @@ module Cyph {
 	 */
 	export class EnvDeploy {
 		/** Indicates whether this is local dev environment. */
-		public static isLocalEnv: boolean	= true;
+		public static isLocalEnv: boolean		= true;
 
 		/** Indicates whether this is our Tor site. */
-		public static isOnion: boolean		=
+		public static isOnion: boolean			=
 			!!location && location.host.split('.').slice(-1)[0] === 'onion'
 		;
 
 		/** URL for backend API ("https://api.cyph.com/" or equivalent). */
-		public static baseUrl: string		= EnvDeploy.isOnion ?
+		public static baseUrl: string			= EnvDeploy.isOnion ?
 			'/api/' :
 			`${location.protocol}//${location.hostname}:42000/`
 		;
 
 		/** URL for Cyph website ("https://www.cyph.com/" or equivalent). */
-		public static homeUrl: string		= EnvDeploy.isOnion ?
+		public static homeUrl: string			= EnvDeploy.isOnion ?
 			'/' :
 			`${location.protocol}//${location.hostname}:42001/`
 		;
 
-		/** URL for starting a new cyph ("https://www.cyph.im/" or equivalent). */
-		public static newCyphUrl: string	= EnvDeploy.isOnion ?
-			`${Config.onionUrl}im/` :
-			`${location.protocol}//${location.hostname}:42002/`
+		/** Base URL for a new cyph link ("https://www.cyph.im/" or equivalent). */
+		public static newCyphBaseUrl: string	= `${location.protocol}//${location.hostname}:42002/`;
+
+		/** URL for starting a new cyph (same as newCyphBaseUrl except on Onion site). */
+		public static newCyphUrl: string		= EnvDeploy.isOnion ?
+			`${Config.onionUrl}im/#new` :
+			EnvDeploy.newCyphBaseUrl
 		;
 
 		/** URL for Cyph account application ("https://www.cyph.me/" or equivalent). */
-		public static cyphMeUrl: string		= EnvDeploy.isOnion ?
+		public static cyphMeUrl: string			= EnvDeploy.isOnion ?
 			`${Config.onionUrl}me/` :
 			`${location.protocol}//${location.hostname}:42003/`
 		;
 
 		/** Correct endpoint for Fake SQS in local environments
 			(replaces Config.awsEndpointFake). */
-		public static awsEndpoint: string	= `${location.protocol}//${location.hostname}:43000`;
+		public static awsEndpoint: string		= `${location.protocol}//${location.hostname}:43000`;
 	}
 }
