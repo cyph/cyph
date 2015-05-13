38a35bd0f1387f0f06ee6d1519ca9c51b5036390
Ryan Lester <hacker@linux.com>
2015-05-13 19:04:27 +0000

    setup script for SD card environments

---
+++ serverconfig/envsetup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+
+sudo apt-get update
+sudo apt-get dist-upgrade -y
+
+sudo apt-get install -y curl wget build-essential git gnupg golang ruby nodejs npm linux-firmware-nonfree gimp chromium-browser
+
+sudo su -c 'wget -qO- https://get.docker.com/ | sh'
+
+sudo gem install sass
+
+sudo npm -g install typescript
+
+wget "$(curl -s 'http://www.sublimetext.com/3' | grep -o 'http.*amd64\.deb')" -O sublime.deb
+sudo dpkg -i sublime.deb
+rm sublime.deb
+
+curl -s 'https://packagecontrol.io/installation' | \
+	tr '\n' ' ' | \
+	perl -pe 's/.*<p class="code st3">\s+<code>\s+(.*?)\s+<\/code>\s+<\/p>.*/\1/g' \
+	> balls
+subl balls
+sleep 1
+rm balls
