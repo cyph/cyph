a0c9ae791871e6aeea74ea09f2e93667c9f33699
Ryan Lester <hacker@linux.com>
2015-05-29 13:44:51 -0400

    merge

--- shared/js/cyph.im/config.ts
+++ shared/js/cyph.im/config.ts
@@ -8,7 +8,8 @@ module Cyph.im {
 
 		/** Known good hashes of cyph.im's WebSign bootstrap. */
 		public static webSignHashes	= {
-			'fd5a7adad3db30942214a6ad564d153bdde504c42b5f1fed5b37c9e8fe2e6952': true
+			'fd5a7adad3db30942214a6ad564d153bdde504c42b5f1fed5b37c9e8fe2e6952': true,
+			'28fc6b09f3b33ef66fc7d7b4ba67ed77b263abafc07508bc28f46a5d09a81e90': true
 		};
 	}
 }
