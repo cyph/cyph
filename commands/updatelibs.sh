7324cc7f4ee3a78573234c37cb4f7a36d01853c7
Ryan Lester <hacker@linux.com>
2015-05-31 22:23:28 +0000

    updatelibs (Sodium, no OTR)

--- commands/updatelibs.sh
+++ commands/updatelibs.sh
@@ -56,10 +56,10 @@ tsd query --resolve --overwrite --save --action install \
 cd ../cryptolib
 
 bower install --save \
-	mnaamani/otr4-em \
+	libsodium.js \
 	rubycon/isaac.js
 
-wget http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/hmac-sha256.js
+wget https://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/hmac-sha256.js
 
 cd ..
 chmod -R 700 .
