662565d6d1c0af66884da0c98a498ae1ba056e67
Ryan Lester <hacker@linux.com>
2015-06-23 04:39:49 +0000

    fork cyph.im -> cyph.video

---
+++ shared/js/cyph.video/config.ts
@@ -0,0 +1,9 @@
+module Cyph.im {
+	/**
+	 * Static/constant configuration values.
+	 */
+	export class Config {
+		/** Number of seconds before new cyph wait screen will abort. */
+		public static newCyphCountdown: number	= 600;
+	}
+}
---
+++ shared/js/cyph.video/ui/enums.ts
@@ -0,0 +1,15 @@
+module Cyph.im {
+	export module UI {
+		/**
+		 * Possible states of cyph.im UI.
+		 */
+		export enum States {
+			none,
+			blank,
+			chat,
+			error,
+			spinningUp,
+			waitingForFriend
+		}
+	}
+}
