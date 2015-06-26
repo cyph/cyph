8fb2f42eb0dff08bbe1248ef4e11761b1ac9d7db
Ryan Lester <hacker@linux.com>
2015-06-26 01:48:09 +0000

    if this doesn't work I'm dropping support for Firefox

--- shared/js/cyph/session/threadedsession.ts
+++ shared/js/cyph/session/threadedsession.ts
@@ -95,13 +95,13 @@ module Cyph {
 				);
 
 				this.thread	= new Thread((vars: any, importScripts: Function, Cyph: any) => {
-					let sodium: any;
 					importScripts('/cryptolib/bower_components/libsodium.js/dist/browsers/combined/sodium.min.js');
 					self['sodium']	= sodium || self['sodium'];
+					self['_sodium']	= self['sodium'];
 
-					let ntru: any;
 					importScripts('/cryptolib/bower_components/ntru.js/dist/ntru.js');
 					self['ntru']	= ntru || self['ntru'];
+					self['_ntru']	= self['ntru'];
 
 					importScripts('/lib/bower_components/aws-sdk-js/dist/aws-sdk.min.js');
 					importScripts('/lib/aws-xml.js');
