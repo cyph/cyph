d5ed9aacf8b19516eed8c17993b1ab121065442b
Ryan Lester <hacker@linux.com>
2015-06-29 20:53:42 +0000

    copy latest version of Mario's code (+ add TS types) (#43)

--- shared/js/preload/dompurify.ts
+++ shared/js/preload/dompurify.ts
@@ -3,34 +3,40 @@
 /* Copied from https://github.com/cure53/DOMPurify/blob/master/demos/hooks-scheme-whitelist.html */
 
 // allowed URI schemes
-var whitelist = ['http', 'https', 'ftp'];
-            
-// build fitting regex
-var regex = RegExp('^(' + whitelist.join('|') + '):', 'gim');
-            
-// Add a hook to enforce URI scheme whitelist
-DOMPurify.addHook('afterSanitizeAttributes', function (node) {
-    // check all href attributes for validity
-    if (node.hasAttribute('href')) {
-        var href = node.getAttribute('href');
-        if (!href.match(regex)) {
-            node.removeAttribute('href');
-        }
-    }
-    // check all action attributes for validity
-    if (node.hasAttribute('action')) {
-        var action = node.getAttribute('action');
-        if (!action.match(regex)) {
-            node.removeAttribute('action');
-        }
-    }
-    // check all xlink:href attributes validity
-    if (node.hasAttribute('xlink:href')) {
-        var xhref = node.getAttribute('xlink:href');
-        if (!xhref.match(regex)) {
-            node.removeAttribute('xlink:href');
-        }
-    }
+const whitelist: string[]	= ['http', 'https', 'ftp'];
 
-    return node;
+// build fitting regex
+const regex: RegExp			= RegExp('^(' + whitelist.join('|') + '):', 'gim');
+
+// Add a hook to enforce URI scheme whitelist
+DOMPurify.addHook('afterSanitizeAttributes', (node: HTMLElement) => {
+
+	// build an anchor to map URLs to
+	const anchor: HTMLAnchorElement	= document.createElement('a');
+
+	// check all href attributes for validity
+	if (node.hasAttribute('href')) {
+		anchor.href	= node.getAttribute('href');
+		if (!anchor.protocol.match(regex)) {
+			node.removeAttribute('href');
+		}
+	}
+
+	// check all action attributes for validity
+	if (node.hasAttribute('action')) {
+		anchor.href	= node.getAttribute('action');
+		if (!anchor.protocol.match(regex)) {
+			node.removeAttribute('action');
+		}
+	}
+
+	// check all xlink:href attributes for validity
+	if (node.hasAttribute('xlink:href')) {
+		anchor.href	= node.getAttribute('xlink:href');
+		if (!anchor.protocol.match(regex)) {
+			node.removeAttribute('xlink:href');
+		}
+	}
+
+	return node;
 });
