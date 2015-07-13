7b39b802c92efd8b4b311f0eb43d1d934f100036
Ryan Lester <hacker@linux.com>
2015-07-13 21:47:28 +0000

    compile errors resolved

--- shared/js/cyph.com/main.ts
+++ shared/js/cyph.com/main.ts
@@ -6,7 +6,10 @@
 /// <reference path="../preload/base.ts" />
 
 /// <reference path="../cyph/controller.ts" />
+/// <reference path="../cyph/ui/dialogmanager.ts" />
 /// <reference path="../cyph/ui/signupform.ts" />
+/// <reference path="../cyph/ui/directives/chat.ts" />
+/// <reference path="../cyph/ui/directives/markdown.ts" />
 /// <reference path="../cyph/ui/directives/signupform.ts" />
 /// <reference path="ui/enums.ts" />
 /// <reference path="ui/elements.ts" />
