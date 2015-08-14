6cbb7a844e6d7b42d43dc8fc31c0a94a3b2d1fc5
Ryan Lester <hacker@linux.com>
2015-08-14 05:14:18 +0000

    build, docs, and Docker fixes

--- commands/docs.sh
+++ commands/docs.sh
@@ -8,17 +8,10 @@ cd $(cd "$(dirname "$0")"; pwd)/..
 cp -rf shared/js shared/.js.tmp
 cd shared/.js.tmp
 
-# TypeScript 1.4 workarounds
-for file in $(find . -name '*.ts') ; do
-	cat $file | perl -pe 's/(^[^*]+)\sof\s/\1 in /g' | perl -pe 's/const\s/let /g' > $file.tmp
-	mv $file.tmp $file
-done
-echo 'var crypto: Crypto;' >> global/base.ts
-
 rm -rf ../js/docs
 
 # NOTE: "--mode file" is a workaround for a typedoc bug; should be "--mode modules"
-typedoc -t ES6 --out ../js/docs --name Cyph --mode file --includeDeclarations --excludeExternals .
+typedoc --out ../js/docs --name Cyph --mode file --includeDeclarations --excludeExternals .
 
 cd ..
 rm -rf .js.tmp
