9b604df6d93df26f41673ec06b81495e565056c0
Ryan Lester <hacker@linux.com>
2015-08-30 06:26:11 +0000

    content sizing fix

--- shared/js/cyph.com/ui/ui.ts
+++ shared/js/cyph.com/ui/ui.ts
@@ -211,7 +211,7 @@ module Cyph.com {
 							const $this: JQuery	= $(elem);
 
 							$this.width(
-								$this.text().
+								$this[0].innerText.
 									split('\n').
 									map((s: string) => (s.match(/[A-Za-z0-9]/g) || []).length).
 									reduce((a: number, b: number) => Math.max(a, b))
