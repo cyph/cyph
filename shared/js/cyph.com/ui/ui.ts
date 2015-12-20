fa52c2c201e32ff1ee174f0304bdabf0b0862da2
Ryan Lester <hacker@linux.com>
2015-12-20 04:16:44 +0000

    final cleanup/fixes/revisions to donate page and FAQ

--- shared/js/cyph.com/ui/ui.ts
+++ shared/js/cyph.com/ui/ui.ts
@@ -5,13 +5,13 @@ module Cyph.com {
 		 */
 		export class UI extends Cyph.UI.BaseButtonManager {
 			/** UI state/view. */
-			public state: States		= States.home;
+			public state: States			= States.home;
 
 			/** Podcast promo page state/view. */
-			public podcast: Podcasts	= Podcasts.none;
+			public podcast: Podcasts		= Podcasts.none;
 
 			/** Donation amount in dollars (default). */
-			public donationAmount	= 10;
+			public donationAmount: number	= 10;
 			
 			/** Home page state/view. */
 			public homeSection: HomeSections;
