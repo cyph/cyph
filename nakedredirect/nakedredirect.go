195b4d116835de971be7dfb1ea693ee573f86510
Ryan Lester <hacker@linux.com>
2016-01-05 00:09:31 +0000

    gofmt: handling naked domain redirect in GAE instead of CloudFlare

--- nakedredirect/nakedredirect.go
+++ nakedredirect/nakedredirect.go
@@ -2,7 +2,7 @@ package nakedredirect
 
 import (
 	"net/http"
-    "strings"
+	"strings"
 )
 
 func init() {
@@ -11,6 +11,6 @@ func init() {
 		w.Header().Set("Public-Key-Pins", "max-age=31536000; includeSubdomains; pin-sha256=\"8jdS3zcG5kUApHWDrLH5Q8wEygqGbGEhYApjSDtufBU=\"; pin-sha256=\"AMRT67hN1KPI+u7Aw9JpZlzyRaKeO+6u2H+jtOmWVy8=\"")
 		w.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubdomains; preload")
 
-		http.Redirect(w, r, "https://www.cyph.com" + strings.SplitAfterN(r.URL.String(), "cyph.com", 2)[1], 301)
+		http.Redirect(w, r, "https://www.cyph.com"+strings.SplitAfterN(r.URL.String(), "cyph.com", 2)[1], 301)
 	})
 }
