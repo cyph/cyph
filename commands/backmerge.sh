4048fb861ef0b5f04d2a1292ca9cbf1b78813d6d
Ryan Lester <hacker@linux.com>
2016-02-25 01:26:36 -0800

    fixes for merge scripts

--- commands/backmerge.sh
+++ commands/backmerge.sh
@@ -6,10 +6,11 @@ dir="$(pwd)"
 cd $(cd "$(dirname "$0")"; pwd)/..
 
 merge () {
+	git checkout $2$(if [ $3 ] ; then echo / ; fi)$3
 	git pull
 	git merge $1
 	git commit -a -m merge
-	git push
+	git push $(if [ $3 ] ; then echo $2 $3 ; fi)
 }
 
 branch="$(git branch | awk '/^\*/{print $2}')"
@@ -18,11 +19,13 @@ git remote add internal git@github.com:cyph/internal.git
 git fetch --all
 git checkout internal/prod
 git pull
-git checkout internal/master
-merge internal/prod
-git checkout origin/master
-merge internal/master
-git checkout $branch
-merge master
+merge internal/prod internal master
+merge internal/master master
+
+if [ "$branch" -ne master -a "$branch" -ne prod ] ; then
+	merge master $branch
+else
+	git checkout $branch
+fi
 
 cd "${dir}"
--- commands/prodmerge.sh
+++ commands/prodmerge.sh
@@ -6,10 +6,11 @@ dir="$(pwd)"
 cd $(cd "$(dirname "$0")"; pwd)/..
 
 merge () {
+	git checkout $2$(if [ $3 ] ; then echo / ; fi)$3
 	git pull
 	git merge $1
 	git commit -a -m merge
-	git push
+	git push $(if [ $3 ] ; then echo $2 $3 ; fi)
 }
 
 branch="$(git branch | awk '/^\*/{print $2}')"
@@ -17,12 +18,10 @@ branch="$(git branch | awk '/^\*/{print $2}')"
 git remote add internal git@github.com:cyph/internal.git
 git remote add public git@github.com:cyph/cyph.git
 git fetch --all
-git checkout internal/prod
-merge internal/master
+merge internal/master internal prod
 git push public master
-git checkout internal/master
-merge internal/prod
+merge internal/prod internal master
 
-git checkout origin/$branch
+git checkout $branch
 
 cd "${dir}"
