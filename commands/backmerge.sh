94391a1d0f601e2ffe36309781130aaede32f037
Ryan Lester <hacker@linux.com>
2016-02-25 01:59:38 -0800

    fixes for merge scripts

--- commands/backmerge.sh
+++ commands/backmerge.sh
@@ -5,17 +5,7 @@ source ~/.bashrc
 dir="$(pwd)"
 cd $(cd "$(dirname "$0")"; pwd)/..
 
-merge () {
-	source="$1"
-	targetA="$2$(if [ $3 ] ; then echo / ; fi)$3"
-	targetB="$(if [ $3 ] ; then echo $2 $3 ; fi)"
-
-	git checkout $targetA
-	git pull $targetB
-	git merge $source
-	git commit -a -m merge
-	git push $targetB
-}
+git checkout $target
 
 branch="$(git branch | awk '/^\*/{print $2}')"
 
@@ -23,7 +13,7 @@ git remote add internal git@github.com:cyph/internal.git
 git fetch --all
 git checkout internal/prod
 git pull internal prod
-merge internal/prod internal master
+merge internal/prod internal/master
 merge internal/master master
 
 if [ "$branch" -ne master -a "$branch" -ne prod ] ; then
