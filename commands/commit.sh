126e05583a0a6fe3eeae883cfe58594c84c94532
Ryan Lester <hacker@linux.com>
2016-03-18 20:53:13 -0400

    added automated cleanup/beautification to commit script

--- commands/commit.sh
+++ commands/commit.sh
@@ -8,12 +8,29 @@ cd $(cd "$(dirname "$0")"; pwd)/..
 rm .git/index.lock 2> /dev/null
 
 git pull
+chmod -R 700 .
 git add .
 git commit -a -m "${*}"
 git push
 
+# Automated cleanup and beautification
+
 find . -name '*.go' | grep -v github.com | xargs -I% gofmt -w "%"
-git commit -a -m "gofmt: ${*}"
+
+ls shared/css/*.scss | xargs -I% bash -c '
+sass-convert --from scss --to scss --dasherize --indent t % | awk "{
+if (\$1 != \"/*\")
+	gsub(/\"/, \"'"'"'\", \$0)
+print \$0
+}" > %.new
+mv %.new %
+'
+
+find shared/js -name '*.ts' -print0 |
+	xargs -0 -I% bash -c "cat % | perl -pe 's/\\s+$/\n/g' | perl -pe 's/    /\\t/g' > %.new ; mv %.new %"
+
+chmod -R 700 .
+git commit -a -m "cleanup: ${*}"
 git push
 
 cd "${dir}"
