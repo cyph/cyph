9fa5f73d30e8faaffa70cc7c047493e97ecde868
Ryan Lester <hacker@linux.com>
2016-05-02 04:30:07 +0000

    Authboss ClientStorer implementation

---
+++ default/client_storer.go
@@ -0,0 +1,54 @@
+package api
+
+import (
+	"github.com/go-authboss/authboss"
+	"github.com/gorilla/securecookie"
+	"net/http"
+)
+
+var cookieStore *securecookie.SecureCookie
+
+type CookieStorer struct {
+	Request *http.Request
+	Writer  http.ResponseWriter
+}
+
+func NewCookieStorer(r *http.Request, w http.ResponseWriter) authboss.ClientStorer {
+	return &CookieStorer{r, w}
+}
+
+func (s CookieStorer) Get(key string) (string, bool) {
+	cookie, err := s.Request.Cookie(key)
+	if err != nil {
+		return "", false
+	}
+
+	var value string
+	err = cookieStore.Decode(key, cookie.Value, &value)
+	if err != nil {
+		return "", false
+	}
+
+	return value, true
+}
+
+func (s CookieStorer) Put(key, value string) {
+	encoded, _ := cookieStore.Encode(key, value)
+
+	cookie := &http.Cookie{
+		Expires: time.Now().UTC().AddDate(1, 0, 0),
+		Name:    key,
+		Value:   encoded,
+		Path:    "/",
+	}
+	http.SetCookie(s.Writer, cookie)
+}
+
+func (s CookieStorer) Del(key string) {
+	cookie := &http.Cookie{
+		MaxAge: -1,
+		Name:   key,
+		Path:   "/",
+	}
+	http.SetCookie(s.Writer, cookie)
+}
