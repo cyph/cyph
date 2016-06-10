87cd93a4b4d3d1f68f5c5b7407c044e0c8ebbe52
Ryan Lester <hacker@linux.com>
2016-06-10 00:51:33 -0400

    jquery fix

--- commands/updatelibs.sh
+++ commands/updatelibs.sh
@@ -107,7 +107,7 @@ jspm install -y \
 	github:andyet/simplewebrtc \
 	npm:animate.css \
 	github:davidchambers/base64.js \
-	jquery \
+	jquery=github:jquery/jquery \
 	jquery-legacy=github:jquery/jquery@^1 \
 	npm:magnific-popup \
 	npm:nanoscroller \
