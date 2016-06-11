b89472394489c4bb4433bfcbfdfb02cd94d1c2bd
Ryan Lester <hacker@linux.com>
2016-06-11 01:35:43 +0000

    updatelibs

--- shared/js/config.js
+++ shared/js/config.js
@@ -24,7 +24,7 @@ System.config({
     "crypto/supersphincs": "github:cyph/supersphincs.js@master",
     "davidchambers/base64.js": "github:davidchambers/base64.js@0.3.0",
     "dompurify": "npm:dompurify@0.8.2",
-    "jquery": "github:jquery/jquery@2.2.4",
+    "jquery": "npm:jquery@2.2.4",
     "jquery-legacy": "github:jquery/jquery@1.12.4",
     "julianlam/tabIndent.js": "github:julianlam/tabIndent.js@master",
     "magnific-popup": "npm:magnific-popup@1.1.0",
@@ -160,15 +160,12 @@ System.config({
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
     },
-    "npm:jquery@3.0.0": {
-      "process": "github:jspm/nodelibs-process@0.1.2"
-    },
     "npm:magnific-popup@1.1.0": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:nanoscroller@0.8.7": {
       "css": "npm:css@2.2.1",
-      "jquery": "npm:jquery@3.0.0"
+      "jquery": "npm:jquery@2.2.4"
     },
     "npm:os-browserify@0.1.2": {
       "os": "github:jspm/nodelibs-os@0.1.0"
