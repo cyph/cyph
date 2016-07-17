29ec552245b5a5e084b1253b36ec189c3ac8e51d
Ryan Lester <hacker@linux.com>
2016-07-17 18:07:14 -0400

    ServiceWorker + AppCache cleanup and fixes

--- shared/websign/serviceworker.js
+++ shared/websign/serviceworker.js
@@ -4,59 +4,81 @@ var files	= [
 	'websign/appcache.appcache',
 	'websign/manifest.json',
 	'serviceworker.js',
-	'unsupportedbrowser'
+	'unsupportedbrowser',
+	'img/metaimage.png',
+	'favicon.ico',
+	'img/favicon/favicon-192x192.png',
+	'img/favicon/favicon-160x160.png',
+	'img/favicon/favicon-96x96.png',
+	'img/favicon/favicon-32x32.png',
+	'img/favicon/favicon-16x16.png',
+	'img/favicon/apple-touch-icon-180x180.png',
+	'img/favicon/apple-touch-icon-152x152.png',
+	'img/favicon/apple-touch-icon-144x144.png',
+	'img/favicon/apple-touch-icon-120x120.png',
+	'img/favicon/apple-touch-icon-114x114.png',
+	'img/favicon/apple-touch-icon-76x76.png',
+	'img/favicon/apple-touch-icon-72x72.png',
+	'img/favicon/apple-touch-icon-60x60.png',
+	'img/favicon/apple-touch-icon-57x57.png',
+	'img/favicon/mstile-144x144.png'
 ].map(function (file) {
 	return new Request(file);
 });
 
 var root	= files[0].url.replace(/(.*)\/$/, '$1');
 
-function openCache (callback) {
-	caches.open('cache').then(callback);
-}
 
+self.addEventListener('install', function () {
+	Promise.all([
+		caches.open('cache'),
+		Promise.all(files.map(function (file) {
+			return fetch(file, {credentials: 'include'});
+		}))
+	]).then(function (results) {
+		var cache		= results[0];
+		var responses	= results[1];
 
-self.addEventListener('install', function (e) {
-	try {
-		openCache(function (cache) {
-			files.forEach(function (file) {
-				fetch(file).then(function (response) {
-					cache.put(file, response);
-				});
-			});
-		});
-	}
-	catch (_) {}
+		for (var i = 0 ; i < responses.length ; ++i) {
+			cache.put(files[i], responses[i]);
+		}
+	});
 });
 
 self.addEventListener('fetch', function (e) {
-	for (var i = 0 ; i < files.length ; ++i) {
-		if (e.request.url === files[i].url) {
-			return e.respondWith(
-				caches.match(e.request).then(function (response) {
-					if (response) {
-						return response;
-					}
-					else {
-						return fetch(e.request.clone()).then(function (response) {
-							var responseToCache	= response.clone();
-
-							openCache(function (cache) {
-								cache.put(e.request, responseToCache);
-							});
-
-							return response;
-						});
-					}
-				})
-			);
-		}
+	/* Let requests to other origins pass through */
+	if (e.request.url.indexOf(root) !== 0) {
+		return;
 	}
 
 	/* Block non-whitelisted paths in this origin */
-	if (e.request.url.indexOf(root) === 0) {
+	if (
+		files.filter(function (file) {
+			return e.request.url === file.url;
+		}).length < 1
+	) {
 		return e.respondWith(new Response('', {status: 404}));
 	}
+
+	return e.respondWith(
+		caches.match(e.request).then(function (cachedResponse) {
+			if (cachedResponse) {
+				return cachedResponse;
+			}
+
+			return Promise.all([
+				caches.open('cache'),
+				fetch(e.request.clone())
+			]).then(function (results) {
+				var cache		= results[0];
+				var response	= results[1];
+
+				cache.put(e.request, response.clone());
+
+				return response;
+			});
+		})
+	);
 });
 
 self.addEventListener('notificationclick', function (e) {
