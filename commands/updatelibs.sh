e21af75307b424555ff115cd698eacfdf05fd181
Ryan Lester <hacker@linux.com>
2016-09-28 21:44:24 -0400

    minor fix

--- commands/updatelibs.sh
+++ commands/updatelibs.sh
@@ -249,8 +249,8 @@ typings install --global --save \
 	dt~angular \
 	dt~angular-material \
 	dt~angular-animate \
-	# dt~webrtc/mediastream \
-	# dt~webrtc/rtcpeerconnection \
+	\ # dt~webrtc/mediastream
+	\ # dt~webrtc/rtcpeerconnection
 	dt~dompurify
 
 mkdir typings/globals/firebase
