67a21ebce1674dc143c8f1f8d32c8a3d8c46cd0f
Ryan Lester <hacker@linux.com>
2016-10-04 10:34:28 -0400

    key caching fixes

--- commands/keycache.sh
+++ commands/keycache.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-source ~/.bashrc
-
 echo -e '\n\n\ncaching SSH and GPG keys\n'
+
 eval "$(ssh-agent)"
 ssh-add ~/.ssh/id_rsa
+
+if [ -f ~/.gnupg/gpg-agent.conf ] ; then
+	mv ~/.gnupg/gpg-agent.conf ~/.gnupg/gpg-agent.conf.bak
+fi
+cat > ~/.gnupg/gpg-agent.conf <<- EOM
+	default-cache-ttl 34560000
+	max-cache-ttl 34560000
+EOM
+gpg-connect-agent reloadagent /bye
+rm ~/.gnupg/gpg-agent.conf
+if [ -f ~/.gnupg/gpg-agent.conf.bak ] ; then
+	mv ~/.gnupg/gpg-agent.conf.bak ~/.gnupg/gpg-agent.conf
+fi
+
 mkdir ~/tmpgit
 cd ~/tmpgit
 git init
 touch balls
 git add balls
 git commit -S -a -m test
-
-bash -c '
-	cd ~/tmpgit
-	while true ; do
-		sleep 60
-		date > balls
-		git commit -S -a -m test
-	done
-' &
