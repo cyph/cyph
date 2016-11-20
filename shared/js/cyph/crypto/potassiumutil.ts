0fea71383e005da6218d0daab4d37fa8933be39d
Ryan Lester <hacker@linux.com>
2016-11-20 18:13:32 -0500

    crypto lib type definitions

--- shared/js/cyph/crypto/potassiumutil.ts
+++ shared/js/cyph/crypto/potassiumutil.ts
@@ -3,19 +3,129 @@
  */
 export class PotassiumUtil {
 	/** @ignore */
-	protected static McEliece		= self['mceliece'] || {};
+	protected static McEliece: {
+		decryptedDataLength: number;
+		encryptedDataLength: number;
+		privateKeyLength: number;
+		publicKeyLength: number;
+
+		decrypt: (encrypted: Uint8Array|string, privateKey: Uint8Array) => Uint8Array;
+		encrypt: (message: Uint8Array|string, publicKey: Uint8Array) => Uint8Array;
+		keyPair: () => {privateKey: Uint8Array; publicKey: Uint8Array};
+	}	= self['mceliece'] || (<any> {});
 
 	/** @ignore */
-	protected static NTRU			= self['ntru'] || {};
+	protected static NTRU: {
+		decryptedDataLength: number;
+		encryptedDataLength: number;
+		privateKeyLength: number;
+		publicKeyLength: number;
+
+		decrypt: (encrypted: Uint8Array|string, privateKey: Uint8Array) => Uint8Array;
+		encrypt: (message: Uint8Array|string, publicKey: Uint8Array) => Uint8Array;
+		keyPair: () => {privateKey: Uint8Array; publicKey: Uint8Array};
+	}	= self['ntru'] || (<any> {});
 
 	/** @ignore */
-	protected static RLWE			= self['rlwe'] || {};
+	protected static RLWE: {
+		privateKeyLength: number;
+		publicKeyLength: number;
+		secretLength: number;
+
+		aliceKeyPair: () => {privateKey: Uint8Array; publicKey: Uint8Array};
+		aliceSecret: (publicKey: Uint8Array, privateKey: Uint8Array) => Uint8Array;
+		bobSecret: (alicePublicKey: Uint8Array) => {publicKey: Uint8Array; secret: Uint8Array};
+	}	= self['rlwe'] || (<any> {});
 
 	/** @ignore */
-	protected static Sodium			= self['sodium'] || {};
+	protected static Sodium: {
+		crypto_aead_chacha20poly1305_ABYTES: number;
+		crypto_aead_chacha20poly1305_KEYBYTES: number;
+		crypto_aead_chacha20poly1305_NPUBBYTES: number;
+		crypto_box_NONCEBYTES: number;
+		crypto_box_PUBLICKEYBYTES: number;
+		crypto_box_SECRETKEYBYTES: number;
+		crypto_onetimeauth_BYTES: number;
+		crypto_onetimeauth_KEYBYTES: number;
+		crypto_pwhash_scryptsalsa208sha256_MEMLIMIT_INTERACTIVE: number;
+		crypto_pwhash_scryptsalsa208sha256_OPSLIMIT_INTERACTIVE: number;
+		crypto_pwhash_scryptsalsa208sha256_OPSLIMIT_SENSITIVE: number;
+		crypto_pwhash_scryptsalsa208sha256_SALTBYTES: number;
+		crypto_scalarmult_BYTES: number;
+		crypto_scalarmult_SCALARBYTES: number;
+
+		crypto_aead_chacha20poly1305_decrypt: (
+			secretNonce: Uint8Array,
+			cyphertext: Uint8Array,
+			additionalData: Uint8Array,
+			publicNonce: Uint8Array,
+			key: Uint8Array
+		) => Uint8Array;
+		crypto_aead_chacha20poly1305_encrypt: (
+			plaintext: Uint8Array,
+			additionalData: Uint8Array,
+			secretNonce: Uint8Array,
+			publicNonce: Uint8Array,
+			key: Uint8Array
+		) => Uint8Array;
+		crypto_box_keypair: () => {keyType: string; privateKey: Uint8Array; publicKey: Uint8Array};
+		crypto_box_seal: (plaintext: Uint8Array, publicKey: Uint8Array) => Uint8Array;
+		crypto_box_seal_open: (
+			cyphertext: Uint8Array,
+			publicKey: Uint8Array,
+			privateKey: Uint8Array
+		) => Uint8Array;
+		crypto_generichash: (
+			outputBytes: number,
+			plaintext: Uint8Array,
+			key?: Uint8Array
+		) => Uint8Array;
+		crypto_onetimeauth: (
+			message: Uint8Array,
+			key: Uint8Array
+		) => Uint8Array;
+		crypto_onetimeauth_verify: (
+			mac: Uint8Array,
+			message: Uint8Array,
+			key: Uint8Array
+		) => boolean;
+		crypto_pwhash_scryptsalsa208sha256: (
+			keyBytes: number,
+			password: Uint8Array,
+			salt: Uint8Array,
+			opsLimit: number,
+			memLimit: number
+		) => Uint8Array;
+		crypto_scalarmult: (privateKey: Uint8Array, publicKey: Uint8Array) => Uint8Array;
+		crypto_scalarmult_base: (privateKey: Uint8Array) => Uint8Array;
+		from_base64: (s: string) => Uint8Array;
+		from_hex: (s: string) => Uint8Array;
+		from_string: (s: string) => Uint8Array;
+		memcmp: (a: Uint8Array, b: Uint8Array) => boolean;
+		memzero: (a: Uint8Array) => void;
+		to_base64: (a: Uint8Array) => string;
+		to_hex: (a: Uint8Array) => string;
+		to_string: (a: Uint8Array) => string;
+	}	= self['sodium'] || {};
 
 	/** @ignore */
-	protected static SuperSphincs	= self['superSphincs'] || {};
+	protected static SuperSphincs: {
+		bytes: number;
+		hashBytes: number;
+		privateKeyBytes: number;
+		publicKeyBytes: number;
+
+		hash: (message: Uint8Array|string, onlyBinary: boolean) => Uint8Array;
+		keyPair: () => Promise<{keyType: string; privateKey: Uint8Array; publicKey: Uint8Array}>;
+		open: (signed: Uint8Array|string, publicKey: Uint8Array) => Promise<string>;
+		sign: (message: Uint8Array|string, privateKey: Uint8Array) => Promise<string>;
+		signDetached: (message: Uint8Array|string, privateKey: Uint8Array) => Promise<string>;
+		verifyDetached: (
+			signature: Uint8Array|string,
+			message: Uint8Array|string,
+			publicKey: Uint8Array
+		) => Promise<boolean>;
+	}	= self['superSphincs'] || (<any> {});
 
 	/** Zeroes out memory. */
 	public static clearMemory (a: ArrayBufferView) : void {
