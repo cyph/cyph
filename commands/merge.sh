d31b9425a15f08203ee821dba46480402a4f6eb9
Ryan Lester <hacker@linux.com>
2016-12-10 14:02:12 -0500

    merge.sh fix

--- commands/merge.sh
+++ commands/merge.sh
@@ -3,7 +3,7 @@
 source="$1"
 target="$2"
 sourceCamel="$(echo $source | perl -pe 's/\/(.)/\u$1/g')"
-targetCamel="$(echo $source | perl -pe 's/\/(.)/\u$1/g')"
+targetCamel="$(echo $target | perl -pe 's/\/(.)/\u$1/g')"
 
 if [ ! "$source" -o ! "$target" ] ; then
 	echo 'fak u gooby'
