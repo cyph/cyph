a48b7d2de5f6f1628d2467fdcc8504e066628d3b
Ryan Lester <hacker@linux.com>
2016-12-13 18:49:04 -0500

    script factor fix

--- commands/cachebust.js
+++ commands/cachebust.js
@@ -1,37 +1,37 @@
 #!/usr/bin/env node
 
-const superSphincs		= require("supersphincs");
+const superSphincs		= require('supersphincs');
 
-const filesToCacheBust	= child_process.spawnSync("find", [
-	"-L",
-	".",
-	"-type",
-	"f",
-	"-mindepth",
-	"2"
-]).stdout.toString().split("\n").filter(s => s).map(s => s.slice(2));
+const filesToCacheBust	= child_process.spawnSync('find', [
+	'-L',
+	'.',
+	'-type',
+	'f',
+	'-mindepth',
+	'2'
+]).stdout.toString().split('\n').filter(s => s).map(s => s.slice(2));
 
-const filesToModify		= child_process.spawnSync("find", [
-	".",
-	"-type",
-	"f",
-	"-and",
-	"\(",
-	"-name",
-	"*.html",
-	"-or",
-	"-name",
-	"*.js",
-	"-or",
-	"-name",
-	"*.css",
-	"\)"
-]).stdout.toString().split("\n").filter(s => s);
+const filesToModify		= child_process.spawnSync('find', [
+	'.',
+	'-type',
+	'f',
+	'-and',
+	'\(',
+	'-name',
+	'*.html',
+	'-or',
+	'-name',
+	'*.js',
+	'-or',
+	'-name',
+	'*.css',
+	'\)'
+]).stdout.toString().split('\n').filter(s => s);
 
 const fileContents		= {};
 const cacheBustedFiles	= {};
 
-const getFileName		= file => file.split("/").slice(-1)[0];
+const getFileName		= file => file.split('/').slice(-1)[0];
 
 
 Promise.all(filesToModify.map(file =>
@@ -68,7 +68,7 @@ Promise.all(filesToModify.map(file =>
 /* To save space, remove unused subresources under lib directory */
 then(() => Promise.all(
 	filesToCacheBust.filter(subresource =>
-		subresource.startsWith("lib/") &&
+		subresource.startsWith('lib/') &&
 		!cacheBustedFiles[getFileName(subresource)]
 	).map(subresource => new Promise(resolve =>
 		fs.unlink(subresource, resolve)
--- commands/websign/subresourceinline.js
+++ commands/websign/subresourceinline.js
@@ -1,27 +1,27 @@
 #!/usr/bin/env node
 
-const datauri		= require("datauri");
+const datauri		= require('datauri');
 
-const filesToMerge	= child_process.spawnSync("find", [
-	"audio",
-	"fonts",
-	"img",
-	"video",
-	"-type",
-	"f"
-]).stdout.toString().split("\n").filter(s => s);
+const filesToMerge	= child_process.spawnSync('find', [
+	'audio',
+	'fonts',
+	'img',
+	'video',
+	'-type',
+	'f'
+]).stdout.toString().split('\n').filter(s => s);
 
-const filesToModify	= ["js", "css"].reduce((arr, ext) =>
+const filesToModify	= ['js', 'css'].reduce((arr, ext) =>
 	arr.concat(
-		child_process.spawnSync("find", [
+		child_process.spawnSync('find', [
 			ext,
-			"-name",
-			"*." + ext,
-			"-type",
-			"f"
-		]).stdout.toString().split("\n")
+			'-name',
+			'*.' + ext,
+			'-type',
+			'f'
+		]).stdout.toString().split('\n')
 	),
-	["index.html"]
+	['index.html']
 ).filter(s => s);
 
 
@@ -38,10 +38,10 @@ for (let file of filesToModify) {
 
 		content	= content.
 			replace(
-				new RegExp(`(src|href|content)=(\\\\?['"'"'"])/?${subresource}\\\\?['"'"'"]`, "g"),
+				new RegExp(`(src|href|content)=(\\\\?['"])/?${subresource}\\\\?['"]`, 'g'),
 				`WEBSIGN-SRI-DATA-START☁$2☁☁☁${dataURI}☁WEBSIGN-SRI-DATA-END`
 			).replace(
-				new RegExp(`/?${subresource}(\\?websign-sri-disable)?`, "g"),
+				new RegExp(`/?${subresource}(\\?websign-sri-disable)?`, 'g'),
 				dataURI
 			).replace(
 				/☁☁☁/g,
