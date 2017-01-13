66798b9b3c58650e5d25bd79c03c56755d9c36c1
Ryan Lester <hacker@linux.com>
2017-01-12 22:24:43 -0500

    split custom build client logic into separate file

---
+++ shared/js/preload/custombuild.ts
@@ -0,0 +1,16 @@
+/**
+ * @file Handle custom builds.
+ */
+
+
+customBuild			= $('meta[name="custom-build"]').attr('content');
+customBuildFavicon	= $('meta[name="custom-build-favicon"]').attr('content');
+
+$('.custom-build-favicon').each((_: number, elem: HTMLElement) => {
+	if (elem instanceof HTMLLinkElement) {
+		elem.href		= customBuildFavicon || '';
+	}
+	else if (elem instanceof HTMLMetaElement) {
+		elem.content	= customBuildFavicon || '';
+	}
+});
