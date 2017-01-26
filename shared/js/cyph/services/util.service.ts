28a8113337db7521bc05cecb41ee201af37fd2c1
Ryan Lester <hacker@linux.com>
2017-01-26 14:42:13 -0500

    misc minor fixes and cleanup

---
+++ shared/js/cyph/services/util.service.ts
@@ -0,0 +1,13 @@
+import {Injectable} from '@angular/core';
+import {Util} from '../util';
+
+
+/**
+ * @see Util
+ */
+@Injectable()
+export class UtilService extends Util {
+	constructor () {
+		super();
+	}
+}
