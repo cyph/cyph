75a9297327df3540517620baa51a67c8049512c2
Ryan Lester <hacker@linux.com>
2017-01-31 12:22:47 -0500

    workaround for https://github.com/palantir/tslint/issues/2157

--- commands/updatelibs.sh
+++ commands/updatelibs.sh
@@ -134,7 +134,8 @@ for d in firebase firebase-server ts-node tslint ; do
 	mkdir -p ~/lib/js/module_locks/${d}
 	cd node_modules/${d}
 	mkdir node_modules 2> /dev/null
-	yarn install || exit 1
+	sed -i 's|https://https://|https://|g' yarn.lock 2> /dev/null
+	yarn install --ignore-platform || exit 1
 	cp yarn.lock package.json ~/lib/js/module_locks/${d}/
 	cd ../..
 done
