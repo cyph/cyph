fcc36e6e9549d8019e0ada309527f689e9fdabe0
Josh Boehm <josh@cyph.com>
2017-02-15 19:37:07 -0800

    ACCOUNTS-7 added presence styling to user avatars

--- shared/js/cyph/components/account-contacts.component.ts
+++ shared/js/cyph/components/account-contacts.component.ts
@@ -1,8 +1,8 @@
 import {Component} from '@angular/core';
+import {UserPresence} from '../account/enums';
 import {AccountAuthService} from '../services/account-auth.service';
 import {AccountContactsService} from '../services/account-contacts.service';
 import {EnvService} from '../services/env.service';
-import {UserPresence} from '../account/enums';
 
 
 /**
