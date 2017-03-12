0cd3132d8255f0dd2e4034ee84e2ff29bc4345b5
Ryan Lester <hacker@linux.com>
2017-03-12 16:57:16 -0400

    move Firebase config

--- shared/js/cyph/firebase-app.ts
+++ shared/js/cyph/firebase-app.ts
@@ -6,11 +6,6 @@ import {util} from './util';
 /** Firebase app instance. */
 export const firebaseApp: Promise<firebase.app.App>	= (async () =>
 	util.retryUntilSuccessful(() =>
-		firebase.apps[0] || firebase.initializeApp({
-			apiKey: 'AIzaSyB7B8i8AQPtgMXS9o6zbfX1Vv-PwW2Q0Jo',
-			authDomain: 'cyphme.firebaseapp.com',
-			databaseURL: env.firebaseEndpoint,
-			storageBucket: 'cyphme.appspot.com'
-		})
+		firebase.apps[0] || firebase.initializeApp(env.firebaseConfig)
 	)
 )();
