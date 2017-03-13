7d090cc27646466591ac7047e5dc1bea7d1a9f19
Josh Boehm <josh@cyph.com>
2017-03-13 07:12:57 -0700

    ACCOUNTS-9 Added menu toggle button

--- shared/js/cyph/components/account-menu.component.ts
+++ shared/js/cyph/components/account-menu.component.ts
@@ -3,7 +3,9 @@ import {States, UserPresence} from '../account/enums';
 import {AccountAuthService} from '../services/account-auth.service';
 import {AccountService} from '../services/account.service';
 import {EnvService} from '../services/env.service';
+import {MdSidenavService} from '../services/material/md-sidenav.service';
 import {UrlStateService} from '../services/url-state.service';
+import {util} from '../util';
 
 
 /**
@@ -15,38 +17,88 @@ import {UrlStateService} from '../services/url-state.service';
 	templateUrl: '../../../templates/account-menu.html'
 })
 export class AccountMenuComponent implements OnInit {
+	/** @ignore */
+	private readonly menu: Promise<angular.material.ISidenavObject>;
+
+	/** @ignore */
+	private readonly menuLock: {}	= {};
+
+	/** Menu component ID. */
+	public readonly menuId: string	= util.generateGuid();
+
 	/** @see States */
 	public states: typeof States	= States;
 
 	/** @see UserPresence */
 	public readonly userPresence: typeof UserPresence	= UserPresence;
 
+	/** Closes account menu. */
+	public async closeMenu () : Promise<void> {
+		return util.lockTryOnce(
+			this.menuLock,
+			async () => {
+				await util.sleep();
+				(await this.menu).close();
+			}
+		);
+	}
+
 	/** Goes to state. */
 	public async goToState (state: States) : Promise<void> {
 		if (this.envService.isMobile) {
-			this.accountService.toggleMenu(false);
+			await this.closeMenu();
 		}
 
 		this.accountService.state	= state;
 		this.urlStateService.setUrl('account/' + States[state]);
 	}
 
+	public get menuExpanded(): boolean {
+		return this.accountService.menuExpanded
+	}
+
 	/** @inheritDoc */
-	public ngOnInit () : void {
-		this.accountService.toggleMenu(!this.envService.isMobile);
+	public async ngOnInit () : Promise<void> {
+		if (this.envService.isMobile) {
+			return;
+		}
+		await util.sleep();
+		await this.openMenu();
+	}
+
+	/** Opens account menu. */
+	public async openMenu () : Promise<void> {
+		await util.sleep();
+		(await this.menu).open();
+	}
+
+	/** Toggles account menu. */
+	public async toggleMenu () : Promise<void> {
+		return util.lockTryOnce(
+			this.menuLock,
+			async () => {
+				await util.sleep();
+				await this.accountService.toggleMenu();
+				(await this.menu).toggle();
+			}
+		);
 	}
 
 	constructor (
+		mdSidenavService: MdSidenavService,
+
 		/** @ignore */
 		private readonly accountService: AccountService,
+
+		/** @ignore */
+		private readonly envService: EnvService,
 
 		/** @see AccountAuthService */
 		public readonly accountAuthService: AccountAuthService,
 
-		/** @see EnvAuthService */
-		public readonly envService: EnvService,
-
 		/** @see UrlStateService */
 		public readonly urlStateService: UrlStateService
-	) {}
+	) {
+		this.menu	= mdSidenavService.getSidenav(this.menuId);
+	}
 }
