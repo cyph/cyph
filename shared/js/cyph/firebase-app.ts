707f4f28f2e3bed527d811c1b03baaf9db1fa762
Ryan Lester <hacker@linux.com>
2017-03-17 15:15:53 -0400

    ACCOUNTS-11 minor fix

--- shared/js/cyph/firebase-app.ts
+++ shared/js/cyph/firebase-app.ts
@@ -6,6 +6,6 @@ import {util} from './util';
 /** Firebase app instance. */
 export const firebaseApp: Promise<firebase.app.App>	= (async () =>
 	util.retryUntilSuccessful(() =>
-		firebase.apps[0] || firebase.initializeApp(env.firebaseConfig)
+		firebase && (firebase.apps[0] || firebase.initializeApp(env.firebaseConfig))
 	)
 )();
