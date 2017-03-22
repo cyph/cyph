ab0575edb1ed4231ff57459ab90f1bdacdb9ed5a
Ryan Lester <hacker@linux.com>
2017-03-21 21:11:01 -0400

    remove unused exports from cyph/base.ts

--- shared/js/cyph/base.ts
+++ shared/js/cyph/base.ts
@@ -1,7 +1,1 @@
-export * from './config';
-export * from './email';
-export * from './env';
 export * from './event-manager';
-export * from './ithread';
-export * from './timer';
-export * from './util';
