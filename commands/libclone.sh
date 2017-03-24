e26f435a813530dc65d01833327217c06334ab2e
Ryan Lester <hacker@linux.com>
2017-03-24 00:09:30 -0400

    PayPal support

--- commands/libclone.sh
+++ commands/libclone.sh
@@ -2,19 +2,24 @@
 
 repo="${1}"
 outdir="${2}"
+branch="${3}"
 
 mkdir -p ${outdir}
 
 git clone \
 	-b $(
-		{
-			echo master;
-			git ls-remote --tags ${repo} |
-				grep -v '{}' |
-				awk -F'/' '{print $3}' |
-				sort -V \
-			;
-		} | tail -n1
+		if [ "${branch}" ] ; then
+			echo "${branch}"
+		else
+			{
+				echo master;
+				git ls-remote --tags ${repo} |
+					grep -v '{}' |
+					awk -F'/' '{print $3}' |
+					sort -V \
+				;
+			} | tail -n1
+		fi
 	) \
 	--depth 1 \
 	--recursive \
