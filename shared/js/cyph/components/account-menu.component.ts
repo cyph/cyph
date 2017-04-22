756024675caeb1f2ef2e29b8c0ad992f3aaad4d8
Ryan Lester <hacker@linux.com>
2017-04-22 04:56:22 +0000

    accounts Material2 port complete

--- shared/js/cyph/components/account-menu.component.ts
+++ shared/js/cyph/components/account-menu.component.ts
@@ -1,11 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {States, UserPresence} from '../account/enums';
 import {AccountAuthService} from '../services/account-auth.service';
 import {AccountService} from '../services/account.service';
 import {EnvService} from '../services/env.service';
-import {MdSidenavService} from '../services/material/md-sidenav.service';
 import {UrlStateService} from '../services/url-state.service';
-import {util} from '../util';
 
 
 /**
@@ -16,89 +14,36 @@ import {util} from '../util';
 	styleUrls: ['../../css/components/account-menu.css'],
 	templateUrl: '../../../templates/account-menu.html'
 })
-export class AccountMenuComponent implements OnInit {
-	/** @ignore */
-	private readonly menu: Promise<angular.material.ISidenavObject>;
-
-	/** @ignore */
-	private readonly menuLock: {}	= {};
-
-	/** Menu component ID. */
-	public readonly menuId: string	= util.generateGuid();
-
+export class AccountMenuComponent {
 	/** @see States */
 	public states: typeof States	= States;
 
 	/** @see UserPresence */
 	public readonly userPresence: typeof UserPresence	= UserPresence;
 
-	/** Closes account menu. */
-	public async closeMenu () : Promise<void> {
-		return util.lockTryOnce(
-			this.menuLock,
-			async () => {
-				await util.sleep();
-				(await this.menu).close();
-			}
-		);
-	}
-
 	/** Goes to state. */
 	public async goToState (state: States) : Promise<void> {
 		if (this.envService.isMobile) {
-			await this.closeMenu();
+			this.accountService.toggleMenu(false);
 		}
 
 		this.accountService.state	= state;
 		this.urlStateService.setUrl('account/' + States[state]);
 	}
 
-	public get menuExpanded(): boolean {
-		return this.accountService.menuExpanded
-	}
-
-	/** @inheritDoc */
-	public async ngOnInit () : Promise<void> {
-		if (this.envService.isMobile) {
-			return;
-		}
-		await util.sleep();
-		await this.openMenu();
-	}
-
-	/** Opens account menu. */
-	public async openMenu () : Promise<void> {
-		await util.sleep();
-		(await this.menu).open();
-	}
-
-	/** Toggles account menu. */
-	public async toggleMenu () : Promise<void> {
-		return util.lockTryOnce(
-			this.menuLock,
-			async () => {
-				await util.sleep();
-				await this.accountService.toggleMenu();
-				(await this.menu).toggle();
-			}
-		);
-	}
-
 	constructor (
-		mdSidenavService: MdSidenavService,
-
 		/** @ignore */
 		private readonly accountService: AccountService,
-
-		/** @ignore */
-		private readonly envService: EnvService,
 
 		/** @see AccountAuthService */
 		public readonly accountAuthService: AccountAuthService,
 
+		/** @see EnvAuthService */
+		public readonly envService: EnvService,
+
 		/** @see UrlStateService */
 		public readonly urlStateService: UrlStateService
 	) {
-		this.menu	= mdSidenavService.getSidenav(this.menuId);
+		this.accountService.toggleMenu(!this.envService.isMobile);
 	}
 }
