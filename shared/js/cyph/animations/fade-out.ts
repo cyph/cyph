d853f095f343c0df41348ec08bd6a7af7b8fe29f
Ryan Lester <hacker@linux.com>
2017-04-26 00:42:34 -0400

    ellipsis spinner for unconfirmed messages

---
+++ shared/js/cyph/animations/fade-out.ts
@@ -0,0 +1,8 @@
+import {animate, state, style, transition, trigger} from '@angular/animations';
+
+export const fadeOut	= trigger('fadeOut', [
+	state('in', style({opacity: '1'})),
+	transition(':leave', [
+		animate(750, style({opacity: '0'}))
+	])
+]);
--- shared/js/cyph/animations/index.ts
+++ shared/js/cyph/animations/index.ts
@@ -1,2 +1,3 @@
 export * from './fade-in';
 export * from './fade-in-out';
+export * from './fade-out';
