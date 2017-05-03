ab681740a827b676ec364a7ffbf5b62f359fc0ed
Ryan Lester <hacker@linux.com>
2017-05-03 19:30:31 -0400

    update for latest tns-core-modules

--- shared/js/native/main.ts
+++ shared/js/native/main.ts
@@ -1,4 +1,4 @@
-/* tslint:disable:no-import-side-effect no-require-imports no-var-requires */
+/* tslint:disable:no-import-side-effect */
 
 /**
  * @file Entry point of app.
@@ -8,17 +8,15 @@
 import {platformNativeScriptDynamic} from 'nativescript-angular/platform';
 
 import {enableProdMode} from '@angular/core';
+import {WebViewInterface} from 'nativescript-webview-interface';
 import 'rxjs/add/operator/toPromise';
+import {WebView} from 'tns-core-modules/ui/web-view';
 import {AppModule} from './app.module';
 import {webCryptoPolyfill} from './js/cyph/crypto/web-crypto-polyfill';
 
 
 (async (importScripts: Function = () => {}) => {
-	const webViewModule: any			= require('ui/web-view');
-	const webViewInterfaceModule: any	= require('nativescript-webview-interface');
-
-	const webView			= new webViewModule.WebView();
-	const webViewInterface	= new webViewInterfaceModule.WebViewInterface(webView, `
+	const webViewInterface	= new WebViewInterface(new WebView(), `
 		<html><body><script>
 			var seed	= crypto.getRandomValues(new Uint8Array(32));
 			nsWebViewInterface.on('_', function () {
@@ -32,7 +30,7 @@ import {webCryptoPolyfill} from './js/cyph/crypto/web-crypto-polyfill';
 
 	const seed	= new Promise<Uint8Array>(resolve => { webViewInterface.on('_', resolve); });
 
-	webViewInterface.emit('_');
+	webViewInterface.emit('_', undefined);
 	webCryptoPolyfill(await seed);
 	webViewInterface.destroy();
 	importScripts('/lib/js/node_modules/libsodium/dist/browsers-sumo/combined/sodium.js');
