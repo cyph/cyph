d8c727b0d598c3e49a634cc27dd1d94d63460d70
Ryan Lester <hacker@linux.com>
2017-05-05 01:09:41 -0400

    build fix

--- shared/js/cyph/crypto/native-web-crypto-polyfill.ts
+++ shared/js/cyph/crypto/native-web-crypto-polyfill.ts
@@ -3,38 +3,40 @@
  */
 
 
-let getRandomByte: () => number;
+(() => {
+	let getRandomByte: () => number;
 
-/* Android */
-if (
-	typeof java !== 'undefined' &&
-	typeof java.security !== 'undefined' &&
-	typeof java.security.SecureRandom !== 'undefined'
-) {
-	const secureRandom	= new java.security.SecureRandom();
-	getRandomByte		= () => secureRandom.nextInt(256);
-}
-/* iOS */
-else if (typeof arc4random_uniform !== 'undefined') {
-	getRandomByte		= () => arc4random_uniform(256);
-}
-/* Other */
-else {
-	throw new Error('Crypto polyfill not implemented for this platform.');
-}
+	/* Android */
+	if (
+		typeof java !== 'undefined' &&
+		typeof java.security !== 'undefined' &&
+		typeof java.security.SecureRandom !== 'undefined'
+	) {
+		const secureRandom	= new java.security.SecureRandom();
+		getRandomByte		= () => secureRandom.nextInt(256);
+	}
+	/* iOS */
+	else if (typeof arc4random_uniform !== 'undefined') {
+		getRandomByte		= () => arc4random_uniform(256);
+	}
+	/* Other */
+	else {
+		throw new Error('Crypto polyfill not implemented for this platform.');
+	}
 
-crypto	= {
-	getRandomValues: (arrayBufferView: ArrayBufferView) => {
-		const bytes	= new Uint8Array(arrayBufferView.buffer);
+	crypto	= {
+		getRandomValues: (arrayBufferView: ArrayBufferView) => {
+			const bytes	= new Uint8Array(arrayBufferView.buffer);
 
-		for (let i = 0 ; i < bytes.length ; ++i) {
-			bytes[i]	= getRandomByte();
-		}
+			for (let i = 0 ; i < bytes.length ; ++i) {
+				bytes[i]	= getRandomByte();
+			}
 
-		return arrayBufferView;
-	},
+			return arrayBufferView;
+		},
 
-	subtle: <SubtleCrypto> {}
-};
+		subtle: <SubtleCrypto> {}
+	};
 
-(<any> self).crypto	= crypto;
+	(<any> self).crypto	= crypto;
+})();
