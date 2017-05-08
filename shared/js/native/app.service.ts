dd5dc120efc128aaaf95a8e75440f6e495be63a8
Ryan Lester <hacker@linux.com>
2017-05-08 18:22:37 -0400

    lint fix

--- shared/js/native/app.service.ts
+++ shared/js/native/app.service.ts
@@ -5,6 +5,7 @@ import {Injectable} from '@angular/core';
  * Angular service for Cyph native UI.
  */
 @Injectable()
+/* tslint:disable-next-line:no-stateless-class */
 export class AppService {
 	constructor () {}
 }
