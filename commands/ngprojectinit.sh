ed36f3f1d6fb2270653b7486787833f5b8733567
Ryan Lester <hacker@linux.com>
2017-05-14 13:10:11 -0400

    CircleCI fix

--- commands/ngprojectinit.sh
+++ commands/ngprojectinit.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 
+circleCI=''
+if [ -f ~/.circleci ] ; then
+	circleCI=true
+fi
+
 rm src/favicon.ico 2> /dev/null
 cp ../shared/favicon.ico src/
 
@@ -12,7 +17,13 @@ for arr in \
 	'src/templates ../shared/templates'
 do
 	read -ra arr <<< "${arr}"
-	rm "${arr[0]}" 2> /dev/null
-	mkdir "${arr[0]}" 2> /dev/null
-	sudo mount --bind "${arr[1]}" "${arr[0]}"
+
+	if [ "${circleCI}" ] ; then
+		rm -rf "${arr[0]}" 2> /dev/null
+		cp -a "${arr[1]}" "${arr[0]}"
+	else
+		rm "${arr[0]}" 2> /dev/null
+		mkdir "${arr[0]}" 2> /dev/null
+		sudo mount --bind "${arr[1]}" "${arr[0]}"
+	fi
 done
