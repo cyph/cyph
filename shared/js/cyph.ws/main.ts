72bcff7ed873ccb779fdf35b01d522c2d78da78f
Ryan Lester <hacker@linux.com>
2017-05-14 03:04:22 -0400

    enable hot module replacement in local env

--- shared/js/cyph.ws/main.ts
+++ shared/js/cyph.ws/main.ts
@@ -12,5 +12,8 @@ import {AppModule} from './app.module';
 if (environment.production) {
 	enableProdMode();
 }
+else if ((<any> module).hot) {
+	(<any> module).hot.accept();
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule);
