734d211c4f2be050b4da8d93004659de782d8176
Ryan Lester <hacker@linux.com>
2017-05-16 01:06:22 -0400

    update docs.sh

--- commands/docs.sh
+++ commands/docs.sh
@@ -4,13 +4,12 @@
 cd $(cd "$(dirname "$0")" ; pwd)/..
 
 
-cp -a shared/js shared/.js.tmp
+rm -rf shared/js/docs shared/.js.tmp 2> /dev/null
+cp -rf shared/js shared/.js.tmp
+cp README.md shared/.js.tmp/
 cd shared/.js.tmp
 
-rm -rf ../js/docs
-
-# NOTE: "--mode file" is a workaround for a typedoc bug; should be "--mode modules"
-typedoc --experimentalDecorators -t ES6 -m system --moduleResolution node --out ../js/docs --name Cyph --mode file --includeDeclarations --excludeExternals .
+compodoc . -n 'Cyph Docs' -d ../js/docs -p .
 
 cd ..
 rm -rf .js.tmp
