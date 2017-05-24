67fceb3c46b15a9f31d534a369d60c76e19a46a3
Ryan Lester <hacker@linux.com>
2017-05-23 20:12:10 -0400

    account routing minor fix

--- shared/js/cyph.ws/app-routes.ts
+++ shared/js/cyph.ws/app-routes.ts
@@ -1,6 +1,7 @@
 /* tslint:disable:object-literal-sort-keys */
 
 import {Routes} from '@angular/router';
+import {AccountComponent} from '../cyph/components/account.component';
 import {account, retry} from '../cyph/routes';
 import {AppService} from './app.service';
 import {EphemeralChatRootComponent} from './ephemeral-chat-root.component';
@@ -12,5 +13,7 @@ account.canActivate	= [AppService];
 export const appRoutes: Routes	= [
 	retry,
 	account,
+	{path: 'extension', component: AccountComponent},
+	{path: 'telehealth', component: AccountComponent},
 	{path: '**', canActivate: [AppService], component: EphemeralChatRootComponent}
 ];
