94523aa297fcdcb9ed496a7d327e1e4480f485e7
Ryan Lester <hacker@linux.com>
2017-05-31 18:34:35 -0400

    ACCOUNTS-16 complete

--- shared/js/cyph/services/account-database.service.ts
+++ shared/js/cyph/services/account-database.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
+import {AccountAuthService} from './account-auth.service';
 import {PotassiumService} from './crypto/potassium.service';
-import {LocalStorageService} from './local-storage.service';
+import {DatabaseService} from './database.service';
 
 
 /**
@@ -8,26 +9,30 @@ import {LocalStorageService} from './local-storage.service';
  */
 @Injectable()
 export class AccountDatabaseService {
-	/** @ignore */
-	private dummyKey (url: string, publicData: boolean) : string {
-		return `${url}_${publicData.toString()}`;
-	}
-
 	/**
 	 * Gets an item's value.
 	 * @param url Path to item.
 	 * @param publicData If true, validates the item's signature. Otherwise, decrypts the item.
 	 */
 	public async getItem (url: string, publicData: boolean = false) : Promise<Uint8Array> {
-		const value	= await this.localStorageService.getItem(
-			this.dummyKey(url, publicData)
-		);
-
-		if (value === undefined) {
-			throw new Error(`Failed to get item at ${url}.`);
+		if (!this.accountAuthService.currentUserKeys) {
+			throw new Error(`User not signed in. Cannot get item at ${url}.`);
 		}
 
-		return this.potassiumService.fromString(value);
+		const data	= await this.databaseService.getItem(url);
+
+		return publicData ?
+			this.potassiumService.fromString(
+				await this.potassiumService.sign.open(
+					data,
+					this.accountAuthService.currentUserKeys.signingKeyPair.publicKey
+				)
+			) :
+			await this.potassiumService.secretBox.open(
+				data,
+				this.accountAuthService.currentUserKeys.symmetricKey
+			)
+		;
 	}
 
 	/**
@@ -35,17 +40,7 @@ export class AccountDatabaseService {
 	 * @param url Path to item.
 	 */
 	public async removeItem (url: string) : Promise<void> {
-		for (const publicData of [true, false]) {
-			const success	= await this.localStorageService.removeItem(
-				this.dummyKey(url, publicData)
-			);
-
-			if (success) {
-				return;
-			}
-		}
-
-		throw new Error(`Failed to remove item at ${url}.`);
+		return this.databaseService.removeItem(url);
 	}
 
 	/**
@@ -59,23 +54,36 @@ export class AccountDatabaseService {
 		value: ArrayBufferView|boolean|number|string,
 		publicData: boolean = false
 	) : Promise<void> {
-		const success	= await this.localStorageService.setItem(
-			this.dummyKey(url, publicData),
-			this.potassiumService.toString(
-				(typeof value === 'boolean' || typeof value === 'number') ?
-					value.toString() :
-					value
-			)
+		if (!this.accountAuthService.currentUserKeys) {
+			throw new Error(`User not signed in. Cannot set item at ${url}.`);
+		}
+
+		const data	= this.potassiumService.fromString(
+			(typeof value === 'boolean' || typeof value === 'number') ?
+				value.toString() :
+				value
 		);
 
-		if (!success) {
-			throw new Error(`Failed to set item at ${url}.`);
-		}
+		return this.databaseService.setItem(
+			url,
+			publicData ?
+				await this.potassiumService.sign.sign(
+					data,
+					this.accountAuthService.currentUserKeys.signingKeyPair.privateKey
+				) :
+				await this.potassiumService.secretBox.seal(
+					data,
+					this.accountAuthService.currentUserKeys.symmetricKey
+				)
+		);
 	}
 
 	constructor (
 		/** @ignore */
-		private readonly localStorageService: LocalStorageService,
+		private readonly accountAuthService: AccountAuthService,
+
+		/** @ignore */
+		private readonly databaseService: DatabaseService,
 
 		/** @ignore */
 		private readonly potassiumService: PotassiumService
