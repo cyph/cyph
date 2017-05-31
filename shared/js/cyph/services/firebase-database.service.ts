1ed67dbcadb5f9fb0c22578069c3ce39bf812e4b
Ryan Lester <hacker@linux.com>
2017-05-31 17:25:31 -0400

    DatabaseService generic get/set value interface (uses Firebase Storage)

--- shared/js/cyph/services/firebase-database.service.ts
+++ shared/js/cyph/services/firebase-database.service.ts
@@ -5,6 +5,7 @@ import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 import 'firebase/storage';
+import {potassiumUtil} from '../crypto/potassium/potassium-util';
 import {env} from '../env';
 import {util} from '../util';
 import {DatabaseService} from './database.service';
@@ -14,7 +15,7 @@ import {DatabaseService} from './database.service';
  * DatabaseService implementation built on Firebase.
  */
 @Injectable()
-export class FirebaseDatabaseService implements DatabaseService {
+export class FirebaseDatabaseService extends DatabaseService {
 	/** @ignore */
 	private app: Promise<firebase.app.App>	= util.retryUntilSuccessful(() => {
 		try {
@@ -43,16 +44,10 @@ export class FirebaseDatabaseService implements DatabaseService {
 	}
 
 	/** @inheritDoc */
-	public async getDatabaseValue<T> (url: string) : Promise<T|undefined> {
-		const value	= (await (await this.getDatabaseRef(url)).once('value')).val();
-
-		/* tslint:disable-next-line:no-null-keyword */
-		if (value !== null) {
-			return value;
-		}
-		else {
-			return undefined;
-		}
+	public async getItem (url: string) : Promise<Uint8Array> {
+		return util.requestBytes({
+			url: await (await this.getStorageRef(url)).getDownloadURL()
+		});
 	}
 
 	/** @inheritDoc */
@@ -87,5 +82,24 @@ export class FirebaseDatabaseService implements DatabaseService {
 		);
 	}
 
-	constructor () {}
+	/** @inheritDoc */
+	public async setItem (
+		url: string,
+		value: ArrayBufferView|boolean|number|string
+	) : Promise<void> {
+		await (await this.getStorageRef(url)).
+			put(
+				new Blob([potassiumUtil.fromString(
+					(typeof value === 'boolean' || typeof value === 'number') ?
+						value.toString() :
+						value
+				)])
+			).
+			then()
+		;
+	}
+
+	constructor () {
+		super();
+	}
 }
