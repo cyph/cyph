d67d4f1c777e50ce2ba903c16693d923bccd71a4
Ryan Lester <hacker@linux.com>
2017-06-09 16:01:01 -0400

    2.5-second timeout for channel disconnects

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,8 +1,30 @@
 const functions	= require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.helloWorld	= functions.https.onRequest((request, response) => {
-	response.send('Hello from Firebase!');
-});
+const channelDisconnectTimeout	= 2500;
+
+
+exports.channelDisconnect	=
+	functions.database.ref('channels/{channel}/disconnects/{user}').onWrite(e => {
+		if (!e.data.exists()) {
+			return;
+		}
+
+		const startingValue	= e.data.val();
+
+		new Promise(resolve =>
+			setTimeout(
+				resolve,
+				Math.max(channelDisconnectTimeout - (Date.now() - startingValue), 0)
+			)
+		).then(() =>
+			e.data.ref.once('value')
+		).then(newData => {
+			if (startingValue !== newData.val()) {
+				return;
+			}
+
+			return e.data.ref.parent.parent.remove();
+		});
+	})
+;
