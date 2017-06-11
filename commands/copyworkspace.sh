aa57abfaf1b5cdd2b302b3238c7897142e317001
Ryan Lester <hacker@linux.com>
2017-06-11 15:51:35 -0400

    Windows fixes

--- commands/copyworkspace.sh
+++ commands/copyworkspace.sh
@@ -3,7 +3,7 @@
 
 source="$(cd "$(dirname "$0")/.." ; pwd)"
 
-find "${1}" -type d -name node_modules -not -path '*/node_modules/*' -exec sudo umount "{}" \; 2> /dev/null
+find "${1}" -type d -name node_modules -not -path '*/node_modules/*' -exec unbindmount "{}" \; 2> /dev/null
 rm -rf "${1}" 2> /dev/null
 mkdir -p "${1}/shared"
 dir="$(realpath "${1}")"
--- commands/ngprojectinit.sh
+++ commands/ngprojectinit.sh
@@ -1,29 +1,16 @@
 #!/bin/bash
 
 
-circleCI=''
-if [ -f ~/.circleci ] ; then
-	circleCI=true
-fi
-
 rm src/favicon.ico 2> /dev/null
 cp ../shared/favicon.ico src/
 
 for arr in \
-	'node_modules /node_modules' \
-	'src/assets ../shared/assets' \
-	'src/css ../shared/css' \
-	'src/js ../shared/js' \
-	'src/templates ../shared/templates'
+	'/node_modules node_modules' \
+	'../shared/assets src/assets' \
+	'../shared/css src/css' \
+	'../shared/js src/js' \
+	'../shared/templates src/templates'
 do
 	read -ra arr <<< "${arr}"
-
-	rm -rf "${arr[0]}" 2> /dev/null
-
-	if [ "${circleCI}" ] ; then
-		cp -a "${arr[1]}" "${arr[0]}"
-	else
-		mkdir "${arr[0]}" 2> /dev/null
-		sudo mount --bind "${arr[1]}" "${arr[0]}"
-	fi
+	bindmount "${arr[0]}" "${arr[1]}"
 done
