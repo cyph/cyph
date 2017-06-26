e3fb8e8e8777d2bb7f3553b59314bfe7fef461a1
Ryan Lester <hacker@linux.com>
2017-06-26 00:06:56 -0400

    updates for data management service interface consistency

--- shared/js/cyph/services/account-database.service.ts
+++ shared/js/cyph/services/account-database.service.ts
@@ -1,11 +1,8 @@
 import {Injectable} from '@angular/core';
 import {User} from '../account/user';
-import {IKeyPair} from '../crypto/ikey-pair';
-import {IPublicKeys} from '../crypto/ipublic-keys';
 import {util} from '../util';
 import {PotassiumService} from './crypto/potassium.service';
-import {DatabaseService} from './database.service';
-import {FileService} from './file.service';
+import {LocalStorageService} from './local-storage.service';
 
 
 /**
@@ -13,50 +10,16 @@ import {FileService} from './file.service';
  */
 @Injectable()
 export class AccountDatabaseService {
-	/** Public keys for AGSE-PKI certificate validation. */
-	private readonly agsePublicSigningKeys	= {
-		rsa: [
-			'eyJhbGciOiJSUzI1NiIsImUiOiJBUUFCIiwiZXh0Ijp0cnVlLCJrZXlfb3BzIjpbInZlcmlmeSJ' +
-			'dLCJrdHkiOiJSU0EiLCJuIjoidkVUOG1HY24zcWFyN1FfaXo1MVZjUmNKdHRFSG5VcWNmN1VybT' +
-			'Vueko4bG80Q2RjQTZLN2dRMDl6bmx4a3NQLTg1RE1NSGdwU29mcU1BY2l6UTVmNW5McGxydEtJX' +
-			'0dtdFJ1T1k2d3RHZnZLcjNNX3pWMGxVYVFKSXEyVmg0aTU0ZHo1akp6QTZwWmp4eU91V01VdnJm' +
-			'SXZrWVg5LUl2MTBxMTEwYm9waGNmRGpNVTFQbTNZeUlVQzhjSEk2TmN0ZGVOV3dzTEg2WkgwbVd' +
-			'QYTgxZUw2bWtyVzBUZkt1Q1ZEaDBFckVCWkJJUUx5TmV1dF9jb2JxR0NoS0V6Y0xVMll6MUUwR1' +
-			'9DbkRLVHVYVG5nNEVUQ0FYakhDUXJwaUp1aV81UG9SUGdhT0xvcEdKV0RmQXkxMF8yX3ZIeGxab' +
-			'3hrNWFxREx2Z3B3Ny1fdHVTNWRzNmlRIn0AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
-		],
-		sphincs: [
-			'z3ztlxR0wvt9oW3zvjSF74ugJKbzCpJ0yHEeUdFiPIiBMZ3zcOu0N4M9adTCvdDQBefFNUSVeFFk' +
-			'7lNUfAPNdIV63DMwYS2hPoSM0q1psU0o9ba0M/vrW51Qkkgh8+U4e0sGtHq3Nrb7Gp+KTo5OB7On' +
-			'SAaudHT/hR3oSj85JdjZ0QhN4iHAJvhujUHceNbLJY/c50YSRlnG/12hc5yxeaslUCvYyYPcXneL' +
-			'Kr3bINmeFkfr9/Lirxkr9AYiN3c4/s4D45MU1XpKY/u9Ar4zil+ejkIokTPVhwGZH6RoSI0j1WX/' +
-			'1MxOIwTBafQ+vSiamDPik9c1nHZMh7Cr573IY0WePSi5qQuY6hbgyyq0qm8+FealryFZAqYQf2kd' +
-			'T8RxzCSnQtQmiisLkqdi/glDiPxi5xs1jSBRUqBv+oaaQAYME2tHdzBqG2V6yz9olOLzzQLAsyhe' +
-			'aoW9C45G4s5ws3eU4CipA3M/rVflgOXyXID8U5t+u+kE3Ncy3b6GSX92bumgqtoaUYRCCN4/TOAB' +
-			'EpMTn/5dlugoyJVDxq986d+oQaGEFQfdkpBybaq9lQZQaUIqwTX26okpmLpvau3s+ENdo76ZxSkK' +
-			'IG8HjP3P0SSLnS3u7aMvm4JW8lI7UG5t38M+5bS5XNUumJdWiWQf6Ay4dDBuaybu7roS8LgTvbfo' +
-			'CLWEUkNT3LmCXYw//+nG6pB0W780ejV9TE0xIIRZ+XzQ8oFNTceJWsbGOgli5IBZh+40LL02TZC9' +
-			'Eo/TFCSNtekG8I2NyZnfP+aeZvOvSW+I95onFjnkRd/KxntSobRTGBPLfJS/UaLAEMnyh9NoPIHi' +
-			'6yUw5QVp7kYYY+slgT66g80GBVj5z1LxBpgsmWIKNnhqbf13qo24C3S+8rf8GAbdSdET9ObarpmA' +
-			'IXjl9Nl7CWL+ZVfFvYrRkKzYHMGIYKnIzj/LHs3pn17yHW+AiN2SEpjUuSnd471+mOqwjmzvfsS4' +
-			'HoN0ehv2cbhc1b+rHaO3thuwemHzS1w1AS9qHOPdFMZ+3wnsqDD16O+yPy5yF060xcdIJkryfMqJ' +
-			'3I9lJfpd2HCLfaHSIYe+KEMJgUhieWahozYDiVX8Y3UVD8wTaC/rrkSq3tNh4k9Qh3qku+QQdzRj' +
-			'/YOeX3tt57DSQEt0/GP3bwXDt1wlaIogeNW93VWXdVpLlt50fom9mf0yMcS4IuXT3qNiMRPjkbTo' +
-			'TfODamHnu9vu/sd/4c6N3soJMJC7e+nIBJWqAgBczXMaTgUb2CsXTB92wf5syOLZNxQqjh073vkT' +
-			'6vQwJ9qYu6ad0carC5esAfgiNY/7ayolQObwd/7wbGGYV1QFCJYs6NCjUTst/DH8KyI8Piw7aknq' +
-			'uqxj7+blqIEewEPKEx1mBKgvDLzGgIoEx3azc/nk'
-		]
-	};
-
 	/** Keys and profile of currently logged in user. Undefined if no user is signed in. */
 	public current?: {
-		keys: {
-			encryptionKeyPair: IKeyPair;
-			signingKeyPair: IKeyPair;
-			symmetricKey: Uint8Array;
-		};
+		keys: {};
 		user: User;
 	};
+
+	/** @ignore */
+	private dummyKey (url: string, publicData: boolean) : string {
+		return `${url}_${publicData.toString()}`;
+	}
 
 	/** @ignore */
 	private processURL (url: string) : string {
@@ -76,29 +39,13 @@ export class AccountDatabaseService {
 	 * @param publicData If true, validates the item's signature. Otherwise, decrypts the item.
 	 */
 	public async getItem (url: string, publicData: boolean = false) : Promise<Uint8Array> {
-		url	= this.processURL(url);
-
-		const data	= await this.databaseService.getItem(url);
-
-		if (publicData) {
-			const username	= (url.match(/\/?users\/(.*?)\//) || [])[1] || '';
-
-			return this.potassiumService.sign.open(
-				data,
-				this.current && username === this.current.user.username ?
-					this.current.keys.signingKeyPair.publicKey :
-					(await this.getUserPublicKeys(username)).signing
-			);
-		}
-		else if (this.current) {
-			return this.potassiumService.secretBox.open(
-				data,
-				this.current.keys.symmetricKey
-			);
-		}
-		else {
+		if (!publicData && !this.current) {
 			throw new Error(`User not signed in. Cannot get private data at ${url}.`);
 		}
+
+		url	= this.processURL(url);
+
+		return this.localStorageService.getItem(this.dummyKey(url, publicData));
 	}
 
 	/**
@@ -143,46 +90,15 @@ export class AccountDatabaseService {
 		);
 	}
 
-	/** Tries to to get public keys belonging to the specified user. */
-	public async getUserPublicKeys (username: string) : Promise<IPublicKeys> {
-		const certificate	= await this.databaseService.getItem(`users/${username}/certificate`);
-		const dataView		= new DataView(certificate.buffer);
-
-		const rsaKeyIndex		= dataView.getUint32(0, true);
-		const sphincsKeyIndex	= dataView.getUint32(4, true);
-		const signed			= new Uint8Array(certificate.buffer, 8);
-
-		if (
-			rsaKeyIndex >= this.agsePublicSigningKeys.rsa.length ||
-			sphincsKeyIndex >= this.agsePublicSigningKeys.sphincs.length
-		) {
-			throw new Error('Invalid AGSE-PKI certificate: bad key index.');
+	/** Checks whether an item exists. */
+	public async hasItem (url: string) : Promise<boolean> {
+		try {
+			await this.getItem(url, true).catch(() => this.getItem(url, false));
+			return true;
 		}
-
-		const verified	= util.parse<{
-			publicEncryptionKey: string;
-			publicSigningKey: string;
-			username: string;
-		}>(
-			this.potassiumService.toString(
-				await this.potassiumService.sign.open(
-					signed,
-					await this.potassiumService.sign.importSuperSphincsPublicKeys(
-						this.agsePublicSigningKeys.rsa[rsaKeyIndex],
-						this.agsePublicSigningKeys.sphincs[sphincsKeyIndex]
-					)
-				)
-			)
-		);
-
-		if (verified.username !== username) {
-			throw new Error('Invalid AGSE-PKI certificate: bad username.');
+		catch (_) {
+			return false;
 		}
-
-		return {
-			encryption: this.potassiumService.fromBase64(verified.publicEncryptionKey),
-			signing: this.potassiumService.fromBase64(verified.publicSigningKey)
-		};
 	}
 
 	/**
@@ -196,7 +112,17 @@ export class AccountDatabaseService {
 
 		url	= this.processURL(url);
 
-		return this.databaseService.removeItem(url);
+		for (const publicData of [true, false]) {
+			const success	= await this.localStorageService.removeItem(
+				this.dummyKey(url, publicData)
+			);
+
+			if (success) {
+				return;
+			}
+		}
+
+		throw new Error(`Failed to remove item at ${url}.`);
 	}
 
 	/**
@@ -220,46 +146,23 @@ export class AccountDatabaseService {
 
 		url	= this.processURL(url);
 
-		const data	=
-			value instanceof ArrayBuffer ?
-				new Uint8Array(value) :
-				ArrayBuffer.isView(value) ?
-					new Uint8Array(value.buffer) :
-					value instanceof Blob ?
-						await this.fileService.getBytes(value, false) :
-						this.potassiumService.fromString(
-							(
-								typeof value === 'boolean' ||
-								typeof value === 'number' ||
-								typeof value === 'string'
-							) ?
-								value.toString() :
-								util.stringify(value)
-						)
-		;
+		const data		= await util.toBytes(value);
 
-		await this.databaseService.setItem(
-			url,
-			publicData ?
-				await this.potassiumService.sign.sign(
-					data,
-					this.current.keys.signingKeyPair.privateKey
-				) :
-				await this.potassiumService.secretBox.seal(
-					data,
-					this.current.keys.symmetricKey
-				)
+		const success	= await this.localStorageService.setItem(
+			this.dummyKey(url, publicData),
+			this.potassiumService.toBase64(data)
 		);
+
+		if (!success) {
+			throw new Error(`Failed to set item at ${url}.`);
+		}
 
 		return url;
 	}
 
 	constructor (
 		/** @ignore */
-		private readonly fileService: FileService,
-
-		/** @ignore */
-		private readonly databaseService: DatabaseService,
+		private readonly localStorageService: LocalStorageService,
 
 		/** @ignore */
 		private readonly potassiumService: PotassiumService
