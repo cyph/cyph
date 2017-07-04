16095e977f5b9c60b2e4eef6e16db1af27e200d8
Ryan Lester <hacker@linux.com>
2017-07-04 13:26:07 -0400

    ACCOUNTS-14 add lock and watch to async value interface

--- shared/js/cyph/iasync-value.ts
+++ shared/js/cyph/iasync-value.ts
@@ -1,3 +1,6 @@
+import {Observable} from 'rxjs';
+
+
 /**
  * Represents an asynchronous value.
  */
@@ -5,6 +8,12 @@ export interface IAsyncValue<T> {
 	/** Gets value. */
 	getValue () : Promise<T>;
 
+	/** Executes a Promise within a mutual-exclusion lock in FIFO order. */
+	lock<T> (f: (reason?: string) => Promise<T>, reason?: string) : Promise<T>;
+
 	/** Sets value. */
 	setValue (value: T) : Promise<void>;
+
+	/** Subscribes to value. */
+	watch () : Observable<T>;
 }
