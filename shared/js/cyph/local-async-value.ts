973fa825fee96f30a62946cffb593125353502e6
Ryan Lester <hacker@linux.com>
2017-07-04 13:26:07 -0400

    ACCOUNTS-14 add lock and watch to async value interface

--- shared/js/cyph/local-async-value.ts
+++ shared/js/cyph/local-async-value.ts
@@ -1,11 +1,20 @@
+import {BehaviorSubject, Observable} from 'rxjs';
 import {potassiumUtil} from './crypto/potassium/potassium-util';
 import {IAsyncValue} from './iasync-value';
+import {LockFunction} from './lock-function-type';
+import {util} from './util';
 
 
 /**
  * IAsyncValue implementation that wraps a local value.
  */
 export class LocalAsyncValue<T> implements IAsyncValue<T> {
+	/** @ignore */
+	private readonly subject: BehaviorSubject<T>	= new BehaviorSubject(this.value);
+
+	/** @inheritDoc */
+	public readonly lock: LockFunction	= util.lockFunction();
+
 	/** @inheritDoc */
 	public async getValue () : Promise<T> {
 		return this.value;
@@ -18,6 +27,13 @@ export class LocalAsyncValue<T> implements IAsyncValue<T> {
 		}
 
 		this.value	= newValue;
+		this.subject.next(this.value);
+	}
+
+	/** @inheritDoc */
+	public watch () : Observable<T> {
+		/* <any> is temporary workaround for https://github.com/ReactiveX/rxjs/issues/2539 */
+		return <any> this.subject;
 	}
 
 	constructor (
