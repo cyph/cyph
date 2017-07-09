9ddb5f32b4df08cff7f73ad63cfc3df43af11220
Ryan Lester <hacker@linux.com>
2017-07-09 04:49:14 -0400

    disable no-stateless-class rule

--- shared/js/cyph.ws/app.module.ts
+++ shared/js/cyph.ws/app.module.ts
@@ -63,7 +63,6 @@ import {LockdownComponent} from './lockdown.component';
 		}
 	]
 })
-/* tslint:disable-next-line:no-stateless-class */
 export class AppModule {
 	constructor (dialogService: DialogService, http: Http) {
 		Util.resolveDialogService(dialogService);
