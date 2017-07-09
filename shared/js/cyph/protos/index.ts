63ab801e95cfa2517280ab914ab040535515e00c
Ryan Lester <hacker@linux.com>
2017-07-09 05:31:10 -0400

    generic protobufs

--- shared/js/cyph/protos/index.ts
+++ shared/js/cyph/protos/index.ts
@@ -1,5 +1,37 @@
+/* tslint:disable:variable-name */
+
+import {
+	BooleanArray,
+	BooleanValue,
+	NumberArray,
+	NumberValue,
+	StringArray,
+	StringValue
+} from '../../proto';
+import {IProto} from '../iproto';
+import {GenericArrayProto} from './generic-array-proto';
+import {GenericProto} from './generic-proto';
+
+
 export * from './binary-proto';
-export * from './boolean-proto';
 export * from './data-uri-proto';
-export * from './number-proto';
-export * from './string-proto';
+export * from './maybe-binary-proto';
+
+
+/** Boolean array encoder/decoder. */
+export const BooleanArrayProto: IProto<boolean[]>	= new GenericArrayProto(BooleanArray);
+
+/** Boolean encoder/decoder. */
+export const BooleanProto: IProto<boolean>			= new GenericProto(BooleanValue);
+
+/** Number array encoder/decoder. */
+export const NumberArrayProto: IProto<number[]>		= new GenericArrayProto(NumberArray);
+
+/** Number encoder/decoder. */
+export const NumberProto: IProto<number>			= new GenericProto(NumberValue);
+
+/** String array encoder/decoder. */
+export const StringArrayProto: IProto<string[]>		= new GenericArrayProto(StringArray);
+
+/** String encoder/decoder. */
+export const StringProto: IProto<string>			= new GenericProto(StringValue);
---
+++ shared/js/cyph/protos/maybe-binary-proto.ts
@@ -0,0 +1,20 @@
+/** Possibly-undefined binary encoder/decoder. (Doesn't actually use Protocol Buffers.) */
+export class MaybeBinaryProto {
+	/** @see IProto.create */
+	public static create () : undefined {
+		return undefined;
+	}
+
+	/** @see IProto.decode */
+	public static decode (bytes: Uint8Array) : Uint8Array|undefined {
+		return bytes.length === 0 ? undefined : bytes;
+	}
+
+	/** @see IProto.encode */
+	public static encode (data?: Uint8Array) : Uint8Array {
+		return data === undefined ? new Uint8Array(0) : data;
+	}
+
+	/** @see IProto.verify */
+	public static verify () : void {}
+}
