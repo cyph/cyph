c5c48d309b4cec710b83440251cbea9c5391ed6e
Ryan Lester <hacker@linux.com>
2017-07-10 04:03:12 -0400

    Box cleanup

--- shared/js/cyph/crypto/potassium/box.ts
+++ shared/js/cyph/crypto/potassium/box.ts
@@ -13,60 +13,21 @@ import {SecretBox} from './secret-box';
 export class Box implements IBox {
 	/** @ignore */
 	private readonly classicalCypher	= {
-		decrypt: async (cyphertext: Uint8Array, keyPair: IKeyPair) => {
-			return this.helpers.open(
-				potassiumUtil.toBytes(cyphertext, this.helpers.nonceBytes),
-				potassiumUtil.toBytes(cyphertext, undefined, this.helpers.nonceBytes),
-				keyPair
-			);
-		},
-		encrypt: async (plaintext: Uint8Array, publicKey: Uint8Array) => {
-			const nonce			= potassiumUtil.randomBytes(this.helpers.nonceBytes);
-			const cyphertext	= await this.helpers.seal(plaintext, nonce, publicKey);
-			return potassiumUtil.concatMemory(true, nonce, cyphertext);
-		}
-	};
-
-	/** @ignore */
-	private readonly helpers: {
-		keyPair: () => Promise<IKeyPair>;
-		nonceBytes: number;
-		open: (
-			cyphertext: Uint8Array,
-			nonce: Uint8Array,
-			keyPair: IKeyPair
-		) => Promise<Uint8Array>;
-		privateKeyBytes: number;
-		publicKeyBytes: number;
-		seal: (
-			plaintext: Uint8Array,
-			nonce: Uint8Array,
-			publicKey: Uint8Array
-		) => Promise<Uint8Array>;
-	}	= {
-		keyPair: async () : Promise<IKeyPair> =>
-			this.isNative ?
-				NativeCrypto.box.keyPair() :
-				sodium.crypto_box_curve25519xchacha20poly1305_keypair()
+		keyPair: this.isNative ?
+			 async () => NativeCrypto.box.keyPair() :
+			 async () => sodium.crypto_box_curve25519xchacha20poly1305_keypair()
 		,
 
-		nonceBytes:
-			this.isNative ?
+		nonceBytes: this.isNative ?
 				NativeCrypto.secretBox.nonceBytes :
 				sodium.crypto_box_curve25519xchacha20poly1305_NONCEBYTES
 		,
 
-		open: async (
-			cyphertext: Uint8Array,
-			nonce: Uint8Array,
-			keyPair: IKeyPair
-		) : Promise<Uint8Array> =>
-			this.isNative ?
-				NativeCrypto.box.open(
-					cyphertext,
-					nonce,
-					keyPair
-				) :
+		decrypt: this.isNative ?
+			async (cyphertext: Uint8Array, keyPair: IKeyPair) =>
+				NativeCrypto.box.open(cyphertext, keyPair)
+			:
+			async (cyphertext: Uint8Array, keyPair: IKeyPair) =>
 				sodium.crypto_box_curve25519xchacha20poly1305_seal_open(
 					cyphertext,
 					keyPair.publicKey,
@@ -74,47 +35,36 @@ export class Box implements IBox {
 				)
 		,
 
-		privateKeyBytes:
-			this.isNative ?
-				NativeCrypto.box.privateKeyBytes :
-				sodium.crypto_box_curve25519xchacha20poly1305_SECRETKEYBYTES
+		encrypt: this.isNative ?
+			async (plaintext: Uint8Array, publicKey: Uint8Array) =>
+				NativeCrypto.box.seal(plaintext, publicKey)
+			:
+			async (plaintext: Uint8Array, publicKey: Uint8Array) =>
+				sodium.crypto_box_curve25519xchacha20poly1305_seal(plaintext, publicKey)
 		,
 
-		publicKeyBytes:
-			this.isNative ?
-				NativeCrypto.box.publicKeyBytes :
-				sodium.crypto_box_curve25519xchacha20poly1305_PUBLICKEYBYTES
+		privateKeyBytes: this.isNative ?
+			NativeCrypto.box.privateKeyBytes :
+			sodium.crypto_box_curve25519xchacha20poly1305_SECRETKEYBYTES
 		,
 
-		seal: async (
-			plaintext: Uint8Array,
-			nonce: Uint8Array,
-			publicKey: Uint8Array
-		) : Promise<Uint8Array> =>
-			this.isNative ?
-				NativeCrypto.box.seal(
-					plaintext,
-					nonce,
-					publicKey
-				) :
-				sodium.crypto_box_curve25519xchacha20poly1305_seal(
-					plaintext,
-					publicKey
-				)
+		publicKeyBytes: this.isNative ?
+			NativeCrypto.box.publicKeyBytes :
+			sodium.crypto_box_curve25519xchacha20poly1305_PUBLICKEYBYTES
 	};
 
 	/** @inheritDoc */
 	public readonly privateKeyBytes: Promise<number>	= (async () =>
 		(await mceliece.privateKeyBytes) +
 		(await ntru.privateKeyBytes) +
-		this.helpers.privateKeyBytes
+		this.classicalCypher.privateKeyBytes
 	)();
 
 	/** @inheritDoc */
 	public readonly publicKeyBytes: Promise<number>		= (async () =>
 		(await mceliece.publicKeyBytes) +
 		(await ntru.publicKeyBytes) +
-		this.helpers.publicKeyBytes
+		this.classicalCypher.publicKeyBytes
 	)();
 
 	/** @ignore */
@@ -211,16 +161,16 @@ export class Box implements IBox {
 			classical: potassiumUtil.toBytes(
 				privateKey,
 				undefined,
-				this.helpers.privateKeyBytes
+				this.classicalCypher.privateKeyBytes
 			),
 			mceliece: potassiumUtil.toBytes(
 				privateKey,
-				this.helpers.privateKeyBytes,
+				this.classicalCypher.privateKeyBytes,
 				await mceliece.privateKeyBytes
 			),
 			ntru: potassiumUtil.toBytes(
 				privateKey,
-				this.helpers.privateKeyBytes + await mceliece.privateKeyBytes,
+				this.classicalCypher.privateKeyBytes + await mceliece.privateKeyBytes,
 				await ntru.privateKeyBytes
 			)
 		};
@@ -236,16 +186,16 @@ export class Box implements IBox {
 			classical: potassiumUtil.toBytes(
 				publicKey,
 				undefined,
-				this.helpers.publicKeyBytes
+				this.classicalCypher.publicKeyBytes
 			),
 			mceliece: potassiumUtil.toBytes(
 				publicKey,
-				this.helpers.publicKeyBytes,
+				this.classicalCypher.publicKeyBytes,
 				await mceliece.publicKeyBytes
 			),
 			ntru: potassiumUtil.toBytes(
 				publicKey,
-				this.helpers.publicKeyBytes + await mceliece.publicKeyBytes,
+				this.classicalCypher.publicKeyBytes + await mceliece.publicKeyBytes,
 				await ntru.publicKeyBytes
 			)
 		};
@@ -254,7 +204,7 @@ export class Box implements IBox {
 	/** @inheritDoc */
 	public async keyPair () : Promise<IKeyPair> {
 		const keyPairs	= {
-			classical: await this.helpers.keyPair(),
+			classical: await this.classicalCypher.keyPair(),
 			mceliece: await mceliece.keyPair(),
 			ntru: await ntru.keyPair()
 		};
