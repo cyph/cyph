36792d144799812eefbb0dd789b5a23db7426e40
Ryan Lester <hacker@linux.com>
2017-07-13 23:06:03 -0400

    minor cleanup

---
+++ shared/js/cyph/files/session-transfer-answers.ts
@@ -0,0 +1,9 @@
+import {SessionTransfer} from '../../proto';
+
+
+/** @see SessionTransfer.Answers */
+/* tslint:disable-next-line:variable-name */
+export const SessionTransferAnswers	= SessionTransfer.Answers;
+
+/** @see SessionTransfer.Answers */
+export type SessionTransferAnswers	= SessionTransfer.Answers;
