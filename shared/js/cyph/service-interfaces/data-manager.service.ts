5e032f6d83e99da701782a0f9fb8836f638d0494
Ryan Lester <hacker@linux.com>
2017-07-15 19:12:46 -0400

    DataManagerService.getOrSetDefault

--- shared/js/cyph/service-interfaces/data-manager.service.ts
+++ shared/js/cyph/service-interfaces/data-manager.service.ts
@@ -11,6 +11,22 @@ export class DataManagerService {
 		throw new Error('Must provide an implementation of getItem.');
 	}
 
+	/** Gets a value and sets a default value if none had previously been set. */
+	public async getOrSetDefault<T> (
+		url: string,
+		proto: IProto<T>,
+		defaultValue: () => T|Promise<T>
+	) : Promise<T> {
+		try {
+			return await this.getItem(url, proto);
+		}
+		catch (_) {
+			const value	= await defaultValue();
+			this.setItem(url, proto, value).catch(() => {});
+			return value;
+		}
+	}
+
 	/** Checks whether an item exists. */
 	public async hasItem (url: string) : Promise<boolean> {
 		try {
