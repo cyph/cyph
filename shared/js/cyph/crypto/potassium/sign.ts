fea11fd0d1ece1aea769c9d9d77c0cc7d06c2d25
Ryan Lester <hacker@linux.com>
2017-07-15 04:49:30 -0400

    expose supersphincs additionalData option

--- shared/js/cyph/crypto/potassium/sign.ts
+++ shared/js/cyph/crypto/potassium/sign.ts
@@ -27,34 +27,38 @@ export class Sign implements ISign {
 	/** @inheritDoc */
 	public async open (
 		signed: Uint8Array|string,
-		publicKey: Uint8Array
+		publicKey: Uint8Array,
+		additionalData?: Uint8Array|string
 	) : Promise<Uint8Array> {
-		return superSphincs.open(signed, publicKey);
+		return superSphincs.open(signed, publicKey, additionalData);
 	}
 
 	/** @inheritDoc */
 	public async sign (
 		message: Uint8Array|string,
-		privateKey: Uint8Array
+		privateKey: Uint8Array,
+		additionalData?: Uint8Array|string
 	) : Promise<Uint8Array> {
-		return superSphincs.sign(message, privateKey);
+		return superSphincs.sign(message, privateKey, additionalData);
 	}
 
 	/** @inheritDoc */
 	public async signDetached (
 		message: Uint8Array|string,
-		privateKey: Uint8Array
+		privateKey: Uint8Array,
+		additionalData?: Uint8Array|string
 	) : Promise<Uint8Array> {
-		return superSphincs.signDetached(message, privateKey);
+		return superSphincs.signDetached(message, privateKey, additionalData);
 	}
 
 	/** @inheritDoc */
 	public async verifyDetached (
 		signature: Uint8Array|string,
 		message: Uint8Array|string,
-		publicKey: Uint8Array
+		publicKey: Uint8Array,
+		additionalData?: Uint8Array|string
 	) : Promise<boolean> {
-		return superSphincs.verifyDetached(signature, message, publicKey);
+		return superSphincs.verifyDetached(signature, message, publicKey, additionalData);
 	}
 
 	constructor () {}
