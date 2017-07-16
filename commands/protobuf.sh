5ded1de89140f729667f141d8ce2d65a84818749
Ryan Lester <hacker@linux.com>
2017-07-16 18:21:08 -0400

    https://github.com/dcodeIO/protobuf.js/issues/863 temporary workaround improvement

--- commands/protobuf.sh
+++ commands/protobuf.sh
@@ -11,7 +11,10 @@ pbjs -t static-module types.proto -o shared/js/proto/index.js
 checkfail
 
 # Temporary workaround for https://github.com/dcodeIO/protobuf.js/issues/863
-while ! tsc shared/js/proto/index.d.ts &> /dev/null ; do
+while \
+	[ $({ wc -l shared/js/proto/index.d.ts 2> /dev/null || echo 0; } | awk '{print $1}') -lt 2000 ] && \
+	! tsc shared/js/proto/index.d.ts &> /dev/null \
+; do
 	pbts shared/js/proto/index.js -o shared/js/proto/index.d.ts
 	checkfail
 done
