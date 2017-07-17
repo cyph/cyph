7cfa65a375f9004c9ba28f320cae44273db54576
Ryan Lester <hacker@linux.com>
2017-07-17 13:43:59 -0400

    workaround for LZ4 issue / probable minor performance improvement

--- commands/getlibs.sh
+++ commands/getlibs.sh
@@ -182,7 +182,7 @@ mkdir -p @types/lz4
 cat > @types/lz4/index.d.ts << EOM
 declare module 'lz4' {
 	const decode: (a: Uint8Array) => Uint8Array;
-	const encode: (a: Uint8Array) => Uint8Array;
+	const encode: (a: Uint8Array, opts?: {streamChecksum: boolean}) => Uint8Array;
 }
 EOM
 
