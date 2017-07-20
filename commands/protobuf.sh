4ff88dd78a387809a720e0c4d3199ea63928cb0e
Ryan Lester <hacker@linux.com>
2017-07-20 19:36:21 -0400

    better protobuf/circleci fix

--- commands/protobuf.sh
+++ commands/protobuf.sh
@@ -11,10 +11,15 @@ pbjs -t static-module types.proto -o shared/js/proto/index.js
 checkfail
 
 # Temporary workaround for https://github.com/dcodeIO/protobuf.js/issues/863
-while \
-	[ $({ wc -l shared/js/proto/index.d.ts 2> /dev/null || echo 0; } | awk '{print $1}') -lt 2700 ] && \
-	! tsc shared/js/proto/index.d.ts &> /dev/null \
-; do
+while
+	{
+		cat shared/js/proto/index.d.ts | tr '\n' ' ' | grep -P "$(
+			grep -oP '^\s*message [A-Za-z0-9]+' types.proto | awk '{print $2}' | tr '\n' '☁' | sed 's|☁|.*|g'
+		)" ||
+		echo 0;
+	} &> /dev/null && 
+	! tsc shared/js/proto/index.d.ts &> /dev/null
+do
 	pbts shared/js/proto/index.js -o shared/js/proto/index.d.ts
 	checkfail
 done
