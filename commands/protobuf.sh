b5d786cff932ed4df43136945a3bc92ab8398fcb
Ryan Lester <hacker@linux.com>
2017-07-25 14:35:40 -0700

    protobuf.sh fix

--- commands/protobuf.sh
+++ commands/protobuf.sh
@@ -12,15 +12,12 @@ checkfail
 
 # Temporary workaround for https://github.com/dcodeIO/protobuf.js/issues/863
 while
-	{
-		cat shared/js/proto/index.d.ts | tr '\n' ' ' | grep -P "$(
-			grep -oP '^\s*message [A-Za-z0-9]+' types.proto |
-				awk '{print $2}' |
-				tr '\n' '~' |
-				sed 's|~| { .* |g'
-		)" ||
-		echo 0;
-	} &> /dev/null && 
+	! cat shared/js/proto/index.d.ts 2> /dev/null | tr '\n' ' ' | grep -P "$(
+		grep -oP '^\s*message [A-Za-z0-9]+' types.proto |
+			awk '{print $2}' |
+			tr '\n' '~' |
+			sed 's|~| { .* |g'
+	)" &> /dev/null ||
 	! tsc shared/js/proto/index.d.ts &> /dev/null
 do
 	pbts shared/js/proto/index.js -o shared/js/proto/index.d.ts
