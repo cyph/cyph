9ff1d3acfbf976c344eb194b3c73197991214852
Ryan Lester <hacker@linux.com>
2017-07-26 18:45:33 -0700

    merge ISession into ISessionService

--- shared/js/cyph/service-interfaces/isession.service.ts
+++ shared/js/cyph/service-interfaces/isession.service.ts
@@ -1,12 +1,13 @@
 import {BehaviorSubject, Observable} from 'rxjs';
-import {ISession} from '../session';
-import {ProFeatures} from '../session/profeatures';
+import {IHandshakeState} from '../crypto/castle/ihandshake-state';
+import {CastleEvents, ISessionMessageAdditionalData, ProFeatures} from '../session';
 
 
 /**
- * Manages a session.
+ * Encapsulates an end-to-end encrypted communication session.
+ * This is the entire non-UI representation of a cyph.
  */
-export interface ISessionService extends ISession {
+export interface ISessionService {
 	/** API flags passed into this session. */
 	readonly apiFlags: {
 		forceTURN: boolean;
@@ -30,6 +31,64 @@ export interface ISessionService extends ISession {
 	/** Remote username (e.g. "friend" or "alice"). */
 	readonly remoteUsername: BehaviorSubject<string>;
 
+	/** State of the cyph (referenced by UI). */
+	readonly state: {
+		cyphID: string;
+		isAlice: boolean;
+		isAlive: boolean;
+		sharedSecret: string;
+		startingNewCyph?: boolean;
+		wasInitiatedByAPI: boolean;
+	};
+
+	/** Castle event handler called by Castle.Transport. */
+	castleHandler (
+		event: CastleEvents,
+		data?: Uint8Array|{author: Observable<string>; plaintext: Uint8Array; timestamp: number}
+	) : Promise<void>;
+
+	/** This kills the cyph. */
+	close () : void;
+
+	/** @see IHandshakeState */
+	handshakeState () : Promise<IHandshakeState>;
+
 	/** Initializes service. */
 	init (channelID: string, userID?: string) : void;
+
+	/** @see ChannelService.lock */
+	lock<T> (f: (reason?: string) => Promise<T>, reason?: string) : Promise<T>;
+
+	/**
+	 * Remove event listener.
+	 * @param event
+	 * @param handler
+	 */
+	off<T> (event: string, handler: (data: T) => void) : void;
+
+	/**
+	 * Add event listener.
+	 * @param event
+	 * @param handler
+	 */
+	on<T> (event: string, handler: (data: T) => void) : void;
+
+	/**
+	 * Returns first occurrence of event.
+	 * @param event
+	 */
+	one<T> (event: string) : Promise<T>;
+
+	/**
+	 * Send at least one message through the session.
+	 * @param messages
+	 */
+	send (...messages: [string, ISessionMessageAdditionalData][]) : void;
+
+	/**
+	 * Trigger event, passing in optional data.
+	 * @param event
+	 * @param data
+	 */
+	trigger (event: string, data?: any) : void;
 }
--- shared/js/cyph/session/index.ts
+++ shared/js/cyph/session/index.ts
@@ -1,6 +1,5 @@
 export * from './enums';
 export * from './ichannel-handlers';
-export * from './isession';
 export * from './isession-message-additional-data';
 export * from './isession-message-data';
 export * from './profeatures';
