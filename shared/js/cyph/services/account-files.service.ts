189d3f17f957d986cd0f1ac787871938e3e3ee6f
Josh Boehm <josh@cyph.com>
2017-08-05 20:03:31 -0700

    Added file preview for images

--- shared/js/cyph/services/account-files.service.ts
+++ shared/js/cyph/services/account-files.service.ts
@@ -168,6 +168,17 @@ export class AccountFilesService {
 		return this.noteSnippets.get(id) || '';
 	}
 
+	/** Previews image files */
+	public async openFile (id: string) : Promise<void> {
+		const file	= await this.getFile(id);
+		if (file.mediaType.indexOf('image/') === 0) {
+			this.dialogService.image(await this.downloadURI(id).result);
+		}
+		if (file.mediaType.indexOf('image/') === 1) {
+			this.downloadAndSave(id);
+		}
+	}
+
 	/** Removes a file. */
 	public async remove (
 		id: string|IAccountFileRecord|Promise<IAccountFileRecord>,
