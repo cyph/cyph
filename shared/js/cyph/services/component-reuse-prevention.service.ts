9df288869d0854c9acd08b80b33f0558bd338a6c
Ryan Lester <hacker@linux.com>
2017-09-12 17:31:37 -0400

    generate ComponentReusePreventionService

---
+++ shared/js/cyph/services/component-reuse-prevention.service.ts
@@ -0,0 +1,10 @@
+import {Injectable} from '@angular/core';
+
+
+/**
+ * Angular service for component reuse prevention.
+ */
+@Injectable()
+export class ComponentReusePreventionService {
+	constructor () {}
+}
