e260e17a96d7efc98ae0b94c47774fb2e6f58861
Ryan Lester <hacker@linux.com>
2017-09-25 22:11:16 -0700

    generate ClaimUsernameComponent

---
+++ shared/js/cyph/components/claim-username.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+
+/**
+ * Angular component for claim username UI.
+ */
+@Component({
+	selector: 'cyph-claim-username',
+	styleUrls: ['../../../css/components/claim-username.scss'],
+	templateUrl: '../../../templates/claim-username.html'
+})
+export class ClaimUsernameComponent {
+	constructor () {}
+}
