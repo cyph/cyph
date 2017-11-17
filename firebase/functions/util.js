68a11370bcc747e9b3fbcfd43c84bd1c4e0a6f89
Ryan Lester <hacker@linux.com>
2017-11-17 17:40:53 -0500

    ACCOUNTS-25 minor fixes

--- firebase/functions/util.js
+++ firebase/functions/util.js
@@ -1,3 +1,10 @@
+global.crypto	= require('crypto');
+
+crypto.getRandomValues = (arr) => {
+	arr.set(crypto.randomBytes(arr.byteLength));
+	return arr;
+};
+
 require('./js/standalone/global');
 require('./js/cyph/util');
 
--- modules/util.js
+++ modules/util.js
@@ -1,3 +1,10 @@
+global.crypto	= require('crypto');
+
+crypto.getRandomValues = (arr) => {
+	arr.set(crypto.randomBytes(arr.byteLength));
+	return arr;
+};
+
 require('child_process').spawnSync('bash', ['../commands/buildunbundledassets.sh']);
 require('../shared/assets/js/standalone/global');
 require('../shared/assets/js/cyph/util');
