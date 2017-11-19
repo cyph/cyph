8dd3b0c2e8546626aa32cf4c389d8a90366bfac0
Ryan Lester <hacker@linux.com>
2017-11-19 03:27:15 +0000

    generate PinInputComponent

---
+++ shared/js/cyph/components/pin-input.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+
+/**
+ * Angular component for pin input UI.
+ */
+@Component({
+	selector: 'cyph-pin-input',
+	styleUrls: ['../../../css/components/pin-input.scss'],
+	templateUrl: '../../../templates/pin-input.html'
+})
+export class PinInputComponent {
+	constructor () {}
+}
