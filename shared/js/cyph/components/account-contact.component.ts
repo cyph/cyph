75d908a86165ca5a93bffb7a82e3be362e033278
Ryan Lester <hacker@linux.com>
2017-11-24 13:38:06 +0000

    generate AccountContactComponent

---
+++ shared/js/cyph/components/account-contact.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+
+/**
+ * Angular component for account contact UI.
+ */
+@Component({
+	selector: 'cyph-account-contact',
+	styleUrls: ['../../../css/components/account-contact.scss'],
+	templateUrl: '../../../templates/account-contact.html'
+})
+export class AccountContactComponent {
+	constructor () {}
+}
