b5caa09797ec613f774a43cb97f25ee8edc509d6
Ryan Lester <hacker@linux.com>
2017-11-27 03:11:36 +0000

    generate BlankComponent

---
+++ shared/js/cyph/components/blank.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+
+/**
+ * Angular component for blank UI.
+ */
+@Component({
+	selector: 'cyph-blank',
+	styleUrls: ['../../../css/components/blank.scss'],
+	templateUrl: '../../../templates/blank.html'
+})
+export class BlankComponent {
+	constructor () {}
+}
