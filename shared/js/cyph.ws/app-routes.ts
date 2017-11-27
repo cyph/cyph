eeca5c686a9d0b1809ec4a1339aa2fd54004bd67
Ryan Lester <hacker@linux.com>
2017-11-26 22:10:47 -0500

    Revert "/#telehealth and /#extension fix"
    This reverts commit 749699c622a0ca4796f7b28e0057050928f95c06.

--- shared/js/cyph.ws/app-routes.ts
+++ shared/js/cyph.ws/app-routes.ts
@@ -1,6 +1,7 @@
 /* tslint:disable:object-literal-sort-keys */
 
 import {Routes} from '@angular/router';
+import {AccountComponent} from '../cyph/components/account.component';
 import {account, retry} from '../cyph/routes';
 import {AppService} from './app.service';
 import {EphemeralChatRootComponent} from './ephemeral-chat-root.component';
@@ -12,5 +13,13 @@ account.canActivate	= [AppService];
 export const appRoutes: Routes	= [
 	retry,
 	account,
+	{
+		path: 'extension',
+		children: [{path: '**', component: AccountComponent}]
+	},
+	{
+		path: 'telehealth',
+		children: [{path: '**', component: AccountComponent}]
+	},
 	{path: '**', canActivate: [AppService], component: EphemeralChatRootComponent}
 ];
