484ba09ea82e3d0c5d72b12f249d82b8cb85f65d
Ryan Lester <hacker@linux.com>
2017-11-27 16:55:33 +0000

    generate ChatComposeComponent

---
+++ shared/js/cyph/components/chat-compose.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+
+/**
+ * Angular component for chat compose UI.
+ */
+@Component({
+	selector: 'cyph-chat-compose',
+	styleUrls: ['../../../css/components/chat-compose.scss'],
+	templateUrl: '../../../templates/chat-compose.html'
+})
+export class ChatComposeComponent {
+	constructor () {}
+}
