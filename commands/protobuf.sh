4939e68ee31e79f39cba5c1d1c713028c842f718
Ryan Lester <hacker@linux.com>
2017-11-28 08:47:04 -0500

    remove temporary workaround for https://github.com/dcodeIO/protobuf.js/issues/863

--- commands/protobuf.sh
+++ commands/protobuf.sh
@@ -9,19 +9,6 @@ rm -rf shared/js/proto 2> /dev/null
 mkdir shared/js/proto
 pbjs -t static-module types.proto -o shared/js/proto/index.js
 checkfail
-
-# Temporary workaround for https://github.com/dcodeIO/protobuf.js/issues/863
-while
-	! cat shared/js/proto/index.d.ts 2> /dev/null | tr '\n' ' ' | grep -P "$(
-		grep -oP '^\s*message [A-Za-z0-9]+' types.proto |
-			awk '{print $2}' |
-			tr '\n' '~' |
-			sed 's|~| { .* |g'
-	)" &> /dev/null ||
-	! tsc shared/js/proto/index.d.ts &> /dev/null
-do
-	pbts shared/js/proto/index.js -o shared/js/proto/index.d.ts
-	checkfail
-done
-
+pbts shared/js/proto/index.js -o shared/js/proto/index.d.ts
+checkfail
 sed -i 's/|null/|undefined/g' shared/js/proto/index.d.ts
