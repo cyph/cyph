6fe54728d238445a1e01a974ed62a662d2de8bc5
Ryan Lester <hacker@linux.com>
2017-12-01 18:58:08 +0000

    generate ChatMessageGeometryService

---
+++ shared/js/cyph/services/chat-message-geometry.service.ts
@@ -0,0 +1,10 @@
+import {Injectable} from '@angular/core';
+
+
+/**
+ * Angular service for chat message geometry.
+ */
+@Injectable()
+export class ChatMessageGeometryService {
+	constructor () {}
+}
