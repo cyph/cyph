5f231a22f0af46d73f131cec149b2b8956753d87
Ryan Lester <hacker@linux.com>
2017-12-04 12:58:49 -0500

    better threadpacking for minifcation

--- commands/websign/threadpack.js
+++ commands/websign/threadpack.js
@@ -16,12 +16,11 @@ fs.writeFileSync(
 	args.path,
 	fs.readFileSync(args.path).toString().replace(
 		/importScripts\(["'](.*?)["']\)/g,
-		(_, value) =>
-			'\n\n' +
+		(_, value) => '(function () {' +
 			fs.readFileSync(
 				value.slice(value[0] === '/' ? 1 : 0).split('?')[0]
 			).toString() +
-			'\n\n'
+		'}).call(this)'
 	)
 );
 
