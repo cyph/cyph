83eacf4f4dde290a354d542b017b38f95fb8e443
Ryan Lester <hacker@linux.com>
2017-12-28 21:10:47 -0500

    lint.sh --html-only

--- commands/lint.sh
+++ commands/lint.sh
@@ -5,8 +5,12 @@ cd $(cd "$(dirname "$0")" ; pwd)/..
 dir="$PWD"
 
 fast=''
+htmlOnly=''
 if [ "${1}" == '--fast' ] ; then
 	fast=true
+	shift
+elif [ "${1}" == '--html-only' ] ; then
+	htmlOnly=true
 	shift
 fi
 
@@ -15,7 +19,7 @@ log 'Starting lint'
 if [ "${fast}" ] ; then
 	./commands/protobuf.sh
 	checkfail
-else
+elif [ ! "${htmlOnly}" ] ; then
 	output="$(./commands/buildunbundledassets.sh 2>&1)"
 	checkfail "${output}"
 fi
@@ -24,105 +28,110 @@ tmpDir="$(mktemp -d)"
 ./commands/copyworkspace.sh "${tmpDir}"
 cd "${tmpDir}/shared"
 
-# WebSign hash whitelist check
+if [ ! "${htmlOnly}" ] ; then
+	# WebSign hash whitelist check
 
-grep $(../commands/websign/bootstraphash.sh) ../websign/hashwhitelist.json > /dev/null
-checkfail 'WebSign hash whitelist check fail'
+	grep $(../commands/websign/bootstraphash.sh) ../websign/hashwhitelist.json > /dev/null
+	checkfail 'WebSign hash whitelist check fail'
 
-# Validate component template/stylesheet count consistency
+	# Validate component template/stylesheet count consistency
 
-componentConsistency="$(
-	node -e '
-		const glob	= require("glob");
+	componentConsistency="$(
+		node -e '
+			const glob	= require("glob");
 
-		/* Used to offset web AppComponents not having component stylesheets */
-		const webIndexHtml		= glob.sync("templates/**", {nodir: true}).filter(s =>
-			s.endsWith("/index.html") && s.indexOf("/native/") < 0
+			/* Used to offset web AppComponents not having component stylesheets */
+			const webIndexHtml		= glob.sync("templates/**", {nodir: true}).filter(s =>
+				s.endsWith("/index.html") && s.indexOf("/native/") < 0
+			);
+
+			const webTemplates		= glob.sync("templates/**", {nodir: true}).filter(s =>
+				s.endsWith(".html") && s.indexOf("/native/") < 0
+			);
+
+			const nativeTemplates	= glob.sync("templates/native/**", {nodir: true}).filter(s =>
+				s.endsWith(".html") && s.indexOf("/app/") < 0
+			);
+
+			const webStylesheets	= glob.sync("css/components/**", {nodir: true}).filter(s =>
+				s.endsWith(".scss")
+			).concat(
+				webIndexHtml
+			);
+
+			const nativeStylesheets	= glob.sync(
+				"css/native/components/**",
+				{nodir: true}
+			).filter(s =>
+				s.endsWith(".scss")
+			).concat(
+				webIndexHtml
+			);
+
+			console.log(
+				[webTemplates, nativeTemplates, webStylesheets, nativeStylesheets].
+					map(a => a.length).
+					reduce((a, b) => a === b ? a : -1)
+				!== -1
+			);
+		'
+	)"
+
+	if [ "${componentConsistency}" != true ] ; then
+		fail 'Component template/stylesheet count mismatch'
+	fi
+
+	# tslint
+
+	cd js
+	bindmount /node_modules node_modules
+	mv tslint.json tslint.json.old
+	cat tslint.json.old | grep -v tslint-microsoft-contrib > tslint.json
+	checkTslintAllOutput="$(check-tslint-all 2>&1)"
+	checkfail "${checkTslintAllOutput}"
+	mv tslint.json.old tslint.json
+	unbindmount node_modules
+	cd ..
+
+	tsc --skipLibCheck js/tslint-rules/*.ts || exit 1
+
+	node -e "
+		const tsconfig	= JSON.parse(
+			fs.readFileSync('js/tsconfig.json').toString().
+				split('\n').
+				filter(s => s.trim()[0] !== '/').
+				join('\n')
 		);
 
-		const webTemplates		= glob.sync("templates/**", {nodir: true}).filter(s =>
-			s.endsWith(".html") && s.indexOf("/native/") < 0
-		);
+		/* Pending Angular AOT fix */
+		tsconfig.compilerOptions.noUnusedParameters	= true;
 
-		const nativeTemplates	= glob.sync("templates/native/**", {nodir: true}).filter(s =>
-			s.endsWith(".html") && s.indexOf("/app/") < 0
-		);
+		tsconfig.compilerOptions.paths	= undefined;
 
-		const webStylesheets	= glob.sync("css/components/**", {nodir: true}).filter(s =>
-			s.endsWith(".scss")
-		).concat(
-			webIndexHtml
-		);
+		tsconfig.files	=
+			'$(cd js ; find . -type f -name '*.ts' | tr '\n' ' ')typings/index.d.ts'.split(' ')
+		;
 
-		const nativeStylesheets	= glob.sync("css/native/components/**", {nodir: true}).filter(s =>
-			s.endsWith(".scss")
-		).concat(
-			webIndexHtml
+		fs.writeFileSync(
+			'js/tsconfig.tslint.json',
+			JSON.stringify(tsconfig)
 		);
+	"
 
-		console.log(
-			[webTemplates, nativeTemplates, webStylesheets, nativeStylesheets].
-				map(a => a.length).
-				reduce((a, b) => a === b ? a : -1)
-			!== -1
-		);
-	'
-)"
+	cp ${dir}/shared/lib/js/package.json ./
+	cp -rf css templates js/
 
-if [ "${componentConsistency}" != true ] ; then
-	fail 'Component template/stylesheet count mismatch'
+	output="$(
+		tslint \
+			-e '/node_modules/**' \
+			--project js/tsconfig.tslint.json \
+		2>&1 |
+			if [ "${fast}" ] ; then grep -v js/native/js ; else cat - ; fi |
+			grep -vP "Warning: Cannot read property '.*?' of undefined"
+	)"
 fi
 
-# tslint
-
-cd js
-bindmount /node_modules node_modules
-mv tslint.json tslint.json.old
-cat tslint.json.old | grep -v tslint-microsoft-contrib > tslint.json
-checkTslintAllOutput="$(check-tslint-all 2>&1)"
-checkfail "${checkTslintAllOutput}"
-mv tslint.json.old tslint.json
-unbindmount node_modules
-cd ..
-
-tsc --skipLibCheck js/tslint-rules/*.ts || exit 1
-
-node -e "
-	const tsconfig	= JSON.parse(
-		fs.readFileSync('js/tsconfig.json').toString().
-			split('\n').
-			filter(s => s.trim()[0] !== '/').
-			join('\n')
-	);
-
-	/* Pending Angular AOT fix */
-	tsconfig.compilerOptions.noUnusedParameters	= true;
-
-	tsconfig.compilerOptions.paths	= undefined;
-
-	tsconfig.files	=
-		'$(cd js ; find . -type f -name '*.ts' | tr '\n' ' ')typings/index.d.ts'.split(' ')
-	;
-
-	fs.writeFileSync(
-		'js/tsconfig.tslint.json',
-		JSON.stringify(tsconfig)
-	);
-"
-
-cp ${dir}/shared/lib/js/package.json ./
-cp -rf css templates js/
-
-output="$(
-	tslint \
-		-e '/node_modules/**' \
-		--project js/tsconfig.tslint.json \
-	2>&1 |
-		if [ "${fast}" ] ; then grep -v js/native/js ; else cat - ; fi |
-		grep -vP "Warning: Cannot read property '.*?' of undefined"
-)"
-
-if [ ! "${fast}" ] ; then
+if [ "${htmlOnly}" ] || [ ! "${fast}" ] ; then
 	# htmllint
 
 	output="${output}$({
@@ -145,7 +154,8 @@ if [ ! "${fast}" ] ; then
 			})()' \
 		\;;
 	} 2>&1)"
-
+fi
+if [ ! "${htmlOnly}" ] && [ ! "${fast}" ] ; then
 	# Retire.js
 
 	cd ..
