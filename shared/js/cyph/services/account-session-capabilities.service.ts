4514b31337d91dc640d9d005904e3dee2a40fbba
Ryan Lester <hacker@linux.com>
2018-01-02 09:57:29 +0000

    generate AccountSessionCapabilitiesService

---
+++ shared/js/cyph/services/account-session-capabilities.service.ts
@@ -0,0 +1,10 @@
+import {Injectable} from '@angular/core';
+
+
+/**
+ * Angular service for account session capabilities.
+ */
+@Injectable()
+export class AccountSessionCapabilitiesService {
+	constructor () {}
+}
