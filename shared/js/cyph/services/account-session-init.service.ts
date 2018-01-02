6a9c743fb212585a1f0bf59ec9fc2455f629ffb3
Ryan Lester <hacker@linux.com>
2018-01-02 00:22:49 +0000

    generate AccountSessionInitService

---
+++ shared/js/cyph/services/account-session-init.service.ts
@@ -0,0 +1,10 @@
+import {Injectable} from '@angular/core';
+
+
+/**
+ * Angular service for account session init.
+ */
+@Injectable()
+export class AccountSessionInitService {
+	constructor () {}
+}
