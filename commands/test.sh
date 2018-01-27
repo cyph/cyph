feb97d0dd08849a7e7fed0b09f7cd1a51b146181
Ryan Lester <hacker@linux.com>
2018-01-27 13:55:18 -0500

    test.sh fixes

--- commands/test.sh
+++ commands/test.sh
@@ -8,7 +8,12 @@ e2e=''
 unit=''
 if [ "${1}" == '--e2e' ] ; then
 	e2e=true
+	shift
 elif [ "${1}" == '--unit' ] ; then
+	unit=true
+	shift
+else
+	e2e=true
 	unit=true
 fi
 
@@ -20,13 +25,14 @@ export CHROME_BIN="$(node -e 'console.log(require("puppeteer").executablePath())
 cd cyph.ws
 ../commands/ngprojectinit.sh
 
-if [ ! "${e2e}" ] ; then
-	ng test --browsers ChromeHeadless
+if [ "${unit}" ] ; then
+	ng test --browsers ChromeHeadless --single-run
 	checkfail
+	echo -e '\n\n\n'
 fi
 
-if [ ! "${unit}" ] ; then
-	../commands/serve.sh "${@}" cyph.ws
+if [ "${e2e}" ] ; then
+	../commands/serve.sh --e2e "${@}" cyph.ws
 	checkfail
 fi
 
