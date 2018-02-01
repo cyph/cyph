01b51a5ea97932e847e69799234e4eb3dcf10d18
Ryan Lester <hacker@linux.com>
2018-02-01 03:15:36 +0000

    generate AccountOrganizationsService

---
+++ shared/js/cyph/services/account-organizations.service.ts
@@ -0,0 +1,10 @@
+import {Injectable} from '@angular/core';
+
+
+/**
+ * Angular service for account organizations.
+ */
+@Injectable()
+export class AccountOrganizationsService {
+	constructor () {}
+}
