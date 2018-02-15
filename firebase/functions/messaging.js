f535fbade432bdf1f49e387f67abcfe3e7c507ba
Ryan Lester <hacker@linux.com>
2018-02-14 23:43:03 -0500

    ACCOUNTS-26 remove expired tokens in Cloud Function

--- firebase/functions/messaging.js
+++ firebase/functions/messaging.js
@@ -1,13 +1,23 @@
-const sendMessage	= async (database, messaging, url, body) => messaging.sendToDevice(
-	Object.keys(database.ref(url).val() || {}),
-	{
+const sendMessage	= async (database, messaging, url, body) => {
+	const ref		= database.ref(url);
+	const tokens	= Object.keys(ref.val() || {});
+
+	const response	= await messaging.sendToDevice(tokens, {
 		notification: {
 			body,
 			icon: 'https://www.cyph.com/assets/img/favicon/favicon-256x256.png',
 			title: 'Cyph'
 		}
-	}
-);
+	});
+
+	await Promise.all(
+		results.
+			filter(result => !!result.error).
+			map(async (_, i) => ref.child(tokens[i]).remove())
+	).catch(
+		() => {}
+	);
+};
 
 
 module.exports	= {sendMessage};
