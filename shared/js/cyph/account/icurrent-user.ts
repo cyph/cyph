cb89308e899e996079bdd21cf3881e8d01e34a6e
Ryan Lester <hacker@linux.com>
2018-02-16 23:16:05 -0500

    docs

--- shared/js/cyph/account/icurrent-user.ts
+++ shared/js/cyph/account/icurrent-user.ts
@@ -2,11 +2,15 @@ import {IKeyPair} from '../proto';
 import {User} from './user';
 
 
+/** Current user data. */
 export interface ICurrentUser {
+	/** User secret keys. */
 	keys: {
 		encryptionKeyPair: IKeyPair;
 		signingKeyPair: IKeyPair;
 		symmetricKey: Uint8Array;
 	};
+
+	/** @see User */
 	user: User;
 }
