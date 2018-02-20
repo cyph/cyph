cc259873ca10433d2cc2ce3d448357c62361ae86
Josh Boehm <josh@cyph.com>
2018-02-20 14:39:34 -0800

    Added appointment info & doctor picture to waiting room

--- shared/js/cyph/components/account-call-waiting/account-call-waiting.component.ts
+++ shared/js/cyph/components/account-call-waiting/account-call-waiting.component.ts
@@ -1,6 +1,9 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {AccountUserTypes, IAppointment} from '../../proto';
+import memoize from 'lodash-es/memoize';
+import {User} from '../../account/user';
+import {AccountUserTypes, CallTypes, IAppointment,} from '../../proto';
 import {AccountService} from '../../services/account.service';
+import {AccountUserLookupService} from '../../services/account-user-lookup.service';
 import {AccountDatabaseService} from '../../services/crypto/account-database.service';
 import {EnvService} from '../../services/env.service';
 import {StringsService} from '../../services/strings.service';
@@ -18,11 +21,20 @@ export class AccountCallWaitingComponent implements OnInit {
 	/** @see AccountUserTypes */
 	public readonly accountUserTypes: typeof AccountUserTypes	= AccountUserTypes;
 
+	/** @see AccountUserTypes */
+	public readonly callTypes: typeof CallTypes	= CallTypes;
+
 	/** @see AccountChatComponent */
 	@Input() public appointment?: IAppointment;
 
 	/** @see AccountChatComponent */
 	@Input() public appointmentID?: string;
+
+	/** Gets user. */
+	public readonly getUser: (username: string) => Promise<User|undefined>	=
+	memoize(async (username: string) =>
+		this.accountUserLookupService.getUser(username)
+	);
 
 	/** @inheritDoc */
 	public ngOnInit () : void {
@@ -35,6 +47,10 @@ export class AccountCallWaitingComponent implements OnInit {
 
 		/** @see AccountDatabaseService */
 		public readonly accountDatabaseService: AccountDatabaseService,
+
+
+		/** @see AccountUserLookupService */
+		public readonly accountUserLookupService: AccountUserLookupService,
 
 		/** @see EnvService */
 		public readonly envService: EnvService,
