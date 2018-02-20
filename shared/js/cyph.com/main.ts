183a9d96c642ae0188fdcf35248b014d9c3d84e4
Ryan Lester <hacker@linux.com>
2018-02-20 00:57:44 -0500

    custom build init fix

--- shared/js/cyph.com/main.ts
+++ shared/js/cyph.com/main.ts
@@ -5,7 +5,7 @@
 
 import {enableProdMode} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {environment} from '../environments';
+import {environment} from '../environments/environment';
 import {AppModule} from './app.module';
 
 
