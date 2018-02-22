7a5c001490447755de485606e4beedf68cfe22dc
Ryan Lester <hacker@linux.com>
2018-02-22 01:32:48 -0500

    SecurityModels documentation

--- shared/js/cyph/account/enums/security-models.ts
+++ shared/js/cyph/account/enums/security-models.ts
@@ -1,5 +1,17 @@
 /**
  * Different types of security depending on a particular datum's threat model.
+ * 
+ * ---
+ * 
+ * `private`: Encrypted for the current user and stored privately.
+ * 
+ * `privateSigned`: Encrypted and signed for/by the current user and stored privately.
+ * 
+ * `public`: Signed by the current user and shared publicly.
+ * 
+ * `publicFromOtherUsers`: Accepts data signed by other users.
+ * 
+ * `unprotected`: No end-to-end cryptographic protection. Use ONLY for server-generated data.
  */
 export enum SecurityModels {
 	private,
