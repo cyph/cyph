204519879c600ff64603e333f7a14d7e64318fbf
Ryan Lester <hacker@linux.com>
2018-02-22 01:20:55 -0500

    docs.sh updates

--- commands/docs.sh
+++ commands/docs.sh
@@ -9,6 +9,14 @@ dir="$PWD"
 cd ~/docs/cyph.ws
 
 cp ../README.md ../LICENSE ./
+echo -e "\n---\n\n$(cat ../PATENTS)" >> README.md
 rm -rf src/js/cyph.com src/js/native src/js/tslint-rules "${dir}/shared/js/docs"
-compodoc -n 'Cyph Docs' -p src/tsconfig.app.json --disablePrivateOrInternalSupport
+
+compodoc \
+	-n 'Cyph Docs' \
+	-p src/tsconfig.app.json \
+	--disablePrivate \
+	--disableProtected \
+	--disableInternal
+
 mv documentation "${dir}/shared/js/docs"
