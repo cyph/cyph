4a58a10c8b285f1640a1142c7dbda62edec0b67f
Ryan Lester <hacker@linux.com>
2018-02-27 18:37:06 -0500

    allow syncing data to/from dynamic forms

--- shared/js/cyph/components/dynamic-form/dynamic-form.component.ts
+++ shared/js/cyph/components/dynamic-form/dynamic-form.component.ts
@@ -1,5 +1,6 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import * as msgpack from 'msgpack-lite';
+import {IAsyncValue} from '../../iasync-value';
 import {Form, IForm} from '../../proto';
 import {EnvService} from '../../services/env.service';
 import {StringsService} from '../../services/strings.service';
@@ -17,12 +18,25 @@ import {timestampToDate} from '../../util/time';
 	styleUrls: ['./dynamic-form.component.scss'],
 	templateUrl: './dynamic-form.component.html'
 })
-export class DynamicFormComponent {
+export class DynamicFormComponent implements OnInit {
 	/** @ignore */
 	private readonly maskCache: Map<Uint8Array, any>		= new Map<Uint8Array, any>();
 
+	/** @ignore */
+	private readonly segmentReducer							= (v: any, segment: number|string) =>
+		(
+			(typeof segment === 'number' && v instanceof Array) ||
+			(typeof segment === 'string' && typeof v === 'object')
+		) ?
+			v[segment] :
+			undefined
+	;
+
 	/** @see Form */
 	@Input() public form?: IForm;
+
+	/** Data source to pull data from on init and save data to on submit. */
+	@Input() public dataSource?: IAsyncValue<any>;
 
 	/** Indicates whether input is disabled. */
 	@Input() public isDisabled: boolean						= false;
@@ -45,6 +59,54 @@ export class DynamicFormComponent {
 	/** @see Form.FormElement.Types */
 	public readonly types: typeof Form.Element.Types		= Form.Element.Types;
 
+	/** @ignore */
+	private iterateFormValues (
+		f: (id: string, segments: (number|string)[], element: Form.IElement) => void
+	) : void {
+		if (!this.form || !this.form.components) {
+			return;
+		}
+
+		for (const component of this.form.components) {
+			if (!component.rows) {
+				continue;
+			}
+
+			for (const row of component.rows) {
+				if (!row.elements) {
+					continue;
+				}
+
+				for (const element of row.elements) {
+					if (!element.id) {
+						continue;
+					}
+
+					const id	=
+						(component.id ? `${component.id}.` : '') +
+						(row.id ? `${row.id}.` : '') +
+						element.id
+					;
+
+					const segments	= id.split('.').
+						map(s => {
+							const arrayIndex = (s.match(/\[\d+\]$/) || [])[0];
+							return !arrayIndex ? s : [
+								s.slice(0, 0 - arrayIndex.length),
+								Number.parseInt(arrayIndex.slice(1, -1), 10)
+							];
+						}).reduce(
+							(arr: (number|string)[], s) => arr.concat(s),
+							[]
+						)
+					;
+
+					f(id, segments, element);
+				}
+			}
+		}
+	}
+
 	/** Decode mask bytes. */
 	public getMask ({mask}: Form.IElement) : any {
 		if (!mask || mask.length < 1) {
@@ -56,6 +118,75 @@ export class DynamicFormComponent {
 			mask,
 			() => msgpack.decode(mask)
 		);
+	}
+
+	/** @inheritDoc */
+	public async ngOnInit () : Promise<void> {
+		if (!this.dataSource || !this.form || !this.form.components) {
+			return;
+		}
+
+		const value	= await this.dataSource.getValue();
+
+		this.iterateFormValues((id, segments, element) => {
+			const elementValue	= segments.reduce(this.segmentReducer, value);
+
+			if (elementValue === undefined) {
+				return;
+			}
+
+			switch (typeof elementValue) {
+				case 'boolean':
+					element.valueBoolean	= elementValue;
+					break;
+
+				case 'number':
+					element.valueNumber		= elementValue;
+					break;
+
+				case 'string':
+					element.valueString		= elementValue;
+					break;
+
+				default:
+					throw new Error(`Invalid form value at ${id}`);
+			}
+		});
+	}
+
+	/** Submit handler. */
+	public async onSubmit () : Promise<void> {
+		if (this.dataSource) {
+			await this.dataSource.updateValue(value => {
+				this.iterateFormValues((id, segments, element) => {
+					const elementValue	=
+						element.valueBoolean === undefined ?
+							element.valueBoolean :
+							element.valueNumber === undefined ?
+								element.valueNumber :
+								element.valueString
+					;
+
+					const lastSegment	= segments.slice(-1)[0];
+
+					if (elementValue === undefined || lastSegment === undefined) {
+						return;
+					}
+
+					const parentValue	= segments.slice(0, -1).reduce(this.segmentReducer, value);
+
+					if (parentValue === undefined) {
+						return;
+					}
+
+					parentValue[lastSegment]	= elementValue;
+				});
+
+				return value;
+			});
+		}
+
+		this.submitForm.emit(this.form);
 	}
 
 	constructor (
