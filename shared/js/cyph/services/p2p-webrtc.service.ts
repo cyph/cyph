f2e3f079dbee343ef5c24c270adce4e6a7557723
Josh Boehm <josh@cyph.com>
2018-03-05 16:23:59 -0800

    added local debugging for webrtc

--- shared/js/cyph/services/p2p-webrtc.service.ts
+++ shared/js/cyph/services/p2p-webrtc.service.ts
@@ -355,6 +355,7 @@ export class P2PWebRTCService implements IP2PWebRTCService {
 					);
 				}
 			},
+			debug: env.environment.local ? true : false,
 			localVideoEl: $localVideo[0],
 			media: {audio: true, video: true},
 			peerConnectionConfig: {
