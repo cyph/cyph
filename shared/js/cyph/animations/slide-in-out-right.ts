1f0b6c0e04dc1a65d41e2d01852645ea42a5bafa
Ryan Lester <hacker@linux.com>
2018-04-11 18:18:01 -0400

    whitespace consistency

--- shared/js/cyph/animations/slide-in-out-right.ts
+++ shared/js/cyph/animations/slide-in-out-right.ts
@@ -1,5 +1,6 @@
 import {animate, state, style, transition, trigger} from '@angular/animations';
 
+
 /** Slide in and out from the right animation. */
 export const slideInOutRight	= trigger('slideInOutRight', [
 	state('in', style({transform: 'translateX(-100%)'})),
