cb8959ad8ce1350e73cadd860c3e89871770a701
Ryan Lester <hacker@linux.com>
2018-04-12 23:25:59 -0400

    add methods to EHRService

--- shared/js/cyph/services/ehr.service.ts
+++ shared/js/cyph/services/ehr.service.ts
@@ -1,4 +1,7 @@
 import {Injectable} from '@angular/core';
+import {request, requestMaybeJSON} from '../util/request';
+import {stringify} from '../util/serialization';
+import {EnvService} from './env.service';
 
 
 /**
@@ -6,5 +9,44 @@ import {Injectable} from '@angular/core';
  */
 @Injectable()
 export class EHRService {
-	constructor () {}
+	/** Deletes an API key issued with this master API key. */
+	public async deleteApiKey (apiKey: string, masterApiKey: string) : Promise<void> {
+		await request({
+			data: {
+				apiKey,
+				masterAPIKey: masterApiKey
+			},
+			method: 'DELETE',
+			url: this.envService.baseUrl + 'redox/apikey'
+		});
+	}
+
+	/** Generates a new API key for the specified user using a master API key. */
+	public async generateApiKey (username: string, masterApiKey: string) : Promise<string> {
+		return request({
+			data: {
+				masterAPIKey: masterApiKey,
+				username
+			},
+			method: 'POST',
+			url: this.envService.baseUrl + 'redox/apikey'
+		});
+	}
+
+	/** Runs a command against the EHR system integration. */
+	public async runCommand (apiKey: string, command: any) : Promise<any> {
+		return requestMaybeJSON({
+			data: {
+				apiKeyOrMasterAPIKey: apiKey,
+				redoxCommand: stringify(command)
+			},
+			method: 'POST',
+			url: this.envService.baseUrl + 'redox/execute'
+		});
+	}
+
+	constructor (
+		/** @ignore */
+		private readonly envService: EnvService
+	) {}
 }
