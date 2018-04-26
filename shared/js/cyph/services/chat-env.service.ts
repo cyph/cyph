a3681fb0c1e7de62399dff7cd4ccef85af5b1b42
Ryan Lester <hacker@linux.com>
2018-04-26 18:08:28 -0400

    minor perf improvement

--- shared/js/cyph/services/chat-env.service.ts
+++ shared/js/cyph/services/chat-env.service.ts
@@ -1,4 +1,5 @@
 import {Injectable} from '@angular/core';
+import memoize from 'lodash-es/memoize';
 import {env} from '../env';
 import {ISessionService} from '../service-interfaces/isession.service';
 import {ConfigService} from './config.service';
@@ -13,37 +14,43 @@ import {SessionInitService} from './session-init.service';
 @Injectable()
 export class ChatEnvService extends EnvService {
 	/** @ignore */
+	private readonly newCyphUrlHelperInternal	= memoize(
+		(base: boolean, sessionService: ISessionService) : string => {
+			const flags		=
+				this.configService.apiFlags.map(flag =>
+					flag.get(sessionService) ? flag.character : ''
+				).join('')
+			;
+
+			const baseURL	= (
+				this.callType === this.sessionInitService.callType ?
+					undefined :
+					this.sessionInitService.callType === 'audio' ?
+						(base ? env.cyphAudioBaseUrl : env.cyphAudioUrl) :
+						this.sessionInitService.callType === 'video' ?
+							(base ? env.cyphVideoBaseUrl : env.cyphVideoUrl) :
+							undefined
+			) || (
+				base ? env.newCyphBaseUrl : env.newCyphUrl
+			);
+
+			const divider	= baseURL.indexOf('#') < 0 ? '#' : '';
+
+			return flags.length > 0 ? `${baseURL}${divider}${flags}` : baseURL;
+		}
+	);
+
+	/** @ignore */
 	private sessionService?: ISessionService;
 
 	/** @ignore */
 	private newCyphUrlHelper (base: boolean) : string {
-		const sessionService	= this.sessionService;
-
-		if (!this.configService || !sessionService) {
+		if (!this.configService || !this.sessionService) {
 			return base ? env.newCyphBaseUrl : env.newCyphUrl;
 		}
-
-		const flags		=
-			this.configService.apiFlags.map(flag =>
-				flag.get(sessionService) ? flag.character : ''
-			).join('')
-		;
-
-		const baseURL	= (
-			this.callType === this.sessionInitService.callType ?
-				undefined :
-				this.sessionInitService.callType === 'audio' ?
-					(base ? env.cyphAudioBaseUrl : env.cyphAudioUrl) :
-					this.sessionInitService.callType === 'video' ?
-						(base ? env.cyphVideoBaseUrl : env.cyphVideoUrl) :
-						undefined
-		) || (
-			base ? env.newCyphBaseUrl : env.newCyphUrl
-		);
-
-		const divider	= baseURL.indexOf('#') < 0 ? '#' : '';
-
-		return flags.length > 0 ? `${baseURL}${divider}${flags}` : baseURL;
+		else {
+			return this.newCyphUrlHelperInternal(base, this.sessionService);
+		}
 	}
 
 	/** EnvService.newCyphBaseUrl adjusted for session API flags and initial call type. */
