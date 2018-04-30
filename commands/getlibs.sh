5e3ab9d7120da58898c12b913ee4e68216df18e7
Ryan Lester <hacker@linux.com>
2018-04-30 15:51:50 -0400

    Revert "temporary workaround for https://github.com/angular/angular-cli/issues/1548"
    This reverts commit be1936256e5479d312ebcbcef9b1240465a0d226.

--- commands/getlibs.sh
+++ commands/getlibs.sh
@@ -257,9 +257,6 @@ cp -f simplewebrtc/out/simplewebrtc-with-adapter.bundle.js simplewebrtc/src/simp
 cat wowjs/dist/wow.js | perl -pe 's/this\.([A-Z][a-z])/self.\1/g' > wowjs/dist/wow.js.new
 mv wowjs/dist/wow.js.new wowjs/dist/wow.js
 
-# Temporary workaround for https://github.com/angular/angular-cli/issues/1548
-sed -i "s|crypto: 'empty'|crypto: true|g" @angular/cli/models/webpack-configs/browser.js
-
 # Temporary workaround for https://github.com/werk85/node-html-to-text/issues/151
 for f in $(grep -rl lodash html-to-text) ; do
 	cat ${f} | perl -pe "s/(require\(['\"])lodash(\/.*?['\"]\))/\1lodash-es\2.default/g" > ${f}.new
