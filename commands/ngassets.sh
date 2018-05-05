2f76fcca220991f1409dabdb2c63aa65100194f3
Ryan Lester <hacker@linux.com>
2018-05-04 21:41:53 -0400

    minor fix

--- commands/ngassets.sh
+++ commands/ngassets.sh
@@ -7,6 +7,6 @@ cp -f shared/favicon.ico cyph.com/src/ 2> /dev/null
 cp -f shared/favicon.ico cyph.ws/src/ 2> /dev/null
 cp -f shared/assets/serviceworker.js websign/manifest.json cyph.ws/src/ 2> /dev/null
 
-if [ -f cyph.ws/src/serviceworker.js ] ; then
+if [ ! -f cyph.ws/src/serviceworker.js ] ; then
 	cp -f websign/serviceworker.js cyph.ws/src/ 2> /dev/null
 fi
