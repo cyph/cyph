110dfc02e5eb154ba79b9d9b91c2745a4854ab06
Josh Boehm <josh@cyph.com>
2018-05-22 18:35:08 -0700

    Added vpn link

--- shared/js/cyph/services/affiliate.service.ts
+++ shared/js/cyph/services/affiliate.service.ts
@@ -10,18 +10,21 @@ export class AffiliateService {
 	private readonly nordVPN	= {
 		copy: {
 			doublevpn: translate('Double VPN Encryption'),
-			recommended: translate("Get Cyph's recommended VPN service — NordVPN")
+			recommended: translate("Get Cyph's recommended VPN service — NordVPN"),
+			protect: translate('Protect your browsing online with NordVPN')
 		},
 		links: {
 			doublevpn: 'https://go.nordvpn.net/SH1FK',
-			threeyear: 'https://go.nordvpn.net/SH1F8'
+			threeyear: 'https://go.nordvpn.net/SH1F8',
+			default: 'https://go.nordvpn.net/SH1F4'
 		}
 	};
 
 	/** VPN affiliate link and ad copy. */
 	public readonly vpnLink	= this.splitTestingService.getValue('affiliatevpn', [
 		{href: this.nordVPN.links.doublevpn, text: this.nordVPN.copy.doublevpn},
-		{href: this.nordVPN.links.threeyear, text: this.nordVPN.copy.recommended}
+		{href: this.nordVPN.links.threeyear, text: this.nordVPN.copy.recommended},
+		{href: this.nordVPN.links.default, text: this.nordVPN.copy.protect}
 	]);
 
 	constructor (
