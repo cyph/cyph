9bbc57915083b5927dc94fdd3b68296904f72142
Ryan Lester <hacker@linux.com>
2018-05-31 22:47:15 -0500

    remove hashes from contact data model

--- shared/js/cyph/services/account-contacts.service.ts
+++ shared/js/cyph/services/account-contacts.service.ts
@@ -2,12 +2,11 @@ import {Injectable} from '@angular/core';
 import memoize from 'lodash-es/memoize';
 import {BehaviorSubject, Observable} from 'rxjs';
 import {map, mergeMap, skip, take} from 'rxjs/operators';
-import {IContactListItem, User} from '../account';
-import {StringProto} from '../proto';
-import {filterDuplicatesOperator, filterUndefined} from '../util/filter';
+import {IContactListItem, SecurityModels, User} from '../account';
+import {BinaryProto, StringProto} from '../proto';
+import {filterUndefined} from '../util/filter';
 import {toBehaviorSubject} from '../util/flatten-observable';
 import {normalize, normalizeArray} from '../util/formatting';
-import {getOrSetDefault, getOrSetDefaultAsync} from '../util/get-or-set-default';
 import {uuid} from '../util/uuid';
 import {AccountUserLookupService} from './account-user-lookup.service';
 import {AccountDatabaseService} from './crypto/account-database.service';
@@ -20,40 +19,14 @@ import {DatabaseService} from './database.service';
  */
 @Injectable()
 export class AccountContactsService {
-	/** @ignore */
-	private readonly contactIdCache: Map<string, Map<string, string>>	=
-		new Map<string, Map<string, string>>()
-	;
-
 	/** List of contacts for current user, sorted alphabetically by username. */
 	public readonly contactList: Observable<(IContactListItem|User)[]>	= toBehaviorSubject(
-		this.accountDatabaseService.watchListKeys('contactUsernames').pipe(
-			mergeMap(async keys =>
-				(await Promise.all(
-					(await Promise.all(
-						keys.map(async key => ({
-							key,
-							username: await this.accountDatabaseService.getItem(
-								`contactUsernames/${key}`,
-								StringProto
-							)
-						}))
-					)).map(async ({key, username}) => ({
-						id: await this.getContactID(username),
-						key,
-						username
-					}))
-				)).
-					filter(({id, key}) => id === key).
-					map(({username}) => username).
-					sort()
-			),
-			filterDuplicatesOperator(),
-			map(usernames => usernames.map(username => ({
+		this.accountDatabaseService.watchListKeys('contacts').pipe(map(usernames =>
+			normalizeArray(usernames).map(username => ({
 				user: this.accountUserLookupService.getUser(username),
 				username
-			})))
-		),
+			}))
+		)),
 		[]
 	);
 
@@ -72,89 +45,92 @@ export class AccountContactsService {
 			)
 	);
 
-	/** Indicates whether spinner should be displayed. */
-	public readonly showSpinner: BehaviorSubject<boolean>	= new BehaviorSubject(true);
-
-	/**
-	 * Adds contact.
-	 * @returns Contact ID.
-	 */
-	public async addContact (username: string|string[]) : Promise<string> {
-		if (username instanceof Array) {
-			await Promise.all(username.map(async groupMember => this.addContact(groupMember)));
-			return this.getContactID(username);
-		}
-
-		const id	= await this.getContactID(username);
-		const url	= `contactUsernames/${id}`;
-
-		if (!(await this.accountDatabaseService.hasItem(url))) {
-			await this.accountDatabaseService.setItem(url, StringProto, normalize(username));
-		}
-
-		return id;
-	}
-
 	/**
 	 * Gets Castle session ID based on username.
 	 * Note: string array parameter is temporary/deprecated.
 	 */
-	public async getCastleSessionID (username: string|string[]) : Promise<string> {
-		const currentUserUsername	=
-			(await this.accountDatabaseService.getCurrentUser()).user.username
-		;
+	public readonly getCastleSessionID	= memoize(
+		async (username: string|string[]) : Promise<string> => {
+			const currentUserUsername	=
+				(await this.accountDatabaseService.getCurrentUser()).user.username
+			;
 
-		if (username instanceof Array) {
-			return this.potassiumService.toHex(await this.potassiumService.hash.hash(
-				normalizeArray([currentUserUsername, ...username]).join(' ')
-			));
+			if (username instanceof Array) {
+				return this.potassiumService.toHex(await this.potassiumService.hash.hash(
+					normalizeArray([currentUserUsername, ...username]).join(' ')
+				));
+			}
+
+			const [userA, userB]		= normalizeArray([currentUserUsername, username]);
+
+			return this.databaseService.getOrSetDefault(
+				`castleSessionIDs/${userA}/${userB}`,
+				StringProto,
+				() => uuid(true)
+			);
 		}
-
-		const [userA, userB]		= normalizeArray([currentUserUsername, username]);
-
-		return this.databaseService.getOrSetDefault(
-			`castleSessionIDs/${userA}/${userB}`,
-			StringProto,
-			() => uuid(true)
-		);
-	}
+	);
 
 	/** Gets contact ID based on username. */
-	public async getContactID (username: string|string[]) : Promise<string> {
-		const currentUserUsername	=
-			(await this.accountDatabaseService.getCurrentUser()).user.username
-		;
+	public readonly getContactID		= memoize(async (username?: string) : Promise<string> =>
+		!username ? '' : this.accountDatabaseService.getOrSetDefault(
+			`contactIDs/${username}`,
+			StringProto,
+			async () => {
+				const id	= uuid();
 
-		return getOrSetDefaultAsync(
-			getOrSetDefault(
-				this.contactIdCache,
-				currentUserUsername,
-				() => new Map<string, string>()
-			),
-			username instanceof Array ? username.join(' ') : username,
-			async () => this.potassiumService.toHex(await this.potassiumService.hash.hash(
-				normalizeArray([currentUserUsername].concat(username)).join(' ')
-			))
-		);
-	}
+				await this.accountDatabaseService.setItem(
+					`contactUsernames/${id}`,
+					StringProto,
+					username,
+					SecurityModels.unprotected
+				);
+
+				return id;
+			},
+			SecurityModels.unprotected
+		)
+	);
 
 	/** Gets contact username based on ID. */
-	public async getContactUsername (id: string) : Promise<string> {
-		return this.accountDatabaseService.getItem(`contactUsernames/${id}`, StringProto);
+	public readonly getContactUsername	= memoize(async (id: string) : Promise<string> => {
+		return this.accountDatabaseService.getItem(
+			`contactUsernames/${id}`,
+			StringProto,
+			SecurityModels.unprotected
+		);
+	});
+
+	/** Indicates whether spinner should be displayed. */
+	public readonly showSpinner: BehaviorSubject<boolean>	= new BehaviorSubject(true);
+
+	/** @ignore */
+	private contactURL (username: string) : string {
+		return `contacts/${normalize(username)}`;
+	}
+
+	/** Adds contact. */
+	public async addContact (username: string) : Promise<void> {
+		const url	= this.contactURL(username);
+
+		if (!(await this.accountDatabaseService.hasItem(url))) {
+			await this.accountDatabaseService.setItem(
+				url,
+				BinaryProto,
+				new Uint8Array(0),
+				SecurityModels.unprotected
+			);
+		}
 	}
 
 	/** Indicates whether the user is already a contact. */
 	public async isContact (username: string) : Promise<boolean> {
-		return this.accountDatabaseService.hasItem(
-			`contactUsernames/${await this.getContactID(username)}`
-		);
+		return this.accountDatabaseService.hasItem(this.contactURL(username));
 	}
 
 	/** Removes contact. */
 	public async removeContact (username: string) : Promise<void> {
-		await this.accountDatabaseService.removeItem(
-			`contactUsernames/${await this.getContactID(username)}`
-		);
+		await this.accountDatabaseService.removeItem(this.contactURL(username));
 	}
 
 	/** Adds or removes contact. */
@@ -169,9 +145,7 @@ export class AccountContactsService {
 
 	/** Watches whether the user is a contact. */
 	public watchIfContact (username: string) : Observable<boolean> {
-		return this.accountDatabaseService.watchExists((async () =>
-			`contactUsernames/${await this.getContactID(username)}`
-		)());
+		return this.accountDatabaseService.watchExists(this.contactURL(username));
 	}
 
 	constructor (
@@ -187,8 +161,8 @@ export class AccountContactsService {
 		/** @ignore */
 		private readonly databaseService: DatabaseService
 	) {
-		this.accountDatabaseService.getListKeys('contactUsernames').then(keys => {
-			if (keys.length < 1) {
+		this.accountDatabaseService.getListKeys('contacts').then(usernames => {
+			if (usernames.length < 1) {
 				this.showSpinner.next(false);
 			}
 		});
