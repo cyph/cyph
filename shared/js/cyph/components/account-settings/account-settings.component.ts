5190af061910f5eb41af71ace46d4743672bbb1a
Ryan Lester <hacker@linux.com>
2018-06-27 17:12:07 -0400

    settings UI

--- shared/js/cyph/components/account-settings/account-settings.component.ts
+++ shared/js/cyph/components/account-settings/account-settings.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {SecurityModels} from '../../account/enums';
+import {BehaviorSubject} from 'rxjs';
+import {SecurityModels, User} from '../../account';
 import {IAsyncValue} from '../../iasync-value';
 import {StringProto} from '../../proto';
 import {AccountSettingsService} from '../../services/account-settings.service';
@@ -7,6 +8,7 @@ import {AccountService} from '../../services/account.service';
 import {AccountDatabaseService} from '../../services/crypto/account-database.service';
 import {EnvService} from '../../services/env.service';
 import {StringsService} from '../../services/strings.service';
+import {normalize} from '../../util/formatting';
 
 
 /**
@@ -18,6 +20,20 @@ import {StringsService} from '../../services/strings.service';
 	templateUrl: './account-settings.component.html'
 })
 export class AccountSettingsComponent implements OnInit {
+	/** Data. */
+	public readonly data	= {
+		current: {
+			email: '',
+			name: '',
+			realUsername: ''
+		},
+		modified: {
+			email: '',
+			name: '',
+			realUsername: ''
+		}
+	};
+
 	/** Email address. */
 	public readonly email: IAsyncValue<string>	= this.accountDatabaseService.getAsyncValue(
 		'email',
@@ -25,9 +41,69 @@ export class AccountSettingsComponent implements OnInit {
 		SecurityModels.unprotected
 	);
 
+	/** Indicates whether page is loading. */
+	public readonly loading: BehaviorSubject<boolean>	= new BehaviorSubject(true);
+
+	/** User. */
+	public user?: User;
+
 	/** @inheritDoc */
-	public ngOnInit () : void {
+	public async ngOnInit () : Promise<void> {
 		this.accountService.transitionEnd();
+
+		this.user	= (await this.accountDatabaseService.getCurrentUser()).user;
+
+		const [email, {name, realUsername}]	= await Promise.all([
+			this.email.getValue(),
+			this.user.accountUserProfile.getValue()
+		]);
+
+		this.data.current	= {email, name, realUsername};
+		this.data.modified	= {...this.data.current};
+
+		this.loading.next(false);
+	}
+
+	/** Indicates whether data is ready to save. */
+	public get ready () : boolean {
+		return (
+			!!this.user &&
+			(
+				this.data.current.email !== this.data.modified.email ||
+				this.data.current.name !== this.data.modified.name ||
+				this.data.current.realUsername !== this.data.modified.realUsername
+			) &&
+			!!this.data.modified.name &&
+			normalize(this.data.modified.realUsername) === this.user.username
+		);
+	}
+
+	/** Saves data updates. */
+	public async save () : Promise<void> {
+		const user	= this.user;
+		if (!user || !this.ready) {
+			return;
+		}
+
+		this.loading.next(true);
+
+		const email			= this.data.modified.email.trim();
+		const name			= this.data.modified.name.trim();
+		const realUsername	= this.data.modified.realUsername.trim();
+
+		await Promise.all([
+			this.data.current.email === email ?
+				undefined :
+				this.email.setValue(email)
+			,
+			this.data.current.name === name && this.data.current.realUsername === realUsername ?
+				undefined :
+				user.accountUserProfile.updateValue(async o => ({...o, name, realUsername}))
+		]);
+
+		this.data.current	= {email, name, realUsername};
+
+		this.loading.next(false);
 	}
 
 	constructor (
