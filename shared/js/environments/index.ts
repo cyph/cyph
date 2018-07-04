ab562ba7c846213df15cf1bb883e7680bd7c288d
Ryan Lester <hacker@linux.com>
2018-07-04 03:39:57 -0400

    minor fix

--- shared/js/environments/index.ts
+++ shared/js/environments/index.ts
@@ -26,69 +26,71 @@ accountRoot		= 'account';
 let altTheme	= false;
 
 /* tslint:disable-next-line:strict-type-predicates */
-if (environment.customBuild && $ !== undefined && typeof window === 'object') {
-	for (const k of Object.keys(Object.getPrototypeOf(environment.customBuild))) {
-		const o	= (<any> environment.customBuild)[k];
-		if (ArrayBuffer.isView(o) && o.byteLength < 1) {
-			(<any> environment.customBuild)[k]	= undefined;
+if ($ !== undefined && typeof window === 'object') {
+	if (environment.customBuild) {
+		for (const k of Object.keys(Object.getPrototypeOf(environment.customBuild))) {
+			const o	= (<any> environment.customBuild)[k];
+			if (ArrayBuffer.isView(o) && o.byteLength < 1) {
+				(<any> environment.customBuild)[k]	= undefined;
+			}
+		}
+
+		if (environment.customBuild.config.accountsOnly) {
+			accountRoot	= '';
+		}
+
+		if (environment.customBuild.config.backgroundColor) {
+			$('head').find(
+				'meta[name="theme-color"],' +
+				'meta[name="msapplication-TileColor"]'
+			).
+				attr('content', environment.customBuild.config.backgroundColor)
+			;
+
+			$('head link[rel="mask-icon"]').attr(
+				'color',
+				environment.customBuild.config.backgroundColor
+			);
+		}
+
+		if (environment.customBuild.config.telehealth) {
+			altTheme	= true;
+			$(document.body).addClass('telehealth');
+		}
+
+		if (environment.customBuild.config.title) {
+			$('title').text(environment.customBuild.config.title);
+		}
+
+		if (environment.customBuild.css) {
+			altTheme			= true;
+			const style			= document.createElement('style');
+			style.textContent	= environment.customBuild.css;
+			document.head.appendChild(style);
+		}
+
+		if (environment.customBuild.favicon) {
+			const faviconURI	=
+				`data:image/png;base64,${potassiumUtil.toBase64(environment.customBuild.favicon)}`
+			;
+
+			$('head').find(
+				'link[type="image/png"],' +
+				'meta[name="msapplication-TileImage"]'
+			).each((_, elem) => {
+				if (elem instanceof HTMLLinkElement) {
+					elem.href		= faviconURI;
+				}
+				else if (elem instanceof HTMLMetaElement) {
+					elem.content	= faviconURI;
+				}
+			});
 		}
 	}
 
-	if (environment.customBuild.config.accountsOnly) {
-		accountRoot	= '';
+	if (!altTheme) {
+		$(document.body).addClass('primary-account-theme');
 	}
-
-	if (environment.customBuild.config.backgroundColor) {
-		$('head').find(
-			'meta[name="theme-color"],' +
-			'meta[name="msapplication-TileColor"]'
-		).
-			attr('content', environment.customBuild.config.backgroundColor)
-		;
-
-		$('head link[rel="mask-icon"]').attr(
-			'color',
-			environment.customBuild.config.backgroundColor
-		);
-	}
-
-	if (environment.customBuild.config.telehealth) {
-		altTheme	= true;
-		$(document.body).addClass('telehealth');
-	}
-
-	if (environment.customBuild.config.title) {
-		$('title').text(environment.customBuild.config.title);
-	}
-
-	if (environment.customBuild.css) {
-		altTheme			= true;
-		const style			= document.createElement('style');
-		style.textContent	= environment.customBuild.css;
-		document.head.appendChild(style);
-	}
-
-	if (environment.customBuild.favicon) {
-		const faviconURI	=
-			`data:image/png;base64,${potassiumUtil.toBase64(environment.customBuild.favicon)}`
-		;
-
-		$('head').find(
-			'link[type="image/png"],' +
-			'meta[name="msapplication-TileImage"]'
-		).each((_, elem) => {
-			if (elem instanceof HTMLLinkElement) {
-				elem.href		= faviconURI;
-			}
-			else if (elem instanceof HTMLMetaElement) {
-				elem.content	= faviconURI;
-			}
-		});
-	}
-}
-
-if (!altTheme) {
-	$(document.body).addClass('primary-account-theme');
 }
 
 
