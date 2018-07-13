24c888f608d6feba736cf0389bbebd31a307232d
Ryan Lester <hacker@linux.com>
2018-07-13 18:21:37 -0400

    build script

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+cd $(cd "$(dirname "$0")" ; pwd)
+
+rm -rf build 2> /dev/null
+mkdir build
+
+for f in * ; do
+	if [ "${f}" != 'build' ] ; then
+		cp -a "${f}" build/
+	fi
+done
+
+cd build
+
+echo -e '\n\nADD PLATFORMS\n\n'
+
+cordova platform add android
+# cordova platform add ios
+
+echo -e '\n\nBUILD\n\n'
+
+cordova build --release --device
