b42b66b5e0c403c69db8e6c0f8892ad87d4d57a3
Ryan Lester <hacker@linux.com>
2018-07-18 18:20:56 -0400

    LastPass mitigation fix

--- shared/js/cyph/directives/enable-last-pass.directive.ts
+++ shared/js/cyph/directives/enable-last-pass.directive.ts
@@ -1,6 +1,7 @@
 import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
 import {memoize} from 'lodash-es';
 import {uuid} from '../util/uuid';
+import {sleep} from '../util/wait';
 
 
 /**
@@ -12,7 +13,7 @@ import {uuid} from '../util/uuid';
 })
 export class EnableLastPassDirective implements OnInit {
 	/** @inheritDoc */
-	public ngOnInit () : void {
+	public async ngOnInit () : Promise<void> {
 		if (!this.elementRef.nativeElement) {
 			return;
 		}
@@ -23,15 +24,16 @@ export class EnableLastPassDirective implements OnInit {
 		this.renderer.setAttribute(this.elementRef.nativeElement, 'data-lpignore', 'true');
 		this.renderer.setAttribute(this.elementRef.nativeElement, 'role', 'note');
 
+		if (!this.elementRef.nativeElement.name) {
+			this.renderer.setAttribute(this.elementRef.nativeElement, 'name', getID());
+		}
+
 		if (
 			!this.elementRef.nativeElement.id ||
 			this.elementRef.nativeElement.id.startsWith('mat-input-')
 		) {
+			await sleep(0);
 			this.renderer.setAttribute(this.elementRef.nativeElement, 'id', getID());
-		}
-
-		if (!this.elementRef.nativeElement.name) {
-			this.renderer.setAttribute(this.elementRef.nativeElement, 'name', getID());
 		}
 	}
 
