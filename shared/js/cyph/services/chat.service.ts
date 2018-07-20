5e77e044290cf16eadb4408089ae850834bfc4f3
Ryan Lester <hacker@linux.com>
2018-07-19 21:08:16 -0400

    more conservative message box throttling

--- shared/js/cyph/services/chat.service.ts
+++ shared/js/cyph/services/chat.service.ts
@@ -166,7 +166,7 @@ export class ChatService {
 	public readonly initiated				= new BehaviorSubject<boolean>(false);
 
 	/** Interval for throttling send buttons. */
-	public readonly messageThrottle: number	= 250;
+	public readonly messageThrottle: number	= 100;
 
 	/** @see P2PService */
 	public readonly p2pService				= resolvable<{
