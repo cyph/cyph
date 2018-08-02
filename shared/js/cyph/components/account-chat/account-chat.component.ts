df34ecccf9837f21cf2fcbd7e2a5ba0967bef436
Ryan Lester <hacker@linux.com>
2018-08-02 14:41:17 -0400

    video UI responsiveness improvement

--- shared/js/cyph/components/account-chat/account-chat.component.ts
+++ shared/js/cyph/components/account-chat/account-chat.component.ts
@@ -9,6 +9,7 @@ import {accountChatProviders} from '../../providers';
 import {AccountChatService} from '../../services/account-chat.service';
 import {AccountContactsService} from '../../services/account-contacts.service';
 import {AccountFilesService} from '../../services/account-files.service';
+import {AccountP2PService} from '../../services/account-p2p.service';
 import {AccountSessionService} from '../../services/account-session.service';
 import {AccountService} from '../../services/account.service';
 import {AccountAuthService} from '../../services/crypto/account-auth.service';
@@ -16,7 +17,6 @@ import {AccountDatabaseService} from '../../services/crypto/account-database.ser
 import {EnvService} from '../../services/env.service';
 import {FileTransferService} from '../../services/file-transfer.service';
 import {P2PWebRTCService} from '../../services/p2p-webrtc.service';
-import {P2PService} from '../../services/p2p.service';
 import {StringsService} from '../../services/strings.service';
 import {normalize} from '../../util/formatting';
 import {lockFunction} from '../../util/lock';
@@ -213,6 +213,10 @@ export class AccountChatComponent implements OnDestroy, OnInit {
 					return;
 				}
 
+				if (sessionSubID) {
+					await this.accountP2PService.initCall(callType, sessionSubID);
+				}
+
 				this.p2pWebRTCService.disconnect.pipe(take(1)).toPromise().then(async () => {
 					if (!this.destroyed) {
 						this.router.navigate(
@@ -270,6 +274,9 @@ export class AccountChatComponent implements OnDestroy, OnInit {
 		/** @see AccountDatabaseService */
 		public readonly accountDatabaseService: AccountDatabaseService,
 
+		/** @see AccountP2PService */
+		public readonly accountP2PService: AccountP2PService,
+
 		/** @see AccountSessionService */
 		public readonly accountSessionService: AccountSessionService,
 
@@ -278,9 +285,6 @@ export class AccountChatComponent implements OnDestroy, OnInit {
 
 		/** @see FileTransferService */
 		public readonly fileTransferService: FileTransferService,
-
-		/** @see P2PService */
-		public readonly p2pService: P2PService,
 
 		/** @see P2PWebRTCService */
 		public readonly p2pWebRTCService: P2PWebRTCService,
