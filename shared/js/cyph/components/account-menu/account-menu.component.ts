38b78c95673e80013e4da6b2eaf3f4ddaba229a0
Josh Boehm <josh@cyph.com>
2018-08-06 22:04:55 -0700

    Added status to menu/removed from profile

--- shared/js/cyph/components/account-menu/account-menu.component.ts
+++ shared/js/cyph/components/account-menu/account-menu.component.ts
@@ -1,7 +1,7 @@
 import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
 import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
-import {UserPresence} from '../../account/enums';
+import {UserPresence, userPresenceSelectOptions} from '../../account/enums';
 import {BaseProvider} from '../../base-provider';
 import {AccountUserTypes} from '../../proto';
 import {AccountFilesService} from '../../services/account-files.service';
@@ -10,6 +10,7 @@ import {AccountAuthService} from '../../services/crypto/account-auth.service';
 import {AccountDatabaseService} from '../../services/crypto/account-database.service';
 import {EnvService} from '../../services/env.service';
 import {StringsService} from '../../services/strings.service';
+import {trackByValue} from '../../track-by/track-by-value';
 import {urlToSafeStyle} from '../../util/safe-values';
 
 
@@ -25,11 +26,17 @@ import {urlToSafeStyle} from '../../util/safe-values';
 export class AccountMenuComponent extends BaseProvider {
 	/** @see AccountUserTypes */
 	public readonly accountUserTypes: typeof AccountUserTypes	= AccountUserTypes;
-
+	
 	/** @see AccountService.menuExpanded */
 	public readonly menuExpanded: Observable<boolean>			=
 		this.accountService.menuExpanded.pipe(map(menuExpanded => this.sidenav || menuExpanded))
 	;
+
+	/** @see UserPresence */
+	public readonly statuses							= userPresenceSelectOptions;
+
+	/** @see trackByValue */
+	public readonly trackByValue						= trackByValue;
 
 	/** If true, is inside a sidenav. */
 	@Input() public sidenav: boolean							= false;
