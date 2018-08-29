1448e11e5a46c89cc3c92582221c45f6711c80e9
Ryan Lester <hacker@linux.com>
2018-08-29 18:15:39 -0400

    handle maps in json serialization

--- shared/js/cyph/util/serialization/json.ts
+++ shared/js/cyph/util/serialization/json.ts
@@ -5,8 +5,14 @@ const stringifyInternal	= <T> (value: T, space?: string) : string => {
 	/* tslint:disable-next-line:ban */
 	return JSON.stringify(
 		value,
-		(_, v) => v instanceof Uint8Array ?
-			{data: potassiumUtil.toBase64(v), isUint8Array: true} :
+		(_, v) =>
+			v instanceof Uint8Array ?
+				{data: potassiumUtil.toBase64(v), isUint8Array: true} :
+			v instanceof Map ?
+				{
+					data: Array.from(v.keys()).reduce((o, k) => ({...o, [k]: v.get(k)}), {}),
+					isMap: true
+				} :
 			v
 		,
 		space
@@ -17,8 +23,10 @@ const stringifyInternal	= <T> (value: T, space?: string) : string => {
 export const parse	= <T> (text: string) : T => {
 	/* tslint:disable-next-line:ban */
 	return JSON.parse(text, (_, v) =>
-		v && v.isUint8Array && typeof v.data === 'string' ?
+		v && v.isUint8Array === true && typeof v.data === 'string' ?
 			potassiumUtil.fromBase64(v.data) :
+		v && v.isMap === true && typeof v.data === 'object' ?
+			new Map(<any> Object.keys(v.data).map(k => [k, v.data[k]])) :
 			v
 	);
 };
