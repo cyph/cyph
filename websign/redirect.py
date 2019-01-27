88a2a5b93208db274b7ed60c34cf3227ec090ba2
Ryan Lester <hacker@linux.com>
2019-01-27 18:29:50 -0500

    redirect fix for Tor

--- websign/redirect.py
+++ websign/redirect.py
@@ -3,17 +3,23 @@ import webapp2
 
 class Redirect (webapp2.RequestHandler):
 	def decorateHeaders (self, path):
-		self.response.headers.add_header('Location', '/#' + path)
+		location = '/#' + path
+
+		# Workaround for Tor server configuration
+		if 'Host' in self.request.headers and self.request.headers['Host'].endswith(':8081'):
+			location = 'https://' + self.request.headers['Host'].split(':')[0] + location
+
+		self.response.headers.add_header('Location', location)
 		self.response.status	= 301
 
 	def get (self, path):
 		self.decorateHeaders(path)
 		self.response.write('')
 
-	def head(self, path):
+	def head (self, path):
 		self.decorateHeaders(path)
 
-	def options(self, path):
+	def options (self, path):
 		self.decorateHeaders(path)
 
 
