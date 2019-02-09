3afe21af4cee7a17435aa9795385c2b02b770e5b
Ryan Lester <hacker@linux.com>
2019-02-09 23:46:44 +0000

    generate AccountInviteService

---
+++ shared/js/cyph/services/account-invite.service.ts
@@ -0,0 +1,13 @@
+import {Injectable} from '@angular/core';
+import {BaseProvider} from '../base-provider';
+
+
+/**
+ * Angular service for account invite.
+ */
+@Injectable()
+export class AccountInviteService extends BaseProvider {
+	constructor () {
+		super();
+	}
+}
