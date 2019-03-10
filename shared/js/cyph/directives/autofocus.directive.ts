41b5935c8782aab3eefe0c299b0cb56ade0bbe1a
Ryan Lester <hacker@linux.com>
2019-03-09 22:33:34 -0800

    Revert "temporary workaround for https://github.com/ionic-team/cordova-plugin-ionic-webview/issues/156"
    This reverts commit 3f14772a4e78c2ec822dcdfdef11aa32f22c1230.

--- shared/js/cyph/directives/autofocus.directive.ts
+++ shared/js/cyph/directives/autofocus.directive.ts
@@ -38,7 +38,7 @@ export class AutofocusDirective extends BaseProvider implements OnChanges, OnIni
 			return;
 		}
 
-		if (this.envService.isCordova && this.envService.isIOS) {
+		if (this.envService.isCordova) {
 			await AutofocusDirective.loadComplete;
 		}
 		else if (this.envService.isMobileOS) {
