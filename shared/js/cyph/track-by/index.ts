72d2c59e1e8c6bde630e7fec43b9a9d8f86c1ffb
Ryan Lester <hacker@linux.com>
2019-03-23 18:20:14 -0400

    minor cleanup

--- shared/js/cyph/track-by/index.ts
+++ shared/js/cyph/track-by/index.ts
@@ -1,4 +1,5 @@
 export * from './track-by-id';
+export * from './track-by-index';
 export * from './track-by-message-list-item';
 export * from './track-by-self';
 export * from './track-by-transfer';
