50bc3f1cc5a240c0cb4780cf3cfc8f02df9ee7d7
Josh Boehm <josh@plusminussays.com>
2019-08-04 18:25:00 -0700

    Revert "remove --privileged for now (may break Windows hosts)"
    This reverts commit e183d694d513e454efebf082211f0f355c867923.

--- docker.js
+++ docker.js
@@ -272,6 +272,7 @@ const containerName		= command => `${image}_${command}`.replace(/\//g, '_');
 const dockerRun			= (command, name, background, noCleanup, additionalArgs, getOutput) => {
 	const processArgs	= [
 		'run',
+		'--privileged',
 		getOutput ? '-i' : '-it'
 	].concat(
 		name ? [`--name=${name}`] : []
