54b5ce5f4d26f63ffebf7809fa8f27a37215b718
Ryan Lester <hacker@linux.com>
2019-08-06 23:24:12 -0700

    prettier ignore yaml for now

--- commands/commit.sh
+++ commands/commit.sh
@@ -53,7 +53,7 @@ find shared/css shared/js \
 	' \
 \;
 
-cyphPrettier --write '**/*.{css,html,js,json,md,scss,ts,tsx,yaml,yml}'
+cyphPrettier --write '**/*.{css,html,js,json,md,scss,ts,tsx}'
 
 find shared/assets/img -type f \( -name '*.jpg' -or -name '*.png' \) -exec bash -c '
 	curl -sf "$(node -e "console.log(JSON.parse('"'"'$(
