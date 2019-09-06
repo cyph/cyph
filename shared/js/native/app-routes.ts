7d061b5d178580c03349298a39977d8c637bcfb4
Ryan Lester <hacker@linux.com>
2019-09-06 19:29:59 -0400

    lint fix

--- shared/js/native/app-routes.ts
+++ shared/js/native/app-routes.ts
@@ -1,7 +1,6 @@
 /* tslint:disable:object-literal-sort-keys */
 
-import {Routes} from '@angular/router';
 import {account, retry} from './js/cyph/routes';
 
 /** @see Routes */
-export const appRoutes = <Routes> [account, retry];
+export const appRoutes = [account, retry];
