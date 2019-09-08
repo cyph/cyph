c1405b4bd77476ff3e3dc12889fea35b7f46aa77
Ryan Lester <hacker@linux.com>
2019-09-08 15:56:37 -0400

    add script to update WebSign libs

---
+++ commands/websign/updatelibs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd $(cd "$(dirname "$0")" ; pwd)/../..
+
+terser /node_modules/whatwg-fetch/dist/fetch.umd.js -o websign/lib/fetch.js
+terser /node_modules/localforage/dist/localforage.min.js -o websign/lib/localforage.js
+terser /node_modules/supersphincs/dist/supersphincs.js -o websign/lib/supersphincs.js
