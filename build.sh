8e92f9992913ccb2322bf81be65b838233c7786a
Ryan Lester <hacker@linux.com>
2019-09-14 16:54:45 -0400

    remove unnecessary build output formats

--- build.sh
+++ build.sh
@@ -102,9 +102,7 @@ if [ "${electron}" ] ; then
 	npx cordova build electron --release
 
 	cp -a platforms/electron/build/mas/*.pkg build/cyph.pkg || exit 1
-	cp platforms/electron/build/*.dmg build/cyph.dmg || exit 1
 	cp platforms/electron/build/*.appx build/cyph.appx || exit 1
-	cp platforms/electron/build/*.exe build/cyph.exe || exit 1
 	cp platforms/electron/build/*.tar.gz build/cyph.tar.gz || exit 1
 fi
 
