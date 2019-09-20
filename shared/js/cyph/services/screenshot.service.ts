cb44e48c82573e851820ef721aa6c9c9799710bb
Ryan Lester <hacker@linux.com>
2019-09-20 20:03:16 +0000

    generate ScreenshotService

---
+++ shared/js/cyph/services/screenshot.service.ts
@@ -0,0 +1,12 @@
+import {Injectable} from '@angular/core';
+import {BaseProvider} from '../base-provider';
+
+/**
+ * Angular service for screenshot.
+ */
+@Injectable()
+export class ScreenshotService extends BaseProvider {
+	constructor () {
+		super();
+	}
+}
