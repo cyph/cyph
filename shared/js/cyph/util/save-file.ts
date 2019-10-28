2f7307a51d06a99e458595cd762f11967290ac3b
Ryan Lester <hacker@linux.com>
2019-10-28 14:08:18 -0400

    saveFile minor fix

--- shared/js/cyph/util/save-file.ts
+++ shared/js/cyph/util/save-file.ts
@@ -19,94 +19,97 @@ export const saveFile = async (
 	const oldBeforeUnloadMessage = beforeUnloadMessage;
 	beforeUnloadMessage = undefined;
 
-	const fileMediaType =
-		mediaType && mediaType.indexOf('/') > 0 ?
-			mediaType :
-			'application/octet-stream';
+	try {
+		const fileMediaType =
+			mediaType && mediaType.indexOf('/') > 0 ?
+				mediaType :
+				'application/octet-stream';
 
-	const fileBlob = new Blob([content], {type: fileMediaType});
+		const fileBlob = new Blob([content], {type: fileMediaType});
 
-	const save = () => {
-		fileSaver.saveAs(fileBlob, fileName, {autoBom: false});
-	};
-
-	if (env.isCordovaMobile) {
-		if (!env.isAndroid) {
-			return shareFile(content, fileName, mediaType);
-		}
-
-		const fs = await new Promise<any>((resolve, reject) => {
-			(<any> self).resolveLocalFileSystemURL(
-				'file:///storage/emulated/0/Download',
-				resolve,
-				reject
-			);
-		});
-
-		/* If balls.png already exists, try balls.0.png, balls.1.png, etc. */
-		let fileNameIncrement = -2;
-		let savedFileName = fileName;
-		const fileEntry = await retryUntilSuccessful(
-			async () =>
-				new Promise<any>((resolve, reject) => {
-					fs.getFile(
-						++fileNameIncrement < 0 ?
-							fileName :
-						fileName.indexOf('.') < 0 ?
-							`${fileName}.${fileNameIncrement.toString()}` :
-							(() => {
-								const fileNameSplit = fileName.split('.');
-								fileNameSplit.splice(
-									-1,
-									0,
-									fileNameIncrement.toString()
-								);
-								savedFileName = fileNameSplit.join('.');
-								return savedFileName;
-							})(),
-						{create: true, exclusive: true},
-						resolve,
-						reject
-					);
-				})
-		);
-
-		const fileWriter = await new Promise<any>((resolve, reject) => {
-			fileEntry.createWriter(resolve, reject);
-		});
-
-		const fileWriteResult = new Promise<any>((resolve, reject) => {
-			fileWriter.onwriteend = resolve;
-			fileWriter.onerror = reject;
-		});
-
-		fileWriter.write(fileBlob);
-		await fileWriteResult;
-
-		await (await staticDialogService).toast(
-			`${translate('Saved')} ${savedFileName} ${translate(
-				'to downloads folder.'
-			)}`,
-			3000
-		);
-	}
-	else if (env.safariVersion === undefined || env.safariVersion >= 10.1) {
-		save();
-	}
-	else {
-		const handler = () => {
-			document.removeEventListener('click', handler);
-			save();
+		const save = () => {
+			fileSaver.saveAs(fileBlob, fileName, {autoBom: false});
 		};
-		document.addEventListener('click', handler);
-		await (await staticDialogService).alert({
-			content: `${translate('Saving file')} "${fileName}" ${translate(
-				'with the name "unknown", due to a Safari bug'
-			)}.`,
-			title: translate('Save File')
-		});
-	}
 
-	await sleep();
-	beforeUnloadMessage = oldBeforeUnloadMessage;
+		if (env.isCordovaMobile) {
+			if (!env.isAndroid) {
+				return shareFile(content, fileName, mediaType);
+			}
+
+			const fs = await new Promise<any>((resolve, reject) => {
+				(<any> self).resolveLocalFileSystemURL(
+					'file:///storage/emulated/0/Download',
+					resolve,
+					reject
+				);
+			});
+
+			/* If balls.png already exists, try balls.0.png, balls.1.png, etc. */
+			let fileNameIncrement = -2;
+			let savedFileName = fileName;
+			const fileEntry = await retryUntilSuccessful(
+				async () =>
+					new Promise<any>((resolve, reject) => {
+						fs.getFile(
+							++fileNameIncrement < 0 ?
+								fileName :
+							fileName.indexOf('.') < 0 ?
+								`${fileName}.${fileNameIncrement.toString()}` :
+								(() => {
+									const fileNameSplit = fileName.split('.');
+									fileNameSplit.splice(
+										-1,
+										0,
+										fileNameIncrement.toString()
+									);
+									savedFileName = fileNameSplit.join('.');
+									return savedFileName;
+								})(),
+							{create: true, exclusive: true},
+							resolve,
+							reject
+						);
+					})
+			);
+
+			const fileWriter = await new Promise<any>((resolve, reject) => {
+				fileEntry.createWriter(resolve, reject);
+			});
+
+			const fileWriteResult = new Promise<any>((resolve, reject) => {
+				fileWriter.onwriteend = resolve;
+				fileWriter.onerror = reject;
+			});
+
+			fileWriter.write(fileBlob);
+			await fileWriteResult;
+
+			await (await staticDialogService).toast(
+				`${translate('Saved')} ${savedFileName} ${translate(
+					'to downloads folder.'
+				)}`,
+				3000
+			);
+		}
+		else if (env.safariVersion === undefined || env.safariVersion >= 10.1) {
+			save();
+		}
+		else {
+			const handler = () => {
+				document.removeEventListener('click', handler);
+				save();
+			};
+			document.addEventListener('click', handler);
+			await (await staticDialogService).alert({
+				content: `${translate('Saving file')} "${fileName}" ${translate(
+					'with the name "unknown", due to a Safari bug'
+				)}.`,
+				title: translate('Save File')
+			});
+		}
+	}
+	finally {
+		await sleep();
+		beforeUnloadMessage = oldBeforeUnloadMessage;
+	}
 };
