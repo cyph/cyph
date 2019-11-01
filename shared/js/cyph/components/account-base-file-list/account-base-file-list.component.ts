5cca0c4bb1aeec6525006286569a9c3cb703ab8c
Ryan Lester <hacker@linux.com>
2019-11-01 15:25:21 -0400

    build fix

--- shared/js/cyph/components/account-base-file-list/account-base-file-list.component.ts
+++ shared/js/cyph/components/account-base-file-list/account-base-file-list.component.ts
@@ -4,7 +4,9 @@ import {
 	Input,
 	ViewChild
 } from '@angular/core';
-import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
 import memoize from 'lodash-es/memoize';
 import {BaseProvider} from '../../base-provider';
 import {
