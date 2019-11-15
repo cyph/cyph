b4c0485c33de1c3e11ee551903a82d8c105d128e
Ryan Lester <hacker@linux.com>
2019-11-14 21:01:40 -0500

    Firebase update

--- firebase/functions/config.js
+++ firebase/functions/config.js
@@ -2,7 +2,10 @@ global.crypto = require('crypto');
 
 require('./js/standalone/global');
 require('./js/cyph/config');
+require('./js/cyph/plan-config');
 
 module.exports = new Config();
+module.exports.planConfig = planConfig;
 module.exports.config = module.exports;
 global.Config = undefined;
+global.planConfig = undefined;
