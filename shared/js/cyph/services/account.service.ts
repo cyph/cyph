4cff0cbf46b57c41a25b42fd0846b9149a4edde0
Ryan Lester <hacker@linux.com>
2019-11-20 04:21:05 -0500

    Revert "Revert "call accept edge case fix""
    This reverts commit 79107115de09f6b8cbef21ffc9eb5f15d5a91fb2.

--- shared/js/cyph/services/account.service.ts
+++ shared/js/cyph/services/account.service.ts
@@ -67,6 +67,9 @@ export class AccountService extends BaseProvider {
 	);
 
 	/** @ignore */
+	private readonly pushNotificationsActive = resolvable();
+
+	/** @ignore */
 	private readonly respondedCallRequests = new Set<string>();
 
 	/** @ignore */
@@ -472,6 +475,12 @@ export class AccountService extends BaseProvider {
 							user
 						} = await this.getIncomingCallRoute(k);
 
+						/* Prevent brief ring after acceptance via notification action */
+						if (this.envService.isCordovaMobile) {
+							await this.pushNotificationsActive.promise;
+							await sleep(1000);
+						}
+
 						const answered = await this.notificationService.ring(
 							this.dialogService.confirm({
 								bottomSheet: true,
@@ -665,35 +674,44 @@ export class AccountService extends BaseProvider {
 			});
 		}
 
-		this.accountDatabaseService.pushNotificationsSubscribe(async data => {
-			if (
-				!data ||
-				!data.additionalData ||
-				data.additionalData.dismissed ||
-				!(data.additionalData.notificationType in NotificationTypes) ||
-				typeof data.additionalData.notificationID !== 'string'
-			) {
-				return;
-			}
+		this.accountDatabaseService
+			.pushNotificationsSubscribe(async data => {
+				if (
+					!data ||
+					!data.additionalData ||
+					data.additionalData.dismissed ||
+					!(
+						data.additionalData.notificationType in
+						NotificationTypes
+					) ||
+					typeof data.additionalData.notificationID !== 'string'
+				) {
+					return;
+				}
 
-			switch (data.additionalData.notificationType) {
-				case NotificationTypes.File:
-					const {recordType} = await this.accountFilesService.getFile(
-						data.additionalData.notificationID
-					);
+				switch (data.additionalData.notificationType) {
+					case NotificationTypes.File:
+						const {
+							recordType
+						} = await this.accountFilesService.getFile(
+							data.additionalData.notificationID
+						);
 
-					this.router.navigate([
-						this.accountFilesService.config[recordType].route
-					]);
-					break;
+						this.router.navigate([
+							this.accountFilesService.config[recordType].route
+						]);
+						break;
 
-				case NotificationTypes.Message:
-					this.router.navigate([
-						'messages',
-						data.additionalData.notificationID
-					]);
-			}
-		});
+					case NotificationTypes.Message:
+						this.router.navigate([
+							'messages',
+							data.additionalData.notificationID
+						]);
+				}
+			})
+			.then(() => {
+				this.pushNotificationsActive.resolve();
+			});
 
 		for (const [callEvent, callAnswer] of <[string, boolean][]> [
 			['callAccept', true],
