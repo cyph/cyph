c62bc11b79d5a16aef65f82c9b2b18e8394286f2
Ryan Lester <hacker@linux.com>
2019-11-24 16:57:26 -0500

    Revert "hide transfer list in accounts chat UI for now"
    This reverts commit c931a8e44876a0e05fc983c422ef3161f2a1b197.

--- shared/js/cyph/components/chat-main/chat-main.component.ts
+++ shared/js/cyph/components/chat-main/chat-main.component.ts
@@ -16,7 +16,6 @@ import {FileTransferService} from '../../services/file-transfer.service';
 import {P2PWebRTCService} from '../../services/p2p-webrtc.service';
 import {P2PService} from '../../services/p2p.service';
 import {ScrollService} from '../../services/scroll.service';
-import {SessionInitService} from '../../services/session-init.service';
 import {SessionService} from '../../services/session.service';
 import {StringsService} from '../../services/strings.service';
 import {trackByTransfer} from '../../track-by/track-by-transfer';
@@ -113,9 +112,6 @@ export class ChatMainComponent extends BaseProvider implements AfterViewInit {
 
 		/** @see SessionService */
 		public readonly sessionService: SessionService,
-
-		/** @see SessionInitService */
-		public readonly sessionInitService: SessionInitService,
 
 		/** @see StringsService */
 		public readonly stringsService: StringsService
