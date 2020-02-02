65055156b9daca7b26ca20d4e98f7026f5e8b2e9
Ryan Lester <hacker@linux.com>
2020-02-02 21:51:45 +0000

    generate AccountPostsService

---
+++ shared/js/cyph/services/account-posts.service.ts
@@ -0,0 +1,12 @@
+import {Injectable} from '@angular/core';
+import {BaseProvider} from '../base-provider';
+
+/**
+ * Angular service for account posts.
+ */
+@Injectable()
+export class AccountPostsService extends BaseProvider {
+	constructor () {
+		super();
+	}
+}
