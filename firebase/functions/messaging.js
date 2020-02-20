bad2308fdd42d9156e464606e0934c6b776d3342
Ryan Lester <hacker@linux.com>
2020-02-19 23:38:18 -0500

    temporarily reenable desktop app notifications for @cyph, @josh, and @ryan

--- firebase/functions/messaging.js
+++ firebase/functions/messaging.js
@@ -8,9 +8,9 @@ const sendMessage = async (
 	body,
 	{actions, additionalData, badge, inboxStyle = true, ring, tag} = {}
 ) => {
-	const ref = database.ref(
-		`${namespace}/users/${normalize(username)}/messagingTokens`
-	);
+	username = normalize(username);
+
+	const ref = database.ref(`${namespace}/users/${username}/messagingTokens`);
 
 	const tokenPlatforms = (await ref.once('value')).val() || {};
 	const tokens = Object.keys(tokenPlatforms);
@@ -20,63 +20,64 @@ const sendMessage = async (
 	}
 
 	return (await Promise.all(
-		tokens
-			.filter(token => tokenPlatforms[token] !== 'electron')
-			.map(async token => {
-				const platform = tokenPlatforms[token];
+		/* For now, make special temporary exception to receive notifications in desktop app */
+		(username === 'cyph' || username === 'josh' || username === 'ryan' ?
+			tokens :
+			tokens.filter(token => tokenPlatforms[token] !== 'electron')
+		).map(async token => {
+			const platform = tokenPlatforms[token];
 
-				const notification = {
-					badge,
-					body,
-					sound:
-						ring && platform === 'android' ? 'ringtone' : 'default',
-					tag,
-					title: 'Cyph',
-					...(platform === 'android' ?
-						{
-							android_channel_id: ring ?
-								'cyph-rings' :
-								'cyph-notifications'
-						} :
-						{}),
-					...(platform === 'unknown' || platform === 'web' ?
-						{
-							icon:
-								'https://www.cyph.com/assets/img/favicon/favicon-256x256.png'
-						} :
-						{})
-				};
+			const notification = {
+				badge,
+				body,
+				sound: ring && platform === 'android' ? 'ringtone' : 'default',
+				tag,
+				title: 'Cyph',
+				...(platform === 'android' ?
+					{
+						android_channel_id: ring ?
+							'cyph-rings' :
+							'cyph-notifications'
+					} :
+					{}),
+				...(platform === 'unknown' || platform === 'web' ?
+					{
+						icon:
+							'https://www.cyph.com/assets/img/favicon/favicon-256x256.png'
+					} :
+					{})
+			};
 
-				const data = {
-					'content-available': true,
-					'priority': 2,
-					'visibility': 1,
-					...(actions ? {actions} : {}),
-					...(inboxStyle ? {style: 'inbox'} : {}),
-					...(tag && platform === 'ios' ? {'thread-id': tag} : {}),
-					...additionalData
-				};
+			const data = {
+				'content-available': true,
+				'priority': 2,
+				'visibility': 1,
+				...(actions ? {actions} : {}),
+				...(inboxStyle ? {style: 'inbox'} : {}),
+				...(tag && platform === 'ios' ? {'thread-id': tag} : {}),
+				...additionalData
+			};
 
-				const payload = {
-					...(platform === 'android' ?
-						{data: {...notification, ...data}} :
-						{data, notification}),
-					priority: 'high',
-					to: token
-				};
+			const payload = {
+				...(platform === 'android' ?
+					{data: {...notification, ...data}} :
+					{data, notification}),
+				priority: 'high',
+				to: token
+			};
 
-				return new Promise(resolve => {
-					messaging.send(payload, async (err, _RESPONSE) => {
-						if (err) {
-							await ref.child(token).remove();
-							resolve(false);
-						}
-						else {
-							resolve(true);
-						}
-					});
+			return new Promise(resolve => {
+				messaging.send(payload, async (err, _RESPONSE) => {
+					if (err) {
+						await ref.child(token).remove();
+						resolve(false);
+					}
+					else {
+						resolve(true);
+					}
 				});
-			})
+			});
+		})
 	)).reduce((a, b) => a || b, false);
 };
 
