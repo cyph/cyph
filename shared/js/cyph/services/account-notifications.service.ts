f6066d887056a06040a5b7b019dddafb5cdc67e2
Ryan Lester <hacker@linux.com>
2020-03-06 18:24:57 +0000

    generate AccountNotificationsService

---
+++ shared/js/cyph/services/account-notifications.service.ts
@@ -0,0 +1,12 @@
+import {Injectable} from '@angular/core';
+import {BaseProvider} from '../base-provider';
+
+/**
+ * Angular service for account notifications.
+ */
+@Injectable()
+export class AccountNotificationsService extends BaseProvider {
+	constructor () {
+		super();
+	}
+}
