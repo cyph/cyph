ce61e645f12f8bb9ded98a9fa3d257f0cb0a9057
Ryan Lester <hacker@linux.com>
2020-04-01 09:03:08 -0400

    disable automatic one-month premium trials

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -697,9 +697,9 @@ exports.generateInvite = onRequest(true, async (req, res, namespace) => {
 			preexistingInviteCodeData.braintreeSubscriptionID;
 	}
 
-	/* Gift free users one-month premium trials */
+	/* Previously gifted free users one-month premium trials */
 	let planTrialEnd = undefined;
-	if (plan === CyphPlans.Free) {
+	if (false && plan === CyphPlans.Free) {
 		plan = CyphPlans.MonthlyPremium;
 		planTrialEnd = new Date().setMonth(new Date().getMonth() + 1);
 	}
@@ -1182,11 +1182,15 @@ exports.sendInvite = onCall(async (data, namespace, getUsername) => {
 			await (async () => {
 				const code = readableID(15);
 
-				/* Gift free users one-month premium trials */
-				const plan = CyphPlans.MonthlyPremium;
-				const planTrialEnd = new Date().setMonth(
-					new Date().getMonth() + 1
-				);
+				/* Previously gifted free users one-month premium trials */
+				let plan = CyphPlans.Free;
+				let planTrialEnd = undefined;
+				if (false) {
+					plan = CyphPlans.MonthlyPremium;
+					planTrialEnd = new Date().setMonth(
+						new Date().getMonth() + 1
+					);
+				}
 
 				await Promise.all([
 					database.ref(`${namespace}/inviteCodes/${code}`).set({
