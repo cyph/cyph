1a335143019f550bbcc252afbed3076fa2ddb082
Ryan Lester <hacker@linux.com>
2020-04-05 12:56:17 -0400

    merge in data model updates from master

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -847,6 +847,35 @@ exports.getCastleSessionID = onCall(async (data, namespace, getUsername) => {
 	);
 });
 
+exports.getReactions = onCall(async (data, namespace, getUsername) => {
+	const currentUser = await getUsername();
+	const username = normalize(data.username);
+	const id = validateInput(data.id);
+	const isComment = data.isComment === true;
+
+	if (!username || !id) {
+		return {};
+	}
+
+	const parent = isComment ? 'postCommentReactions' : 'postReactions';
+
+	const reactionsRef = database.ref(
+		`${namespace}/users/${username}/${parent}/${id}`
+	);
+
+	const reactions = (await reactionsRef.once('value')).val() || {};
+
+	return Object.entries(reactions)
+		.map(([k, v]) => [
+			k,
+			{
+				count: Object.keys(v).length,
+				selected: currentUser in v
+			}
+		])
+		.reduce((o, [k, v]) => ({...o, [k]: v}), {});
+});
+
 exports.getUserToken = onCall(async (data, namespace, getUsername) => {
 	const [tokenKey, username] = await Promise.all([
 		getTokenKey(namespace),
