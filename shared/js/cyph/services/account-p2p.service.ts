39d30265a8f0e61ac40706044b7eea49421d6637
Ryan Lester <hacker@linux.com>
2020-04-08 11:22:29 -0400

    Revert "Accounts Burner chat calling fix"
    This reverts commit 203d2ce276b19bf0bb202f9ac466c0be7f1f5eb5.

--- shared/js/cyph/services/account-p2p.service.ts
+++ shared/js/cyph/services/account-p2p.service.ts
@@ -50,10 +50,7 @@ export class AccountP2PService extends P2PService {
 	protected async p2pWarningPersist (
 		f: () => Promise<boolean>
 	) : Promise<boolean> {
-		if (
-			!this.accountDatabaseService.currentUser.value ||
-			this.sessionInitService.ephemeral
-		) {
+		if (!this.accountDatabaseService.currentUser.value) {
 			return f();
 		}
 
