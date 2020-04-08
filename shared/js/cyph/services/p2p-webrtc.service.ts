1d541a4f5bca0260081793a4f202d8835171e9fb
Ryan Lester <hacker@linux.com>
2020-04-07 23:51:59 -0400

    re-enable trickle ICE

--- shared/js/cyph/services/p2p-webrtc.service.ts
+++ shared/js/cyph/services/p2p-webrtc.service.ts
@@ -675,7 +675,7 @@ export class P2PWebRTCService extends BaseProvider
 								.join('\n') :
 							sdp,
 					stream: localStream,
-					trickle: false
+					trickle: true
 				});
 
 				peer.on('close', async () => {
