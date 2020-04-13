4a2d8ff1485dc2a12963e6fb99bb1f5902bb8527
Ryan Lester <hacker@linux.com>
2020-04-13 13:33:35 -0400

    bundle optimization

--- shared/js/externals/request.ts
+++ shared/js/externals/request.ts
@@ -1,24 +1,25 @@
 /* eslint-disable */
 
 import {envDeploy} from '../cyph/env-deploy';
-import {request} from '../cyph/util/request';
 
 /** @file request external. */
 
 /* Temporary workaround for universal-analytics */
 export const post: any = (path: string, options: any, callback: any) => {
-	request({
-		contentType: '',
-		data: options.body,
-		headers: options.headers,
-		method: 'post',
-		url:
-			path.startsWith('http://') || path.startsWith('https://') ?
-				path :
-			envDeploy.baseUrl.startsWith('https://') ?
-				`https://${path}` :
-				`http://${path}`
-	})
+	/* Reduce size / drop Angular dependency on cyph.com home page */
+	/* eslint-disable-next-line @typescript-eslint/tslint/config */
+	fetch(
+		path.startsWith('http://') || path.startsWith('https://') ?
+			path :
+		envDeploy.baseUrl.startsWith('https://') ?
+			`https://${path}` :
+			`http://${path}`,
+		{
+			body: options.body,
+			headers: options.headers,
+			method: 'post'
+		}
+	)
 		.then(() => {
 			callback();
 		})
