928465498d950ed749a4f266654b0093f209ec0c
Ryan Lester <hacker@linux.com>
2020-05-23 19:58:08 +0000

    generate SocialShareService

---
+++ shared/js/cyph/services/social-share.service.ts
@@ -0,0 +1,12 @@
+import {Injectable} from '@angular/core';
+import {BaseProvider} from '../base-provider';
+
+/**
+ * Angular service for social share.
+ */
+@Injectable()
+export class SocialShareService extends BaseProvider {
+	constructor () {
+		super();
+	}
+}
