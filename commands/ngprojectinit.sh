84b9c9fa765cd52318ddf3fcc51d60cb43eaaf94
Ryan Lester <hacker@linux.com>
2020-05-25 09:56:33 -0400

    better workaround for Angular bug

--- commands/ngprojectinit.sh
+++ commands/ngprojectinit.sh
@@ -30,3 +30,43 @@ do
 		git checkout "${arr[1]}" &> /dev/null
 	fi
 done
+
+cd ..
+
+# Workaround for Angular bug ("Module not found: Error: Recursion in resolving")
+
+node -e "
+	const glob = require('glob');
+
+	const tsconfig = JSON.parse(
+		fs.readFileSync('shared/js/tsconfig.json').toString().
+			split('\n').
+			filter(s => s.trim()[0] !== '/').
+			join('\n')
+	);
+
+	for (const [k, v] of Array.from(
+		new Set([
+			...(
+				fs
+					.readFileSync('shared/lib/js/yarn.lock')
+					.toString()
+					.match(/\\n\"?@?[A-Za-z0-9_\\-\\.\\/]+/g) || []
+			)
+				.map(s => s.trim().replace('\"', ''))
+				.filter(s => s),
+			...glob.sync('node_modules/@angular/*/*/').map(s => s.slice(13, -1))
+		])
+	)
+		.sort()
+		.map(m => [m, [\`../node_modules/\${m}\`]])
+	) {
+		if (k in tsconfig.compilerOptions.paths) {
+			continue;
+		}
+
+		tsconfig.compilerOptions.paths[k] = v;
+	}
+
+	fs.writeFileSync('shared/js/tsconfig.ng.json', JSON.stringify(tsconfig));
+"
