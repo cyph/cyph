d386bc8f156f0939cbca56094a8f87d2a2737c7f
Ryan Lester <hacker@linux.com>
2020-06-09 13:10:26 -0400

    minor fix

--- build.sh
+++ build.sh
@@ -150,14 +150,15 @@ if [ "${iOS}" ] ; then
 	pod install --project-directory=platforms/ios
 
 	# https://github.com/cordova-rtc/cordova-plugin-iosrtc/blob/master/docs/Building.md#apple-store-submission
-
-	cd plugins/cordova-plugin-iosrtc/extra
-	node ios_arch.js --extract
-	node ios_arch.js --device
-	node ios_arch.js --clean
-	npx cordova platform remove ios
-	npx cordova platform add ios
-	cd -
+	if [ ! "${iOSEmulator}" ] ; then
+		cd plugins/cordova-plugin-iosrtc/extra
+		node ios_arch.js --extract
+		node ios_arch.js --device
+		node ios_arch.js --clean
+		npx cordova platform remove ios
+		npx cordova platform add ios
+		cd -
+	fi
 fi
 
 
