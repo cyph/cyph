45845981ce08a9fcdbe986ef451e2f69c92a9dce
Ryan Lester <hacker@linux.com>
2020-06-21 18:18:34 -0400

    Revert "minor cleanup"
    This reverts commit 09d21ebafe05b8b648ad78ec53c381b7deca1e75.

--- shared/js/cyph/services/chat.service.ts
+++ shared/js/cyph/services/chat.service.ts
@@ -1646,6 +1646,14 @@ export class ChatService extends BaseProvider {
 
 			this.sessionService.closed.promise.then(async () => this.close());
 
+			if (this.sessionInitService.ephemeral) {
+				this.sessionService.connected.promise.then(() => {
+					this.chat.state = States.keyExchange;
+					this.updateChat();
+					this.initProgressStart();
+				});
+			}
+
 			this.sessionService.connected.promise.then(async () => {
 				this.sessionCapabilitiesService.resolveWalkieTalkieMode(
 					this.walkieTalkieMode.value
@@ -1661,12 +1669,6 @@ export class ChatService extends BaseProvider {
 						unconfirmedMessages: this.chat.unconfirmedMessages.value
 					}
 				}));
-
-				if (this.sessionInitService.ephemeral) {
-					this.chat.state = States.keyExchange;
-					this.updateChat();
-					this.initProgressStart();
-				}
 
 				if (
 					callType === undefined ||
