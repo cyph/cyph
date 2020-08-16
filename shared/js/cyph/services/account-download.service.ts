2d64ecfcbcc82f28967e7f114241bfa206a9ac1d
Ryan Lester <hacker@linux.com>
2020-08-16 21:51:24 +0000

    generate AccountDownloadService

---
+++ shared/js/cyph/services/account-download.service.ts
@@ -0,0 +1,12 @@
+import {Injectable} from '@angular/core';
+import {BaseProvider} from '../base-provider';
+
+/**
+ * Angular service for account download.
+ */
+@Injectable()
+export class AccountDownloadService extends BaseProvider {
+	constructor () {
+		super();
+	}
+}
