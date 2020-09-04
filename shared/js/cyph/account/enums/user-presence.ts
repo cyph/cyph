92dadbc84898e7b7cab631e4727879e46de7ac57
Ryan Lester <hacker@linux.com>
2020-09-03 20:24:27 -0400

    temporary workaround pending https://github.com/typescript-eslint/typescript-eslint/pull/2470

--- shared/js/cyph/account/enums/user-presence.ts
+++ shared/js/cyph/account/enums/user-presence.ts
@@ -1,8 +1,9 @@
 import {AccountUserPresence} from '../../proto';
 
 /** @see AccountUserPresence.Statuses */
-/* eslint-disable-next-line @typescript-eslint/naming-convention, @typescript-eslint/no-redeclare */
+/* eslint-disable-next-line @typescript-eslint/naming-convention */
 export const UserPresence = AccountUserPresence.Statuses;
 
 /** @see AccountUserPresence.Statuses */
+/* eslint-disable-next-line @typescript-eslint/no-redeclare */
 export type UserPresence = AccountUserPresence.Statuses;
