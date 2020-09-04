7e8e2b1fc13b7974b71bcc39e17d027b1aeaf674
Ryan Lester <hacker@linux.com>
2020-09-04 03:11:45 +0000

    generate SessionWrapperService

---
+++ shared/js/cyph/services/session-wrapper.service.ts
@@ -0,0 +1,12 @@
+import {Injectable} from '@angular/core';
+import {BaseProvider} from '../base-provider';
+
+/**
+ * Angular service for session wrapper.
+ */
+@Injectable()
+export class SessionWrapperService extends BaseProvider {
+	constructor () {
+		super();
+	}
+}
