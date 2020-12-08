d5a254cf761e84c4fb4d8d80891afb94702ba1ef
Ryan Lester <hacker@linux.com>
2020-12-07 22:59:31 -0500

    build fix

--- shared/js/typings/libs.d.ts
+++ shared/js/typings/libs.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="/node_modules/@types/jquery/index.d.ts" />
 /// <reference path="/node_modules/@types/node/index.d.ts" />
 /// <reference path="/node_modules/tns-platform-declarations/ios.d.ts" />
 /// <reference path="/node_modules/tns-platform-declarations/android.d.ts" />
