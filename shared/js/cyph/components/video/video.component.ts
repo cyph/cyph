ce3234935f18037b8ebacaece8f7bd440ac88bd8
Ryan Lester <hacker@linux.com>
2021-01-17 17:46:16 -0500

    video preview audio level meter

--- shared/js/cyph/components/video/video.component.ts
+++ shared/js/cyph/components/video/video.component.ts
@@ -9,6 +9,7 @@ import {
 	SimpleChanges,
 	ViewChild
 } from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
 import {BaseProvider} from '../../base-provider';
 import {StringsService} from '../../services/strings.service';
 
@@ -24,7 +25,17 @@ import {StringsService} from '../../services/strings.service';
 export class VideoComponent extends BaseProvider
 	implements AfterViewInit, OnChanges, OnDestroy {
 	/** @ignore */
+	private audioContext?: {
+		context: AudioContext;
+		processor: ScriptProcessorNode;
+		src: MediaStreamAudioSourceNode;
+	};
+
+	/** @ignore */
 	private srcObjectURL?: string;
+
+	/** Audio level. */
+	public readonly audioLevel = new BehaviorSubject<number>(0);
 
 	/** @see HTMLVideoElement.autoplay */
 	@Input() public autoplay: boolean = false;
@@ -34,6 +45,9 @@ export class VideoComponent extends BaseProvider
 
 	/** @see HTMLVideoElement.playsinline */
 	@Input() public playsinline: boolean = false;
+
+	/** Indicates whether audio level should be displayed. */
+	@Input() public showAudioLevel: boolean = false;
 
 	/** Path, URL, or `srcObject`. */
 	@Input() public src?: string | MediaStream | MediaSource | Blob;
@@ -48,12 +62,18 @@ export class VideoComponent extends BaseProvider
 	private setVideoSrc (
 		src?: string | MediaStream | MediaSource | Blob
 	) : void {
+		if (this.audioContext) {
+			this.audioContext.processor.disconnect();
+			this.audioContext.src.disconnect();
+		}
+
 		if (this.srcObjectURL) {
 			URL.revokeObjectURL(this.srcObjectURL);
 			this.srcObjectURL = undefined;
 		}
 
 		if (!(this.video?.nativeElement instanceof HTMLVideoElement)) {
+			this.subscribeAudioLevels();
 			return;
 		}
 
@@ -79,6 +99,66 @@ export class VideoComponent extends BaseProvider
 		}
 
 		this.video.nativeElement.muted = this.muted;
+
+		this.subscribeAudioLevels();
+	}
+
+	/** @ignore */
+	private subscribeAudioLevels () : void {
+		this.audioContext = undefined;
+		this.audioLevel.next(0);
+
+		if (
+			!this.showAudioLevel ||
+			!(this.video?.nativeElement?.srcObject instanceof MediaStream)
+		) {
+			return;
+		}
+
+		try {
+			const context = new AudioContext();
+
+			this.audioContext = {
+				context,
+				processor: context.createScriptProcessor(2048, 1, 1),
+				src: context.createMediaStreamSource(
+					this.video.nativeElement.srcObject
+				)
+			};
+		}
+		catch {
+			this.showAudioLevel = false;
+		}
+
+		if (!this.audioContext) {
+			return;
+		}
+
+		const {context, processor, src} = this.audioContext;
+
+		src.connect(processor);
+		processor.connect(context.destination);
+
+		let lastTimestamp = 0;
+
+		processor.addEventListener('audioprocess', e => {
+			if (e.timeStamp - lastTimestamp < 100) {
+				return;
+			}
+
+			lastTimestamp = e.timeStamp;
+
+			const input = e.inputBuffer.getChannelData(0);
+
+			let sum = 0;
+			for (let i = 0; i < input.length; ++i) {
+				sum += input[i] * input[i];
+			}
+
+			const average = Math.sqrt(sum / input.length);
+
+			this.audioLevel.next(Math.min(Math.floor(average * 1000), 100));
+		});
 	}
 
 	/** @inheritDoc */
