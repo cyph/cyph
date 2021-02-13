e150afdd88e3f183df1272fd0e51cb4391ab9127
Ryan Lester <hacker@linux.com>
2021-02-13 17:02:52 +0000

    generate ProductTourService

---
+++ shared/js/cyph/services/product-tour.service.ts
@@ -0,0 +1,12 @@
+import {Injectable} from '@angular/core';
+import {BaseProvider} from '../base-provider';
+
+/**
+ * Angular service for product tour.
+ */
+@Injectable()
+export class ProductTourService extends BaseProvider {
+	constructor () {
+		super();
+	}
+}
