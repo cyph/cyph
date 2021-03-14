5012bb5a1dd286cc157170e67047991df2208faa
Ryan Lester <hacker@linux.com>
2021-03-13 23:20:19 -0500

    lint fix

--- shared/js/sdk/app.module.ts
+++ shared/js/sdk/app.module.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+
 /* eslint-disable-next-line @typescript-eslint/triple-slash-reference */
 /// <reference path="../typings/index.d.ts" />
 
