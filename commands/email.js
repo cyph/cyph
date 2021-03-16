2af4e44c05247220addffe0261d867b4b244dfe8
Ryan Lester <hacker@linux.com>
2021-03-16 16:51:34 -0400

    email.js fix

--- commands/email.js
+++ commands/email.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import {getMeta} from '../modules/base.js';
-const {__dirname, isCLI, require} = getMeta(import.meta);
+const {__dirname, isCLI} = getMeta(import.meta);
 
 import fs from 'fs';
 import os from 'os';
@@ -16,27 +16,37 @@ const tmp = `${tmpParent}/root`;
 fs.mkdirSync(tmp);
 
 fs.symlinkSync(`${__dirname}/../shared`, `${tmpParent}/shared`);
-fs.symlinkSync(
-	`${os.homedir()}/.cyph/email-credentials.js`,
-	`${tmp}/email-credentials.js`
+fs.writeFileSync(
+	`${tmp}/email-credentials.js`,
+	fs.readFileSync(`${os.homedir()}/.cyph/email-credentials.js`)
 );
 fs.symlinkSync(
 	`${__dirname}/../backend/shared/email.html`,
 	`${tmp}/email.html`
 );
 fs.symlinkSync(`${__dirname}/../backend/shared/templates`, `${tmp}/templates`);
+fs.writeFileSync(
+	`${tmp}/base.js`,
+	fs.readFileSync(`${__dirname}/../modules/base.js`)
+);
 fs.symlinkSync(
 	`${__dirname}/../modules/dompurify-html-sanitizer.js`,
 	`${tmp}/dompurify-html-sanitizer.js`
 );
 fs.writeFileSync(
+	`${tmp}/package.json`,
+	fs.readFileSync(`${__dirname}/package.json`)
+);
+fs.writeFileSync(
 	`${tmp}/index.js`,
-	fs.readFileSync(`${__dirname}/../firebase/functions/email.js`)
+	fs.readFileSync(`${__dirname}/../firebase/functions/js/email.js`)
 );
 fs.writeFileSync(
 	`${tmp}/markdown-templating.js`,
-	fs.readFileSync(`${__dirname}/../firebase/functions/markdown-templating.js`)
+	fs.readFileSync(
+		`${__dirname}/../firebase/functions/js/markdown-templating.js`
+	)
 );
 fs.writeFileSync(`${tmp}/namespaces.js`, 'export const namespaces = {};');
 
-export const sendMail = (await import(tmp)).sendMailInternal;
+export const sendMail = (await import(`${tmp}/index.js`)).sendMailInternal;
