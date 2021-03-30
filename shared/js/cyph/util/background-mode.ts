ca570aa7e78f05fe6d8a35776214f886ef0525ae
Ryan Lester <hacker@linux.com>
2021-03-30 14:37:24 -0400

    lint fix

--- shared/js/cyph/util/background-mode.ts
+++ shared/js/cyph/util/background-mode.ts
@@ -2,20 +2,24 @@ const backgroundMode = (<any> self).cordova?.plugins?.backgroundMode;
 
 /** Disables background mode (if applicable). */
 export const disableBackgroundMode = () => {
-	if (backgroundMode) {
-		try {
-			backgroundMode.disable();
-		}
-		catch {}
+	if (!backgroundMode) {
+		return;
 	}
+
+	try {
+		backgroundMode.disable();
+	}
+	catch {}
 };
 
 /** Enables background mode (if applicable). */
 export const enableBackgroundMode = () => {
-	if (backgroundMode) {
-		try {
-			backgroundMode.enable();
-		}
-		catch {}
+	if (!backgroundMode) {
+		return;
 	}
+
+	try {
+		backgroundMode.enable();
+	}
+	catch {}
 };
