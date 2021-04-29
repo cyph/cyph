78312df99b344b21760da6714f2e5d8b987da8c9
Ryan Lester <hacker@linux.com>
2021-04-29 00:07:21 -0400

    treat invite codes with inactive subscriptions as invalid

--- firebase/functions/js/functions/check-invite-code.js
+++ firebase/functions/js/functions/check-invite-code.js
@@ -2,6 +2,7 @@ import {proto} from '@cyph/sdk';
 import {getInviteTemplateData} from '../get-invite-template-data.js';
 import {database, onCall} from '../init.js';
 import {renderTemplate} from '../markdown-templating.js';
+import {stripe} from '../stripe.js';
 import {validateInput} from '../validation.js';
 
 const {CyphPlans} = proto;
@@ -22,10 +23,39 @@ export const checkInviteCode = onCall(async (data, namespace, getUsername) => {
 		inviterUsername,
 		keybaseUsername,
 		pgpPublicKey,
-		reservedUsername
+		reservedUsername,
+		stripe: stripeData
 	} = inviteData;
 	const plan =
 		inviteData.plan in CyphPlans ? inviteData.plan : CyphPlans.Free;
+
+	/* TODO: Check other subscription types as well (Apple, Braintree) */
+	if (stripeData) {
+		const subscriptionItem =
+			stripeData.customerID &&
+			stripeData.subscriptionID &&
+			stripeData.subscriptionItemID ?
+				await stripe.subscriptionItems
+					.retrieve(stripeData.subscriptionItemID)
+					.catch(() => undefined) :
+				undefined;
+
+		const subscription =
+			subscriptionItem &&
+			subscriptionItem.subscription === stripeData.subscriptionID ?
+				await stripe.subscriptions
+					.retrieve(stripeData.subscriptionID)
+					.catch(() => undefined) :
+				undefined;
+
+		if (
+			!subscription ||
+			subscription.customer !== stripeData.customerID ||
+			subscription.status !== 'active'
+		) {
+			return {isValid: false};
+		}
+	}
 
 	const templateData = getInviteTemplateData({
 		fromApp: true,
