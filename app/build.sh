c3533e2826d54b683eb88f566083d4412755ee26
Ryan Lester <hacker@linux.com>
2021-05-08 15:09:23 -0400

    Revert "Revert "minor fix""
    This reverts commit 37e1dc28d970a8509014aeeb1604405320594742.

--- app/build.sh
+++ app/build.sh
@@ -219,6 +219,8 @@ initPlatform () {
 		{stdio: 'inherit'}
 	).status)" ; done
 
+	node node_modules/patch-package/index.js
+
 	node -e "
 		const oldPackageJSON = JSON.parse(fs.readFileSync('package.json.old').toString());
 		const packageJSON = JSON.parse(fs.readFileSync('package.json').toString());
