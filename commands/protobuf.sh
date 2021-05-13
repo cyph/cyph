311b63dda4380ffd6ddbe284175bc43254425512
Ryan Lester <hacker@linux.com>
2021-05-13 01:03:11 -0400

    Revert "Revert "Revert "workaround for https://github.com/protobufjs/protobuf.js/issues/1306"""
    This reverts commit 5e1b772913bb0d5924a3c78ec257d7a1db24166e.

--- commands/protobuf.sh
+++ commands/protobuf.sh
@@ -14,7 +14,3 @@ pbts shared/js/proto/index.js -o shared/js/proto/types.d.ts
 checkfail
 touch shared/js/proto/types.js
 echo "export * from './types';" > shared/js/proto/index.d.ts
-
-# https://github.com/protobufjs/protobuf.js/issues/1306#issuecomment-549204730
-sed -i "s|\[ 'object' \]\.|Record|g" shared/js/proto/types.d.ts
-sed -i "s|\[ 'Array' \]\.|Array|g" shared/js/proto/types.d.ts
