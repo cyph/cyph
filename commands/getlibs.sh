1358873ed5dadb4bdc8898564682666d53a17657
Ryan Lester <hacker@linux.com>
2021-05-13 22:52:20 -0400

    minor fix

--- commands/getlibs.sh
+++ commands/getlibs.sh
@@ -38,7 +38,7 @@ installPackages () {
 }
 
 
-rm -rf "${GOPATH}"
+sudo rm -rf "${GOPATH}"
 find . -maxdepth 2 -type f -name .go.mod -exec bash -c '
 	cd $(echo "{}" | sed "s|/.go.mod||")
 	go mod download
