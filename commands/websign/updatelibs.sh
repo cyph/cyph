e76b9133ebd691effb79f06b1648f58171154c91
Ryan Lester <hacker@linux.com>
2021-08-27 23:54:55 -0400

    minor fix

--- commands/websign/updatelibs.sh
+++ commands/websign/updatelibs.sh
@@ -3,6 +3,7 @@
 cd $(cd "$(dirname "$0")" ; pwd)/../..
 
 terser ~/brotli/js/decode.js -cmo websign/lib/brotli.js
+terser /node_modules/comlink/dist/umd/comlink.min.js -o websign/lib/comlink.js
 terser /node_modules/whatwg-fetch/dist/fetch.umd.js -o websign/lib/fetch.js
 terser /node_modules/localforage/dist/localforage.min.js -o websign/lib/localforage.js
 terser ~/oldsupersphincs/node_modules/supersphincs/dist/supersphincs.js -o websign/lib/supersphincs.js
