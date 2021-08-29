a9d9858e7c10eeb5a2e9941cc6fca6a21cd3d6b1
Ryan Lester <hacker@linux.com>
2021-08-28 20:30:48 -0400

    disable mobile rocksdb for now

--- app/www/nodejs-project/index.js
+++ app/www/nodejs-project/index.js
@@ -1,24 +1,6 @@
 const comlink = require('comlink');
 const cordova = require('cordova-bridge');
 
-const rocksDBPromise = new Promise((resolve, reject) => {
-	try {
-		const rocksDB = require('rocksdb')('./data.db');
-
-		rocksDB.close(() => {
-			try {
-				resolve(require('levelup')(rocksDB));
-			}
-			catch (err) {
-				reject(err);
-			}
-		});
-	}
-	catch (err) {
-		reject(err);
-	}
-});
-
 const listeners = new Set();
 cordova.channel.on('message', message => {
 	for (const listener of listeners) {
@@ -26,36 +8,17 @@ cordova.channel.on('message', message => {
 	}
 });
 
-rocksDBPromise.then(rocksDB => {
-	comlink.expose(
-		{
-			rocksDB,
-			rocksDBKeys: async () =>
-				new Promise((resolve, reject) => {
-					const stream = rocksDB.createKeyStream();
-					const keys = [];
-
-					stream.on('data', key => {
-						keys.push(key.toString());
-					});
-					stream.on('end', () => {
-						resolve(keys);
-					});
-					stream.on('error', err => {
-						reject(err);
-					});
-				})
+comlink.expose(
+	{},
+	{
+		addEventListener: (_TYPE, listener, _OPTIONS) => {
+			listeners.add(listener);
 		},
-		{
-			addEventListener: (_TYPE, listener, _OPTIONS) => {
-				listeners.add(listener);
-			},
-			postMessage: (message, _TRANSFER) => {
-				cordova.channel.send(message);
-			},
-			removeEventListener: (_TYPE, listener, _OPTIONS) => {
-				listeners.delete(listener);
-			}
+		postMessage: (message, _TRANSFER) => {
+			cordova.channel.send(message);
+		},
+		removeEventListener: (_TYPE, listener, _OPTIONS) => {
+			listeners.delete(listener);
 		}
-	);
-});
+	}
+);
