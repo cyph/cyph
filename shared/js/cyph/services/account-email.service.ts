113664ced5ee835b8419a5516ccb647781e60702
Ryan Lester <hacker@linux.com>
2021-09-06 19:43:09 +0000

    generate AccountEmailService

---
+++ shared/js/cyph/services/account-email.service.ts
@@ -0,0 +1,12 @@
+import {Injectable} from '@angular/core';
+import {BaseProvider} from '../base-provider';
+
+/**
+ * Angular service for account email.
+ */
+@Injectable()
+export class AccountEmailService extends BaseProvider {
+	constructor () {
+		super();
+	}
+}
