3c0e98b4f8320c266d029f6029a044c4fd779802
Ryan Lester <hacker@linux.com>
2021-09-26 22:46:40 -0400

    quick workaround for syncfusion-ej2-wordprocessor bug

--- syncfusion/src/main/java/functions/Convert.java
+++ syncfusion/src/main/java/functions/Convert.java
@@ -17,27 +17,30 @@ public class Convert implements HttpFunction {
 		String result = "";
 		int status = 500;
 
-		try {
-			final Document document = Jsoup.parse(
-				new String(
-					request.getInputStream().readNBytes(134217728),
-					"UTF-8"
-				)
-			);
-
-			document
-				.outputSettings()
-				.syntax(Document.OutputSettings.Syntax.xml);
-
-			result =
-				WordProcessorHelper.loadString(
-					document.html(),
-					FormatType.Html
+		/* Retry logic to work around error: `java.lang.ClassCastException: class [Ljava.lang.Object; cannot be cast to class [Lcom.syncfusion.docio.HTMLConverterImpl$TextFormat; ([Ljava.lang.Object; is in module java.base of loader 'bootstrap'; [Lcom.syncfusion.docio.HTMLConverterImpl$TextFormat; is in unnamed module of loader com.google.cloud.functions.invoker.runner.Invoker$FunctionClassLoader @1c655221)` */
+		for (int i = 0; status != 200 && i < 5; ++i) {
+			try {
+				final Document document = Jsoup.parse(
+					new String(
+						request.getInputStream().readNBytes(134217728),
+						"UTF-8"
+					)
 				);
 
-			status = 200;
-		} catch (Exception err) {
-			result = err.toString();
+				document
+					.outputSettings()
+					.syntax(Document.OutputSettings.Syntax.xml);
+
+				result =
+					WordProcessorHelper.loadString(
+						document.html(),
+						FormatType.Html
+					);
+
+				status = 200;
+			} catch (Exception err) {
+				result = err.toString();
+			}
 		}
 
 		response.setStatusCode(status);
