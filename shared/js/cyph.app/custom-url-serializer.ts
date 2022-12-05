e35d43574958d5bb520d3ede5698587ecea3a379
Ryan Lester <hacker@linux.com>
2022-12-04 20:19:19 -0500

    update for latest Angular

--- shared/js/cyph.app/custom-url-serializer.ts
+++ shared/js/cyph.app/custom-url-serializer.ts
@@ -1,9 +1,17 @@
-import {DefaultUrlSerializer, UrlTree} from '@angular/router';
+import {DefaultUrlSerializer, UrlSerializer, UrlTree} from '@angular/router';
 
 /** https://github.com/angular/angular/issues/16621 */
-export class CustomUrlSerializer extends DefaultUrlSerializer {
+export class CustomUrlSerializer implements UrlSerializer {
+	/** @ignore */
+	private readonly defaultUrlSerializer = new DefaultUrlSerializer();
+
+	/** @inheritDoc */
+	public parse (url: string) : UrlTree {
+		return this.defaultUrlSerializer.parse(url);
+	}
+
 	/** @inheritDoc */
 	public serialize (tree: UrlTree) : string {
-		return super.serialize(tree).replace(/^\//, '');
+		return this.defaultUrlSerializer.serialize(tree).replace(/^\//, '');
 	}
 }
