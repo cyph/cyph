bdcc856c35e55ea737a7c4c5b833796a9710d9ed
Ryan Lester <hacker@linux.com>
2022-12-14 01:42:53 -0500

    Revert "Revert "temporary workaround for Firebase bug""
    This reverts commit e644486f43b0661ceb9ae0d54e64d6ae9bf23dd7.

--- commands/deploy.sh
+++ commands/deploy.sh
@@ -686,6 +686,7 @@ then
 	cd functions
 
 	npm ci
+	rm package-lock.json # TODO: Revert after Firebase supports Node.js 18
 
 	cp -rf ../../modules ~/.cyph/email-credentials.json js/
 	cp ~/.cyph/*.mmdb ~/.cyph/github.token js/modules/
