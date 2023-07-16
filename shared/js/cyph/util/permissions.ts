92f4dab4e29279233f3657085dae8b6bed5175e6
Ryan Lester <hacker@linux.com>
2023-07-15 21:58:36 -0400

    permissions fix/cleanup

--- shared/js/cyph/util/permissions.ts
+++ shared/js/cyph/util/permissions.ts
@@ -1,5 +1,23 @@
 import {env} from '../env';
 
+const checkPermissionInternal = async (permission: string) =>
+	new Promise<{hasPermission: boolean}>((resolve, reject) => {
+		(<any> self).cordova.plugins.permissions.checkPermission(
+			permission,
+			resolve,
+			reject
+		);
+	});
+
+const requestPermissionsInternal = async (permissions: string[]) =>
+	new Promise<{hasPermission: boolean}>((resolve, reject) => {
+		(<any> self).cordova.plugins.permissions.requestPermissions(
+			permissions,
+			resolve,
+			reject
+		);
+	});
+
 /**
  * If applicable, requests permissions from OS. No-op on non-Android platforms for now.
  * @returns Whether permissions are granted.
@@ -7,34 +25,33 @@ import {env} from '../env';
 export const requestPermissions = async (
 	...permissions: string[]
 ) : Promise<boolean> => {
-	if (!(env.isCordova && env.isAndroid)) {
+	if (!(env.isCordova && env.isAndroid) || permissions.length < 1) {
 		return true;
 	}
 
-	permissions = permissions.map(s => `permission.${s}`);
+	permissions = permissions.map(s => `android.permission.${s}`);
 
 	try {
-		const oldStatus = await new Promise<{hasPermission: boolean}>(
-			(resolve, reject) => {
-				(<any> self).cordova.plugins.permissions.checkPermission(
-					permissions,
-					resolve,
-					reject
-				);
-			}
+		const newPermissions = (
+			await Promise.all(
+				permissions.map(async permission => ({
+					permission,
+					status: await checkPermissionInternal(permission)
+				}))
+			)
+		)
+			.filter(o => o.status.hasPermission)
+			.map(o => o.permission);
+
+		if (newPermissions.length < 1) {
+			return true;
+		}
+
+		const newPermissionsStatus = await requestPermissionsInternal(
+			newPermissions
 		);
 
-		const newStatus = oldStatus.hasPermission ?
-			oldStatus :
-			await new Promise<{hasPermission: boolean}>((resolve, reject) => {
-				(<any> self).cordova.plugins.permissions.requestPermission(
-					permissions,
-					resolve,
-					reject
-				);
-			});
-
-		return newStatus.hasPermission;
+		return newPermissionsStatus.hasPermission;
 	}
 	catch {
 		return false;
