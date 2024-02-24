343a048a428e6e65cb3cd60b8e4aa01bb9a70f9c
Ryan Lester <hacker@linux.com>
2024-02-24 20:33:47 +0000

    updatelibs

--- shared/js/cyph/syncfusion-license-key.ts
+++ shared/js/cyph/syncfusion-license-key.ts
@@ -1,3 +1,3 @@
 /** Syncfusion license key. */
 export const syncfusionLicenseKey =
-	'ORg4AjUWIQA/Gnt2UVhiQlZPd11dXmJWd1p/THNYflR1fV9DaUwxOX1dQl9nSXxRdURiXH5feHRXTmE=';
+	'ORg4AjUWIQA/Gnt2UVhiQlZPd11dXmJWd1p/THNYflR1fV9DaUwxOX1dQl9nSXxRckRmWHxadHZWQWQ=';
